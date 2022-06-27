990134831eced875e59f61a57118b57dae62eab0
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2022-06-27 14:29:23 +0300

    fix: wrong import

--- packages/ui/src/1_atoms/Lead/Lead.tsx
+++ packages/ui/src/1_atoms/Lead/Lead.tsx
@@ -1,5 +1,5 @@
 import React, { FC, PropsWithChildren } from 'react';
-import styles from './Lead.module.css';
+import styles from './lead.module.css';
 
 export type LeadProps = {
   test?: boolean;

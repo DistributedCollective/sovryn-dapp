1d30dbc457e315cc4e25fdbee0b8d8357c1a650c
Peiman <peymannaderi3@yahoo.com>
2022-08-23 19:40:18 +0430

    feat: add multiple theme support

--- apps/frontend/src/app/5_pages/App.tsx
+++ apps/frontend/src/app/5_pages/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import { Button, Lead } from '@sovryn/ui';
 import styles from './index.module.css';
 
+enum Theme {
+  'sovryn' = 'sovryn',
+  'dark' = 'dark',
+  'light' = 'light',
+  'coffee' = 'coffee',
+}
+
 function App() {
+  const handleThemeChange = (theme: Theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+  };
+
   return (
     <div className="my-2 px-4">
       <header>
@@ -16,6 +27,39 @@ function App() {
         <Button />
         <Lead test={false}>abc</Lead>
         <Lead test={true}>def</Lead>
+
+        <div className="flex items-center gap-4">
+          <div
+            className="cursor-pointer"
+            onClick={() => handleThemeChange(Theme.sovryn)}
+          >
+            Sovryn
+          </div>
+          <div
+            className="cursor-pointer"
+            onClick={() => handleThemeChange(Theme.dark)}
+          >
+            Dark
+          </div>
+          <div
+            className="cursor-pointer"
+            onClick={() => handleThemeChange(Theme.light)}
+          >
+            Light
+          </div>
+          <div
+            className="cursor-pointer"
+            onClick={() => handleThemeChange(Theme.coffee)}
+          >
+            coffee
+          </div>
+        </div>
+        <p className="text-primary">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+          tempora itaque voluptatum veritatis perferendis temporibus tenetur
+          quod corrupti rerum voluptatibus? Totam a quas recusandae quasi
+          molestiae ipsa omnis dolorum aliquam.
+        </p>
       </header>
     </div>
   );

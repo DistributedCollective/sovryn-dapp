49e2dcc23aa0a8909ac0e1dd5d9898bf1f8b178f
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-09-27 19:24:53 +0200

    [Dropdown] - update className

--- packages/ui/src/2_molecules/Dropdown/Dropdown.tsx
+++ packages/ui/src/2_molecules/Dropdown/Dropdown.tsx
@@ -76,7 +76,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
     return getDropdownPositionStyles(coords, mode);
   }, [coords, mode]);
 
-  const classNameComplete = useMemo(
+  const classNames = useMemo(
     () =>
       classNames(styles.button, styles[size], className, {
         [styles.isOpen]: isOpen,
@@ -103,7 +103,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
   return (
     <>
       <button
-        className={classNames(classNameComplete)}
+        className={classNames(classNames)}
         data-action-id={dataActionId}
         onClick={onButtonClick}
         type="button"

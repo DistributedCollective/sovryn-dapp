c16efedae95079c71d58de973d1b9fabb7853341
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-09-28 09:17:52 +0200

    chore: review fixes

--- packages/ui/src/1_atoms/Paragraph/Paragraph.stories.tsx
+++ packages/ui/src/1_atoms/Paragraph/Paragraph.stories.tsx
@@ -14,32 +14,104 @@ export default {
   component: Paragraph,
 } as Meta;
 
-const Template: Story<ComponentProps<typeof Paragraph>> = args => (
+const Template: Story<ComponentProps<typeof Paragraph>> = () => (
   <>
     <Paragraph
-      {...args}
       size={ParagraphSize.small}
       style={ParagraphStyle.short}
+      type={ParagraphType.regular}
+      children="small short regular"
     />
     <br />
     <Paragraph
-      {...args}
+      size={ParagraphSize.small}
+      style={ParagraphStyle.short}
+      type={ParagraphType.medium}
+      children="small short medium"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.small}
+      style={ParagraphStyle.short}
+      type={ParagraphType.semibold}
+      children="small short semibold"
+    />
+    <br />
+    <Paragraph
       size={ParagraphSize.base}
       style={ParagraphStyle.normal}
       type={ParagraphType.regular}
+      children="base normal regular"
     />
     <br />
     <Paragraph
-      {...args}
+      size={ParagraphSize.base}
+      style={ParagraphStyle.normal}
+      type={ParagraphType.medium}
+      children="base normal medium"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.base}
+      style={ParagraphStyle.normal}
+      type={ParagraphType.semibold}
+      children="base normal semibold"
+    />
+    <br />
+    <Paragraph
       size={ParagraphSize.large}
       style={ParagraphStyle.tall}
+      type={ParagraphType.regular}
+      children="large tall regular"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.large}
+      style={ParagraphStyle.tall}
+      type={ParagraphType.medium}
+      children="large tall medium"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.large}
+      style={ParagraphStyle.tall}
+      type={ParagraphType.semibold}
+      children="large tall semibold"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.extraLarge}
+      style={ParagraphStyle.normal}
+      type={ParagraphType.regular}
+      children="extraLarge normal regular"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.extraLarge}
+      style={ParagraphStyle.normal}
+      type={ParagraphType.medium}
+      children="extraLarge normal medium"
+    />
+    <br />
+    <Paragraph
+      size={ParagraphSize.extraLarge}
+      style={ParagraphStyle.normal}
+      type={ParagraphType.semibold}
+      children="extraLarge normal semibold"
     />
   </>
 );
 
 export const Default = Template.bind({});
-Default.args = {
-  children:
-    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque tempora itaque voluptatum veritatis perferendis temporibus tenetur quod corrupti rerum voluptatibus? Totam a quas recusandae quasi molestiae ipsa omnis dolorum aliquam.',
+
+const InteractiveTemplate: Story<ComponentProps<typeof Paragraph>> = args => {
+  return <Paragraph {...args} />;
+};
+
+export const Interactive = InteractiveTemplate.bind({});
+Interactive.args = {
+  children: 'You can change this text',
   type: ParagraphType.regular,
+  size: ParagraphSize.base,
+  style: ParagraphStyle.normal,
 };
--- packages/ui/src/1_atoms/Paragraph/Paragraph.types.ts
+++ packages/ui/src/1_atoms/Paragraph/Paragraph.types.ts
@@ -1,11 +1,14 @@
 export enum ParagraphType {
+  medium = 'medium',
   regular = 'regular',
+  semibold = 'semibold',
 }
 
 export enum ParagraphSize {
   small = 'small',
   base = 'base',
   large = 'large',
+  extraLarge = 'extraLarge',
 }
 
 export enum ParagraphStyle {

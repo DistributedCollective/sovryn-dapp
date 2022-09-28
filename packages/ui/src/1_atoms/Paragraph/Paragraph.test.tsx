2f9fa99b5f5119d1f4ba7dca910939c63a3f21f0
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-09-28 09:32:07 +0200

    chore: review fixes, added identity-obj-proxy for styles rendering

--- packages/ui/src/1_atoms/Paragraph/Paragraph.test.tsx
+++ packages/ui/src/1_atoms/Paragraph/Paragraph.test.tsx
@@ -3,16 +3,42 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { Paragraph } from './Paragraph';
+import {
+  ParagraphSize,
+  ParagraphStyle,
+  ParagraphType,
+} from './Paragraph.types';
 
 describe('Paragraph', () => {
-  test('renders a paragraph', () => {
+  it('renders a paragraph', () => {
     render(<Paragraph children="Testing paragraph" />);
     expect(screen.getByText('Testing paragraph')).toBeInTheDocument();
   });
 
-  test('check paragraph instance', () => {
+  it('check paragraph instance', () => {
     render(<Paragraph children="test" />);
     const paragraph = screen.getByText('test');
     expect(paragraph).toBeInstanceOf(HTMLParagraphElement);
+  });
+
+  it('should render a paragraph size with a className equal to the extraLarge', () => {
+    const size = ParagraphSize.extraLarge;
+    render(<Paragraph children="Paragraph size" size={size} />);
+    const paragraph = screen.getByText('Paragraph size').getAttribute('class');
+    expect(paragraph).toContain('extraLarge');
+  });
+
+  it('should render a paragraph type with a className equal to the semibold', () => {
+    const type = ParagraphType.semibold;
+    render(<Paragraph children="Paragraph type" type={type} />);
+    const paragraph = screen.getByText('Paragraph type').getAttribute('class');
+    expect(paragraph).toContain('semibold');
+  });
+
+  it('should render a paragraph style with a className equal to the tall', () => {
+    const style = ParagraphStyle.tall;
+    render(<Paragraph children="Paragraph style" style={style} />);
+    const paragraph = screen.getByText('Paragraph style').getAttribute('class');
+    expect(paragraph).toContain('tall');
   });
 });

a467d5b26b6c84a37c3808d9642e6e5cbb88b085
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-09-30 16:30:23 +0200

    [Button] - fix comments

--- packages/ui/src/1_atoms/Button/Button.test.tsx
+++ packages/ui/src/1_atoms/Button/Button.test.tsx
@@ -1,21 +1,21 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import React from 'react';
 
 import { Button } from './Button';
-// import { ButtonSize, ButtonStyle } from './Button.types';
 
+// import { ButtonSize, ButtonStyle } from './Button.types';
 describe('Button', () => {
   it('renders a button', () => {
     render(<Button text="Button" />);
     expect(screen.getByText('Button')).toBeInTheDocument();
   });
-
   it('eventHandler called on click', () => {
     const handleClick = jest.fn();
     render(<Button text="Button" onClick={handleClick} />);
     const button = screen.getByText('Button');
-    fireEvent.click(button);
+    userEvent.click(button);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
@@ -23,22 +23,19 @@ describe('Button', () => {
     const handleClick = jest.fn();
     render(<Button text="Button" disabled onClick={handleClick} />);
     const button = screen.getByText('Button');
-    fireEvent.click(button);
+    userEvent.click(button);
     expect(handleClick).toHaveBeenCalledTimes(0);
   });
 
   // uncomment these test cases once Paragraph component will be merged (https://github.com/DistributedCollective/sovryn-dapp/pull/13)
   // it('should render a button size with a className equal to the large', () => {
-  //   const size = ButtonSize.large;
-  //   render(<Button text="Button size" size={size} />);
-  //   const button = screen.getByText('Button size').getAttribute('class');
+  //   render(<Button text="Button size" size={ButtonSize.large} />);
+  //   const classes = screen.getByText('Button size').getAttribute('class');
   //   expect(button).toContain('large');
   // });
-
   // it('should render a button style with a className equal to the secondary', () => {
-  //   const style = ButtonStyle.secondary;
-  //   render(<Button text="Button style" style={style} />);
-  //   const button = screen.getByText('Button style').getAttribute('class');
+  //   render(<Button text="Button style" style={ButtonStyle.secondary} />);
+  //   const classes = screen.getByText('Button style').getAttribute('class');
   //   expect(button).toContain('secondary');
   // });
 });

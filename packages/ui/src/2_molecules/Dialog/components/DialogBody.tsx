b6f6831a485a67bf35cee714c555e48675de196b
soulBit <its.soulBit@gmail.com>
2022-09-30 16:36:38 +0100

    chore: use uniform padding and add className prop in DialogBody

--- packages/ui/src/2_molecules/Dialog/components/DialogBody.tsx
+++ packages/ui/src/2_molecules/Dialog/components/DialogBody.tsx
@@ -1,7 +1,18 @@
 import { FC, PropsWithChildren } from 'react';
 
+import classNames from 'classnames';
+
 import styles from './DialogBody.module.css';
 
-export const DialogBody: FC<PropsWithChildren> = ({ children }) => {
-  return <main className={styles.container}>{children}</main>;
+type DialogBodyProps = {
+  className?: string;
+};
+
+export const DialogBody: FC<PropsWithChildren<DialogBodyProps>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <main className={classNames(styles.container, className)}>{children}</main>
+  );
 };

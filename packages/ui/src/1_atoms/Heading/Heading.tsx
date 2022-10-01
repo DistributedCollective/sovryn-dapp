1733dc0fce23b5646c3276d59bace4548bce8491
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-10-01 11:34:00 +0200

    [Heading] - add component

---
+++ packages/ui/src/1_atoms/Heading/Heading.tsx
@@ -0,0 +1,25 @@
+import React, { FC, ReactNode } from 'react';
+
+import classNames from 'classnames';
+
+import styles from './Heading.module.css';
+import { HeadingType } from './Heading.types';
+
+export type HeadingProps = {
+  children?: ReactNode;
+  type?: HeadingType;
+  className?: string;
+};
+
+export const Heading: FC<HeadingProps> = ({
+  children,
+  type = HeadingType.h1,
+  className,
+}) =>
+  React.createElement(
+    type,
+    {
+      className: classNames(styles[type], className),
+    },
+    children,
+  );

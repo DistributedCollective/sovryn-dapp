1663d053ee5c44e8310f7b3e25470d6b3501c24f
Peiman <peymannaderi3@yahoo.com>
2022-10-04 12:45:57 +0330

    resolve PR issues

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: [require('@sovryn/tailwindcss-config/index.js')],
   content: [
-    './src/**/*.{html,ts,tsx,css}',
+    './src/**/*.{html,ts,tsx}',
     './public/**/*.html',
-    '../../packages/ui/src/**/*.{html,ts,tsx,css}',
+    '../../packages/ui/src/**/*.{html,ts,tsx}',
   ],
 };

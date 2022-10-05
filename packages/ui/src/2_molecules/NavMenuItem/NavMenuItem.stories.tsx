bba8a52b0f405ef296f7fe04e194a1cc11881810
Pietro Maximoff <74987028+pietro-maximoff@users.noreply.github.com>
2022-10-05 11:14:07 +0300

    NavMenuItem component (#23)
    * [NavMenuItem] - implement component
    
    * chore: add fixes
    
    * chore: add fixes
    
    * chore: update jest test
    
    * chore: rename max count

---
+++ packages/ui/src/2_molecules/NavMenuItem/NavMenuItem.stories.tsx
@@ -0,0 +1,41 @@
+import { Story } from '@storybook/react';
+
+import React, { ComponentProps, useReducer } from 'react';
+
+import { NavMenuItem } from './NavMenuItem';
+
+export default {
+  title: 'Molecule/NavMenuItem',
+  component: NavMenuItem,
+};
+
+const Template: Story<ComponentProps<typeof NavMenuItem>> = args => (
+  <>
+    <NavMenuItem {...args} />
+  </>
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  children: 'Menu item',
+  count: 100,
+  className: 'm-2',
+  isActive: false,
+  maxCount: 99,
+  dataActionId: '',
+};
+
+const InteractiveTemplate: Story<ComponentProps<typeof NavMenuItem>> = args => {
+  const [active, handleToggle] = useReducer(state => !state, false);
+  return <NavMenuItem {...args} onClick={handleToggle} isActive={active} />;
+};
+
+export const Interactive = InteractiveTemplate.bind({});
+Interactive.args = {
+  children: 'Menu item',
+  count: 1,
+  className: 'm-2',
+  isActive: false,
+  maxCount: 99,
+  dataActionId: '',
+};
---
+++ packages/ui/src/2_molecules/NavMenuItem/NavMenuItem.tsx
@@ -0,0 +1,51 @@
+import React, { FC, ReactNode, useMemo } from 'react';
+
+import classNames from 'classnames';
+
+import { Badge } from '../../1_atoms/Badge/Badge';
+import styles from './NavMenuItem.module.css';
+
+const MAX_COUNT_DEFAULT = 99;
+
+type NavMenuItemProps = {
+  count?: number;
+  onClick?: () => void;
+  children: ReactNode;
+  isActive?: boolean;
+  className?: string;
+  dataActionId?: string;
+  maxCount?: number;
+};
+
+export const NavMenuItem: FC<NavMenuItemProps> = ({
+  count,
+  onClick,
+  children,
+  isActive,
+  className,
+  dataActionId,
+  maxCount = MAX_COUNT_DEFAULT,
+}) => {
+  const formattedCount = useMemo(
+    () =>
+      count && count > maxCount
+        ? `${maxCount}+`
+        : count,
+    [count],
+  );
+
+  return (
+    <button
+      type="button"
+      data-action-id={dataActionId}
+      onClick={onClick}
+      className={classNames(styles.navMenuItem, className, {
+        [styles.active]: isActive,
+        [styles.count]: formattedCount,
+      })}
+    >
+      {children}
+      {formattedCount && <Badge content={formattedCount} />}
+    </button>
+  );
+};

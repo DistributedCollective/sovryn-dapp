3b31de03a9dd8959272d5613870fee25d134042c
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-10-20 20:46:43 +0200

    chore: cleanups

--- packages/ui/src/1_atoms/Button/Button.test.tsx
+++ packages/ui/src/1_atoms/Button/Button.test.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { Button } from './Button';
+import { ButtonSize, ButtonStyle } from './Button.types';
 
 // import { ButtonSize, ButtonStyle } from './Button.types';
 describe('Button', () => {
@@ -65,15 +66,14 @@ describe('Button', () => {
     expect(document.activeElement).toEqual(ref.current);
   });
 
-  // uncomment these test cases once Paragraph component will be merged (https://github.com/DistributedCollective/sovryn-dapp/pull/13)
-  // it('should render a button size with a className equal to the large', () => {
-  //   render(<Button text="Button size" size={ButtonSize.large} />);
-  //   const classes = screen.getByText('Button size').getAttribute('class');
-  //   expect(button).toContain('large');
-  // });
-  // it('should render a button style with a className equal to the secondary', () => {
-  //   render(<Button text="Button style" style={ButtonStyle.secondary} />);
-  //   const classes = screen.getByText('Button style').getAttribute('class');
-  //   expect(button).toContain('secondary');
-  // });
+  it('should render a button size with a className equal to the large', () => {
+    render(<Button text="Button size" size={ButtonSize.large} />);
+    const classes = screen.getByText('Button size').getAttribute('class');
+    expect(classes).toContain('large');
+  });
+  it('should render a button style with a className equal to the secondary', () => {
+    render(<Button text="Button style" style={ButtonStyle.secondary} />);
+    const classes = screen.getByText('Button style').getAttribute('class');
+    expect(classes).toContain('secondary');
+  });
 });

7a05dcea35b3f05252ee623e0a1750fe6b02f1e8
Victor Creed <victor@sovryn.app>
2022-10-20 14:51:15 +0300

    fix: expose component types for use after bundling

---
+++ .changeset/hip-knives-sleep.md
@@ -0,0 +1,7 @@
+---
+'@sovryn/tailwindcss-config': patch
+'@sovryn/tsconfig': patch
+'@sovryn/ui': patch
+---
+
+SOV-633: fix non exported packages and dependencies
--- packages/ui/src/2_molecules/Dialog/Dialog.tsx
+++ packages/ui/src/2_molecules/Dialog/Dialog.tsx
@@ -11,9 +11,6 @@ import {
   IDialogFunctionComponent,
 } from './Dialog.types';
 
-export * from './components/DialogBody/DialogBody';
-export * from './components/DialogHeader/DialogHeader';
-
 type DialogProps = {
   isOpen: boolean;
   children: React.ReactNode;

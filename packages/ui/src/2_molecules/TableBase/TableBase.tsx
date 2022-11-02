04869b5171fc2882d2fbaf10660a944b6ac6f86d
tiltom <tiltom.defi@protonmail.com>
2022-11-02 13:08:31 +0100

    SOV-678: add address table component (#44)
    * Initial commit
    
    * Add Jest tests
    
    * Add clickable and hover styles
    
    * Fix row borders to be 1px wide
    
    * Fix review remarks
    
    * Create empty-socks-grin.md
    
    * QA adjustments

---
+++ packages/ui/src/2_molecules/TableBase/TableBase.tsx
@@ -0,0 +1,76 @@
+import React, { ReactNode } from 'react';
+
+import classNames from 'classnames';
+
+import styles from './TableBase.module.css';
+import { ColumnOptions, RowObject } from './TableBase.types';
+import { TableRow } from './components/TableRow/TableRow';
+import rowStyles from './components/TableRow/TableRow.module.css';
+
+export type TableBaseProps<RowType extends RowObject> = {
+  className?: string;
+  columns: ColumnOptions<RowType>[];
+  rows?: RowType[];
+  rowKey?: (row: RowType) => number | string;
+  noData?: ReactNode;
+  onRowClick?: (row: RowType) => void;
+  dataAttribute?: string;
+  isClickable?: boolean;
+};
+
+// No React.FC, since doesn't support Generic PropTypes
+export const TableBase = <RowType extends RowObject>({
+  className,
+  columns,
+  rows,
+  rowKey,
+  noData,
+  onRowClick,
+  dataAttribute,
+  isClickable,
+}: TableBaseProps<RowType>) => {
+  return (
+    <table
+      className={classNames(styles.table, className)}
+      data-layout-id={dataAttribute}
+    >
+      <thead>
+        <tr>
+          {columns.map(column => (
+            <th
+              key={column.id.toString()}
+              className={classNames(
+                styles.header,
+                column.align && [styles[`text${column.align}`]],
+                column.className,
+              )}
+            >
+              <>{column.title || column.id}</>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className={styles.body}>
+        {rows && rows.length >= 1 ? (
+          rows.map((row, index) => (
+            <TableRow
+              key={rowKey?.(row) || JSON.stringify(row)}
+              columns={columns}
+              row={row}
+              index={index}
+              onRowClick={onRowClick}
+              dataAttribute={dataAttribute}
+              isClickable={isClickable}
+            />
+          ))
+        ) : (
+          <tr className={rowStyles.row}>
+            <td className={styles.noData} colSpan={999}>
+              {noData ? noData : 'No data'}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};

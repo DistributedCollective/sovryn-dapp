9c2f95f5dfb14fb9c256099e2dc6f8195da63fd5
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-11-16 14:49:14 +0100

    chore: update footer links

--- apps/frontend/src/app/3_organisms/Footer/Footer.tsx
+++ apps/frontend/src/app/3_organisms/Footer/Footer.tsx
@@ -5,31 +5,66 @@ import { Link, Footer as UIFooter } from '@sovryn/ui';
 import { SocialLinks } from '../../2_molecules';
 import { SovrynLogo } from '../../2_molecules/SovrynLogo/SovrynLogo';
 import Logo from '../../../assets/images/sovryn-small-logo.svg';
-import { footerLinks } from '../../../utils/constants';
+import { sovrynLinks } from '../../../utils/constants';
 
-export const Footer: FC = () => (
-  <UIFooter
-    leftContent={
-      <SovrynLogo
-        image={Logo}
-        dataAttribute="logo"
-        className="max-h-4 max-w-fit mr-2"
-        text="Powered by Bitcoin"
-        link="/"
-      />
-    }
-    links={
-      <>
-        {footerLinks.map(link => (
-          <Link
-            key={link.id}
-            href={link.href}
-            text={link.name}
-            className="mr-7 mb-5 sm:mb-0"
-          />
-        ))}
-      </>
-    }
-    rightContent={<SocialLinks />}
-  />
-);
+export const Footer: FC = () => {
+  const footerLinks = [
+    {
+      id: 'start',
+      href: sovrynLinks.wiki,
+      name: 'Getting started',
+    },
+    {
+      id: 'dapp-alfa',
+      href: sovrynLinks.dappAlpha,
+      name: 'Sovryn Alpha',
+    },
+    {
+      id: 'blog',
+      href: sovrynLinks.blog,
+      name: 'Blog',
+    },
+    {
+      id: 'fee',
+      href: '#',
+      name: 'Fee prices',
+    },
+    {
+      id: 'terms',
+      href: '#',
+      name: 'Terms & Conditions',
+    },
+    {
+      id: 'policy',
+      href: '#',
+      name: 'Privacy policy',
+    },
+  ];
+
+  return (
+    <UIFooter
+      leftContent={
+        <SovrynLogo
+          image={Logo}
+          dataAttribute="logo"
+          className="max-h-4 max-w-fit mr-2"
+          text="Powered by Bitcoin"
+          link="/"
+        />
+      }
+      links={
+        <>
+          {footerLinks.map(link => (
+            <Link
+              key={link.id}
+              href={link.href}
+              text={link.name}
+              className="mr-7 mb-5 sm:mb-0"
+            />
+          ))}
+        </>
+      }
+      rightContent={<SocialLinks />}
+    />
+  );
+};
--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -5,39 +5,9 @@ export const sovrynLinks = {
   telegram: 'https://t.me/SovrynBitcoin',
   twitter: 'https://twitter.com/Sovrynbtc',
   github: 'https://github.com/DistributedCollective',
+  wiki: 'https://wiki.sovryn.app/en/getting-started',
+  dappAlpha: 'https://live.sovryn.app/',
+  blog: 'https://www.sovryn.app/blog',
 };
-
-export const footerLinks = [
-  {
-    id: 'start',
-    href: 'https://wiki.sovryn.app/en/getting-started',
-    name: 'Getting started',
-  },
-  {
-    id: 'dapp-alfa',
-    href: 'https://live.sovryn.app/',
-    name: 'Sovryn Alpha',
-  },
-  {
-    id: 'blog',
-    href: 'https://www.sovryn.app/blog',
-    name: 'Blog',
-  },
-  {
-    id: 'fee',
-    href: '#',
-    name: 'Fee prices',
-  },
-  {
-    id: 'terms',
-    href: '#',
-    name: 'Terms & Conditions',
-  },
-  {
-    id: 'policy',
-    href: '#',
-    name: 'Privacy policy',
-  },
-];
 
 export const requiredChain = ChainIds.RSK_TESTNET;

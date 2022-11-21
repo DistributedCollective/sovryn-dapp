5e84d848749f4b17ed2041fed60d9eff3e957af3
soulBit <its.soulBit@gmail.com>
2022-11-21 15:09:46 +0000

    feat: add PageContainer and implement in App

--- apps/frontend/src/app/5_pages/App/App.tsx
+++ apps/frontend/src/app/5_pages/App/App.tsx
@@ -1,110 +1,27 @@
-import React, { useReducer } from 'react';
+import React, { FC } from 'react';
 
-import { useTranslation } from 'react-i18next';
+import { Heading, HeadingType } from '@sovryn/ui';
 
-import { Button, Dialog, StatusType } from '@sovryn/ui';
+import { SovrynLogo } from '../../2_molecules';
+import { DebugContent } from '../../2_molecules/DebugContent';
+import { PageContainer } from '../../4_templates';
 
-import { ConnectWalletButton } from '../../2_molecules';
-import { ExampleProviderCall } from '../../2_molecules/ExampleProviderCall';
-import { TransactionStep, Header } from '../../3_organisms';
-import { Footer } from '../../3_organisms/Footer/Footer';
-import { useTheme } from '../../../hooks/useTheme';
-import { useWalletConnect } from '../../../hooks/useWalletConnect';
-import { translations } from '../../../locales/i18n';
-import { AppTheme } from '../../../types/tailwind';
-
-function App() {
-  const { handleThemeChange } = useTheme();
-  const { t } = useTranslation();
-  const [isOpen, toggle] = useReducer(p => !p, false);
-  const { connectWallet, disconnectWallet, wallets, pending } =
-    useWalletConnect();
-
-  return (
-    <>
-      <Header />
-      <div className="my-2 px-4">
-        <div>
-          <ExampleProviderCall />
-
-          <hr className="my-12" />
-
-          <Button text="Open Dialog" onClick={toggle} />
-
-          <Dialog isOpen={isOpen} onClose={toggle}>
-            <div className="p-4">Hello.</div>
-          </Dialog>
-
-          <div className="flex items-center gap-4">
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.sovryn)}
-            >
-              Sovryn
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.dark)}
-            >
-              Dark
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.light)}
-            >
-              Light
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.coffee)}
-            >
-              coffee
-            </div>
-          </div>
-          <p className="text-primary">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-            tempora itaque voluptatum veritatis perferendis temporibus tenetur
-            quod corrupti rerum voluptatibus? Totam a quas recusandae quasi
-            molestiae ipsa omnis dolorum aliquam.
-          </p>
-          <p className="text-primary">
-            GTM: {process.env.REACT_APP_GOOGLE_ANALYTICS}
-          </p>
+const App: FC = () => (
+  <>
+    <PageContainer contentClassName="flex flex-col">
+      <div className="flex flex-col flex-grow justify-center">
+        <div className="flex justify-center items-center">
+          <SovrynLogo dataAttribute="construction-logo" className="w-96" />
         </div>
-        <main>
-          <TransactionStep
-            step="1"
-            title="Approve FISH tokens"
-            subtitle="Allow Sovryn protocol to use FISH tokens for the trade"
-            txDetails={{
-              amount: '0.17519949',
-              token: 'FISH',
-              gasFee: '0.00006191',
-            }}
-            status={StatusType.idle}
-            txID="0xEDb8897aB6E907bc63CB256f74437D36298507E2"
-          />
-          <br />
-          <br />
-          <br />
-          <div>
-            <ConnectWalletButton
-              onConnect={connectWallet}
-              onDisconnect={disconnectWallet}
-              address={wallets[0]?.accounts[0]?.address}
-              pending={pending}
-            />
-          </div>
-
-          <br />
-          <br />
-
-          <p>{t(translations.wallet)}</p>
-        </main>
+        <Heading type={HeadingType.h1} className="text-center mt-2">
+          Sovryn Dapp is currently under construction.
+        </Heading>
       </div>
-      <Footer />
-    </>
-  );
-}
+      <div className="w-full justify-self-end">
+        <DebugContent />
+      </div>
+    </PageContainer>
+  </>
+);
 
 export default App;

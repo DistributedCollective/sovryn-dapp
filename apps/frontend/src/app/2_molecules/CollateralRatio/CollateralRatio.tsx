508f41abc8d79882fd2fdc516f0fb8b013dcc365
Peiman <peymannaderi3@yahoo.com>
2022-11-21 12:38:25 +0330

    feat: CollateralRatio component

---
+++ apps/frontend/src/app/2_molecules/CollateralRatio/CollateralRatio.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react';
+
+import {
+  Heading,
+  HealthBar,
+  HeadingType,
+  HelperButton,
+  Paragraph,
+} from '@sovryn/ui';
+
+export type CollateralRatioProps = {
+  value?: number;
+  minCRatio: number;
+};
+
+export const CollateralRatio: FC<CollateralRatioProps> = ({
+  value,
+  minCRatio,
+}) => {
+  return (
+    <div className="w-80">
+      <div className="flex items-center justify-between mb-3">
+        <Heading
+          className="text-gray-10 flex items-center"
+          type={HeadingType.h3}
+        >
+          Collateral Ratio
+          <HelperButton className="ml-1.5" content="Collateral Ratio" />
+        </Heading>
+        <Heading
+          className="text-gray-30 flex items-center"
+          type={HeadingType.h3}
+        >
+          {value !== undefined ? `${value}%` : 'N/A'}
+        </Heading>
+      </div>
+      <HealthBar
+        start={90}
+        middleStart={110}
+        middleEnd={150}
+        end={250}
+        value={value}
+      />
+      <Paragraph className="text-gray-30 flex items-center mt-2.5">
+        Collateral ratio must be above {minCRatio}%
+      </Paragraph>
+    </div>
+  );
+};

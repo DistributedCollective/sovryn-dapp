8b405c956ce8a8a4205a1e3db129b60c591f5f4c
soulBit <its.soulBit@gmail.com>
2022-11-21 15:09:46 +0000

    feat: add PageContainer and implement in App

--- apps/frontend/src/app/2_molecules/DebugContent.tsx
+++ apps/frontend/src/app/2_molecules/DebugContent.tsx
@@ -1,15 +1,24 @@
-import React, { useReducer } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { Accordion, StatusType } from '@sovryn/ui';
+import { Accordion, Button } from '@sovryn/ui';
 
-import { TransactionStep } from '../3_organisms';
+import { TransactionStepDialog } from '../3_organisms';
 import { useTheme, useWalletConnect } from '../../hooks';
 import { translations } from '../../locales/i18n';
 import { AppTheme } from '../../types/tailwind';
 import { ConnectWalletButton } from './ConnectWalletButton/ConnectWalletButton';
 import { ExampleProviderCall } from './ExampleProviderCall';
+import { APPROVAL_FUNCTION } from '../../utils/constants';
+import { defaultChainId } from '../../config/chains';
+import { useTransactionContext } from '../../context/transactionContext';
+import { ethers } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
+
+import { getTokenDetails, SupportedTokens } from '@sovryn/contracts';
+import { ExampleTokenDetails } from './ExampleTokenDetails';
+import { ExampleTypedDataSign } from './ExampleTypedDataSign';
 
 // usage example, to be removed
 export const DebugContent = () => {
@@ -19,10 +28,56 @@ export const DebugContent = () => {
   const { connectWallet, disconnectWallet, wallets, pending } =
     useWalletConnect();
 
+  const { setTransactions, setIsOpen, setTitle } = useTransactionContext();
+
+  const approve = useCallback(async () => {
+    if (!wallets[0].provider) {
+      return;
+    }
+    const { address, abi, symbol } = await getTokenDetails(
+      SupportedTokens.xusd,
+      defaultChainId,
+    );
+
+    const provider = new ethers.providers.Web3Provider(wallets[0].provider);
+    const signer = provider.getSigner();
+    const xusd = new ethers.Contract(address, abi, signer);
+
+    setTransactions([
+      {
+        title: `Approve ${symbol} tokens`,
+        subtitle: `Allow Sovryn protocol to use ${symbol} tokens for the trade`,
+        contract: xusd,
+        fnName: APPROVAL_FUNCTION,
+        args: [address, parseUnits('2')],
+      },
+      {
+        title: `Transfer 2 ${symbol} tokens`,
+        contract: xusd,
+        fnName: 'transfer',
+        args: [wallets[0]?.accounts[0]?.address, parseUnits('2')],
+      },
+    ]);
+    setTitle('Transaction approval');
+    setIsOpen(true);
+  }, [setIsOpen, setTitle, setTransactions, wallets]);
+
   return (
     <Accordion label="Debug content" open={isOpen} onClick={toggle}>
+      <TransactionStepDialog />
       <ExampleProviderCall />
+      <ExampleTokenDetails />
 
+      <hr className="my-12" />
+
+      {wallets[0]?.accounts[0]?.address ? (
+        <div>
+          <Button text="Approve" onClick={approve} />
+          <ExampleTypedDataSign />
+        </div>
+      ) : (
+        <Button text="Connect to RSK Testnet" onClick={connectWallet} />
+      )}
       <hr className="my-12" />
 
       <div className="flex items-center gap-4">
@@ -60,18 +115,6 @@ export const DebugContent = () => {
       <p className="text-primary">
         GTM: {process.env.REACT_APP_GOOGLE_ANALYTICS}
       </p>
-      <TransactionStep
-        step="1"
-        title="Approve FISH tokens"
-        subtitle="Allow Sovryn protocol to use FISH tokens for the trade"
-        txDetails={{
-          amount: '0.17519949',
-          token: 'FISH',
-          gasFee: '0.00006191',
-        }}
-        status={StatusType.idle}
-        txID="0xEDb8897aB6E907bc63CB256f74437D36298507E2"
-      />
       <br />
       <br />
       <br />
--- apps/frontend/src/app/5_pages/App/App.tsx
+++ apps/frontend/src/app/5_pages/App/App.tsx
@@ -1,154 +1,27 @@
-import React, { useCallback, useReducer } from 'react';
+import React, { FC } from 'react';
 
-import { ethers } from 'ethers';
-import { parseUnits } from 'ethers/lib/utils';
-import { useTranslation } from 'react-i18next';
+import { Heading, HeadingType } from '@sovryn/ui';
 
-import { getTokenDetails, SupportedTokens } from '@sovryn/contracts';
-import { Button, Dialog } from '@sovryn/ui';
+import { SovrynLogo } from '../../2_molecules';
+import { DebugContent } from '../../2_molecules/DebugContent';
+import { PageContainer } from '../../4_templates';
 
-import { ConnectWalletButton } from '../../2_molecules';
-import { ExampleProviderCall } from '../../2_molecules/ExampleProviderCall';
-import { ExampleTokenDetails } from '../../2_molecules/ExampleTokenDetails';
-import { ExampleTypedDataSign } from '../../2_molecules/ExampleTypedDataSign';
-import { Header } from '../../3_organisms';
-import { Footer } from '../../3_organisms/Footer/Footer';
-import { TransactionStepDialog } from '../../3_organisms/TransactionStepDialog/TransactionStepDialog';
-import { defaultChainId } from '../../../config/chains';
-import { useTransactionContext } from '../../../context/transactionContext';
-import { useTheme } from '../../../hooks/useTheme';
-import { useWalletConnect } from '../../../hooks/useWalletConnect';
-import { translations } from '../../../locales/i18n';
-import { AppTheme } from '../../../types/tailwind';
-import { APPROVAL_FUNCTION } from '../../../utils/constants';
-
-function App() {
-  const { handleThemeChange } = useTheme();
-  const { t } = useTranslation();
-  const [isOpen, toggle] = useReducer(p => !p, false);
-  const { connectWallet, disconnectWallet, wallets, pending } =
-    useWalletConnect();
-
-  const { setTransactions, setIsOpen, setTitle } = useTransactionContext();
-
-  const approve = useCallback(async () => {
-    if (!wallets[0].provider) return;
-
-    const { address, abi, symbol } = await getTokenDetails(
-      SupportedTokens.xusd,
-      defaultChainId,
-    );
-
-    const provider = new ethers.providers.Web3Provider(wallets[0].provider);
-    const signer = provider.getSigner();
-    const xusd = new ethers.Contract(address, abi, signer);
-
-    setTransactions([
-      {
-        title: `Approve ${symbol} tokens`,
-        subtitle: `Allow Sovryn protocol to use ${symbol} tokens for the trade`,
-        contract: xusd,
-        fnName: APPROVAL_FUNCTION,
-        args: [address, parseUnits('2')],
-      },
-      {
-        title: `Transfer 2 ${symbol} tokens`,
-        contract: xusd,
-        fnName: 'transfer',
-        args: [wallets[0]?.accounts[0]?.address, parseUnits('2')],
-      },
-    ]);
-    setTitle('Transaction approval');
-    setIsOpen(true);
-  }, [setIsOpen, setTitle, setTransactions, wallets]);
-
-  return (
-    <>
-      <Header />
-      <TransactionStepDialog />
-
-      <div className="my-2 px-4">
-        <div>
-          <ExampleProviderCall />
-          <ExampleTokenDetails />
-
-          <hr className="my-12" />
-
-          {wallets[0]?.accounts[0]?.address ? (
-            <div>
-              <Button text="Approve" onClick={approve} />
-              <ExampleTypedDataSign />
-            </div>
-          ) : (
-            <Button text="Connect to RSK Testnet" onClick={connectWallet} />
-          )}
-
-          <hr className="my-12" />
-
-          <Button text="Open Dialog" onClick={toggle} />
-
-          <Dialog isOpen={isOpen} onClose={toggle}>
-            <div className="p-4">Hello.</div>
-          </Dialog>
-
-          <div className="flex items-center gap-4">
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.sovryn)}
-            >
-              Sovryn
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.dark)}
-            >
-              Dark
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.light)}
-            >
-              Light
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleThemeChange(AppTheme.coffee)}
-            >
-              coffee
-            </div>
-          </div>
-          <p className="text-primary">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-            tempora itaque voluptatum veritatis perferendis temporibus tenetur
-            quod corrupti rerum voluptatibus? Totam a quas recusandae quasi
-            molestiae ipsa omnis dolorum aliquam.
-          </p>
-          <p className="text-primary">
-            GTM: {process.env.REACT_APP_GOOGLE_ANALYTICS}
-          </p>
+const App: FC = () => (
+  <>
+    <PageContainer contentClassName="flex flex-col">
+      <div className="flex flex-col flex-grow justify-center">
+        <div className="flex justify-center items-center">
+          <SovrynLogo dataAttribute="construction-logo" className="w-96" />
         </div>
-        <main>
-          <br />
-          <br />
-          <br />
-          <div>
-            <ConnectWalletButton
-              onConnect={connectWallet}
-              onDisconnect={disconnectWallet}
-              address={wallets[0]?.accounts[0]?.address}
-              pending={pending}
-            />
-          </div>
-
-          <br />
-          <br />
-
-          <p>{t(translations.wallet)}</p>
-        </main>
+        <Heading type={HeadingType.h1} className="text-center mt-2">
+          Sovryn Dapp is currently under construction.
+        </Heading>
       </div>
-      <Footer />
-    </>
-  );
-}
+      <div className="w-full justify-self-end">
+        <DebugContent />
+      </div>
+    </PageContainer>
+  </>
+);
 
 export default App;

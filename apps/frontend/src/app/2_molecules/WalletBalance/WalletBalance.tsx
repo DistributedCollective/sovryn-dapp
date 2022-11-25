88a1831e49ce6abf59d90e00e159ef4322d32cad
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-11-25 11:27:11 +0100

    chore: fixes from comments

--- apps/frontend/src/app/2_molecules/WalletBalance/WalletBalance.tsx
+++ apps/frontend/src/app/2_molecules/WalletBalance/WalletBalance.tsx
@@ -6,17 +6,29 @@ import { SimpleTable, SimpleTableRow } from '@sovryn/ui';
 import styles from './WalletBalance.module.css';
 
 export const WalletBalance: FC = () => {
-  const tokenOptions = useMemo(() => {
-    return SupportedTokenList.map(token => (
-      <SimpleTableRow
-        className={styles.row}
-        key={token.symbol}
-        label={token.symbol}
-        value="0" //TODO get balance from the wallet, task SOV-1101
-      />
-      //TODO: hardcoded, need to remove filter once we have a full list of tokens and ask Gilad to provide token symbols
-    )).filter(token => token.props.label !== SupportedTokens.sov);
-  }, []);
+  const tokensToDisplay = useMemo(
+    () => [
+      SupportedTokens.rbtc,
+      SupportedTokens.zusd,
+      SupportedTokens.xusd,
+      SupportedTokens.dllr,
+    ],
+    [],
+  );
+
+  const tokenOptions = useMemo(
+    () =>
+      SupportedTokenList.map(token => (
+        <SimpleTableRow
+          className={styles.row}
+          key={token.symbol}
+          label={token.symbol}
+          value="0" //TODO get balance from the wallet, task SOV-1101
+        />
+        //TODO: hardcoded, need to remove filter once we have a full list of tokens and ask Gilad to provide token symbols
+      )).filter(token => tokensToDisplay.includes(token.props.label)),
+    [tokensToDisplay],
+  );
 
   return (
     <SimpleTable children={tokenOptions} className={styles.walletBalance} />

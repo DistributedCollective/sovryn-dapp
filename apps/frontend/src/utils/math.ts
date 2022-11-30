8c039b3036ab847a0ae7c935f9a8f631aa3aebb5
Victor Creed <victor@sovryn.app>
2022-11-30 13:39:35 +0200

    feat: helper functions for working with ethers bignumbers

--- apps/frontend/src/utils/math.ts
+++ apps/frontend/src/utils/math.ts
@@ -1,3 +1,29 @@
-import { BigNumber } from 'ethers';
+import { isBigNumberish } from '@ethersproject/bignumber/lib/bignumber';
 
-export const add = (a: number, b: number) => a + b;
+import { BigNumberish, BigNumber } from 'ethers';
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+// helper function to convert any type of ethers value to wei.
+export const toWei = (value: BigNumberish): BigNumber => {
+  if (isBigNumberish(value)) {
+    return BigNumber.from(value).mul(BigNumber.from(10).pow(18));
+  }
+
+  const number = Number(value);
+  if (!Number.isNaN(number) && Number.isFinite(number) && value % 1 !== 0) {
+    return parseUnits(number.toString());
+  }
+
+  throw new Error(`Invalid BigNumberish value: ${value}`);
+};
+
+export const fromWei = (
+  value: BigNumberish,
+  unitName?: BigNumberish,
+): string => {
+  if (isBigNumberish(value)) {
+    return formatUnits(BigNumber.from(value).toString(), unitName);
+  }
+
+  throw new Error(`Invalid BigNumberish value: ${value}`);
+};

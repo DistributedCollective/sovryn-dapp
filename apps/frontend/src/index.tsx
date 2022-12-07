79378c9a0db9843fece60e6904373546a6409584
Peiman <25097709+Rickk137@users.noreply.github.com>
2022-12-07 16:44:55 +0330

    feat: PrivacyPolicy & Terms of use  (#118)
    * feat: PrivacyPolicy
    
    * feat: TermsOfUse page
    
    * feat: add loadable component
    
    * Create real-birds-refuse.md
    
    * fix: package version

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,20 +1,28 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import loadable from '@loadable/component';
 
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import setupChains from '@sovryn/ethers-provider';
 import { OnboardProvider } from '@sovryn/onboard-react';
 
-import App from './app/5_pages/App/App';
 import { chains } from './config/chains';
 import { MaintenanceModeContextProvider } from './contexts/MaintenanceModeContext';
 import { TransactionProvider } from './contexts/TransactionContext';
 import './locales/i18n';
 import './styles/tailwindcss/index.css';
 import { graphRskUrl } from './utils/constants';
+
+const App = loadable(() => import('./app/5_pages/App/App'));
+const PrivacyPolicy = loadable(
+  () => import('./app/5_pages/PrivacyPolicy/PrivacyPolicy'),
+);
+const TermsOfUse = loadable(
+  () => import('./app/5_pages/TermsOfUse/TermsOfUse'),
+);
 
 setupChains(chains);
 
@@ -28,13 +36,21 @@ const rskClient = new ApolloClient({
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ApolloProvider client={rskClient}>
         <MaintenanceModeContextProvider>
           <TransactionProvider>
-            <App />
+            <Routes>
+              <Route
+                path="/policies/privacy-policy"
+                element={<PrivacyPolicy />}
+              />
+              <Route path="/policies/terms-of-use" element={<TermsOfUse />} />
+              <Route path="*" element={<App />} />
+            </Routes>
           </TransactionProvider>
         </MaintenanceModeContextProvider>
         <OnboardProvider />

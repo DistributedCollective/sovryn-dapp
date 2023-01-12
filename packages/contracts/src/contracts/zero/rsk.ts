d75a3802c9b3a84573d9ef3285af4eb954fa8230
Victor Creed <victor@sovryn.app>
2023-01-12 16:29:54 +0200

    fix: typo on imported abi file name

--- packages/contracts/src/contracts/zero/rsk.ts
+++ packages/contracts/src/contracts/zero/rsk.ts
@@ -4,6 +4,6 @@ export const rsk: Record<string, AsyncContractConfigData> = {
   borrowerOperations: {
     address: '0x135A1545fa1B2989251921737CD7862c98244FbF',
     getAbi: async () =>
-      (await import('../../abis/zero/BorrowerOperations.json')).default,
+      (await import('../../abis/zero/borrowerOperations.json')).default,
   },
 };

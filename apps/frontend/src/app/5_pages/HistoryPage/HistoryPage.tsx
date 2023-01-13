fa1cca74242bc5f30a097c30ea0127870e0c0f3a
soulBit <its.soulBit@gmail.com>
2023-01-13 12:05:38 +0000

    feat: initial history page implementation

--- apps/frontend/src/app/5_pages/HistoryPage/HistoryPage.tsx
+++ apps/frontend/src/app/5_pages/HistoryPage/HistoryPage.tsx
@@ -1,12 +1,95 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 
-import { Heading, Paragraph } from '@sovryn/ui';
+import classNames from 'classnames';
 
-const HistoryPage: FC = () => (
-  <>
-    <Heading>History</Heading>
-    <Paragraph>This is history page.</Paragraph>
-  </>
+import { Heading, Select, SelectOption, Tabs } from '@sovryn/ui';
+
+import { TransactionHistoryFrame } from '../../3_organisms';
+import { useAccount } from '../../../hooks/useAccount';
+import styles from './HistoryPage.module.css';
+
+const ComingSoon = () => (
+  <div className="px-4 py-12 flex flex-row justify-center">
+    <Heading className="inline">Coming soon...</Heading>
+  </div>
 );
+
+const ACTIVE_CLASSNAME = 'border-t-primary-30';
+
+const HistoryPage: FC = () => {
+  const [index, setIndex] = useState(0);
+  const { account } = useAccount();
+  const locHistoryFrame = useMemo(
+    () => (
+      <div className="p-4">
+        <TransactionHistoryFrame account={account} />
+      </div>
+    ),
+    [account],
+  );
+  const items = useMemo(
+    () => [
+      {
+        label: 'Line of Credit',
+        content: locHistoryFrame,
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'default',
+      },
+      {
+        label: 'Stability pool',
+        content: <ComingSoon />,
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'stability',
+      },
+      {
+        label: 'Conversion',
+        content: <ComingSoon />,
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'conversion',
+      },
+      {
+        label: 'Funding',
+        content: <ComingSoon />,
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'funding',
+      },
+    ],
+    [locHistoryFrame],
+  );
+
+  const options: SelectOption[] = useMemo(
+    () => items.map((val, i) => ({ value: String(i), label: val.label })),
+    [items],
+  );
+
+  return (
+    <div className="w-full text-gray-10 mt-9 sm:mt-24">
+      <Heading className="pb-6 text-base sm:text-2xl">
+        My transaction history
+      </Heading>
+      <div className="w-full">
+        <div className={classNames(styles.mobile)}>
+          <Select
+            options={options}
+            value={String(index)}
+            onChange={(value: string) => setIndex(Number(value))}
+          />
+        </div>
+        <div className={classNames(styles.desktop)}>
+          <Tabs
+            items={items}
+            onChange={setIndex}
+            index={index}
+            className="w-full"
+          />
+        </div>
+        <div className={classNames(styles.mobile)}>
+          {index === 0 && locHistoryFrame}
+          {index > 0 && <ComingSoon />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HistoryPage;

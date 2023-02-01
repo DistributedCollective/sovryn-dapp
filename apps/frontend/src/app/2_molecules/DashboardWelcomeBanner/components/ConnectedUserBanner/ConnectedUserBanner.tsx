b49251c6127dd34ff2cf1ac992896cc5a5135fa3
tiltom <tilnak.tomas@gmail.com>
2023-02-01 15:06:35 +0100

    Zero welcome dashboard banner implementation

--- apps/frontend/src/app/2_molecules/DashboardWelcomeBanner/components/ConnectedUserBanner/ConnectedUserBanner.tsx
+++ apps/frontend/src/app/2_molecules/DashboardWelcomeBanner/components/ConnectedUserBanner/ConnectedUserBanner.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 
-import { useTranslation } from 'react-i18next';
+import { t } from 'i18next';
 
 import { SupportedTokens } from '@sovryn/contracts';
 import { Button } from '@sovryn/ui';
 
+import { FastBtcDialog } from '../../../../3_organisms/FastBtcDialog/FastBtcDialog';
 import { useAssetBalance } from '../../../../../hooks/useAssetBalance';
 import { translations } from '../../../../../locales/i18n';
 
@@ -15,32 +16,51 @@ export type ConnectedUserBannerProps = {
 export const ConnectedUserBanner: FC<ConnectedUserBannerProps> = ({
   openLOC,
 }) => {
-  const { t } = useTranslation();
   const { value, loading } = useAssetBalance(SupportedTokens.rbtc);
+  const [isFastBtcDialogOpen, setIsFastBtcDialogOpen] = useState(false);
+
+  const openFastBtcDialog = useCallback(() => setIsFastBtcDialogOpen(true), []);
+  const closeFastBtcDialog = useCallback(
+    () => setIsFastBtcDialogOpen(false),
+    [],
+  );
+
+  const hasRbtcBalance = useMemo(() => Number(value) !== 0, [value]);
+
+  const onClickHandler = useMemo(
+    () => (hasRbtcBalance ? openLOC : openFastBtcDialog),
+    [hasRbtcBalance, openLOC, openFastBtcDialog],
+  );
+
+  const dataAttribute = useMemo(
+    () => (hasRbtcBalance ? 'zero-loc-open' : 'zero-fund'),
+    [hasRbtcBalance],
+  );
+
+  const buttonText = useMemo(
+    () =>
+      hasRbtcBalance
+        ? t(translations.dashboardWelcomeBanner.OpenCredit)
+        : t(translations.dashboardWelcomeBanner.fundWallet),
+    [hasRbtcBalance],
+  );
 
   if (loading) {
     return <div className="mb-9 h-8" />;
-  }
-
-  if (Number(value) === 0) {
-    return (
-      <div className="flex justify-end mb-9">
-        <Button
-          className="flex-1 md:flex-initial"
-          text={t(translations.dashboardWelcomeBanner.fundWallet)}
-          dataAttribute="zero-fund"
-        />
-      </div>
-    );
   }
 
   return (
     <div className="flex justify-end mb-9">
       <Button
         className="flex-1 md:flex-initial"
-        onClick={openLOC}
-        text={t(translations.dashboardWelcomeBanner.OpenCredit)}
-        dataAttribute="zero-loc-open"
+        onClick={onClickHandler}
+        text={buttonText}
+        dataAttribute={dataAttribute}
+      />
+      <FastBtcDialog
+        isOpen={isFastBtcDialogOpen}
+        onClose={closeFastBtcDialog}
+        shouldHideSend
       />
     </div>
   );

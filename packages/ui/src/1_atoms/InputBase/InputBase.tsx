7178cac5c049f52907c711419a8f8951bc0ec282
Victor Creed <victor@sovryn.app>
2023-02-06 16:27:46 +0200

    fix: input glitching when user stops typing

--- packages/ui/src/1_atoms/InputBase/InputBase.tsx
+++ packages/ui/src/1_atoms/InputBase/InputBase.tsx
@@ -25,7 +25,6 @@ export type InputBaseProps = Omit<
   'ref' | 'size'
 > & {
   debounce?: number;
-
   dataAttribute?: string;
   onChangeText?: (value: string) => void;
   /** @deprecated Use onChangeText if possible */

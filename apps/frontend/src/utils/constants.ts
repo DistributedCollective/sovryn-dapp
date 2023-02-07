337ff5403e8a09b1b407baecc33712e68211bc32
tiltom <tilnak.tomas@gmail.com>
2023-02-07 10:57:22 +0100

    Adjust stability pool gas limit

--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -65,7 +65,7 @@ export const GAS_LIMIT_OPEN_TROVE = 700_000;
 export const GAS_LIMIT_ADJUST_TROVE = 420_000;
 export const GAS_LIMIT_CLOSE_TROVE = 180_000;
 export const GAS_LIMIT_CONVERT = 145_000;
-export const GAS_LIMIT_STABILITY_POOL = 500_000;
+export const GAS_LIMIT_STABILITY_POOL = 220_000;
 
 // TODO: Refactor it into a similar config we have in dapp v1 (blockExplorers constant)
 export const rskExplorer = {

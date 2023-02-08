fcea7e955cb66d6929be49e71e7a16ae7ed492d3
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2023-02-08 14:18:50 +0200

    SOV-1586: trigger funding flow from anywhere within dapp (#201)
    * feat: trigger funding flow from anywhere within dapp SOV-1586
    
    * Create spicy-ties-cough.md
    
    ---------
    
    Co-authored-by: tiltom <tiltom.defi@protonmail.com>

--- apps/frontend/src/app/3_organisms/Header/Header.tsx
+++ apps/frontend/src/app/3_organisms/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useMemo, useReducer, useState } from 'react';
+import React, { FC, useCallback, useMemo, useReducer } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
@@ -19,7 +19,7 @@ import { SovrynLogo } from '../../2_molecules/SovrynLogo/SovrynLogo';
 import { useWalletConnect, useWrongNetworkCheck } from '../../../hooks';
 import { useAssetBalance } from '../../../hooks/useAssetBalance';
 import { translations } from '../../../locales/i18n';
-import { FastBtcDialog } from '../FastBtcDialog/FastBtcDialog';
+import { sharedState } from '../../../store/rxjs/shared-state';
 
 export const Header: FC = () => {
   const { t } = useTranslation();
@@ -40,7 +40,10 @@ export const Header: FC = () => {
     }
   }, [isOpen]);
 
-  const [isFastBtcDialogOpen, setIsFastBtcDialogOpen] = useState(false);
+  const handleFastBtcClick = useCallback(
+    () => sharedState.actions.openFastBtcDialog(),
+    [],
+  );
 
   return (
     <>
@@ -113,14 +116,10 @@ export const Header: FC = () => {
               text={t(translations.header.funding)}
               style={ButtonStyle.secondary}
               dataAttribute="dapp-header-funding"
-              onClick={() => setIsFastBtcDialogOpen(true)}
+              onClick={handleFastBtcClick}
             />
           )
         }
-      />
-      <FastBtcDialog
-        isOpen={isFastBtcDialogOpen}
-        onClose={() => setIsFastBtcDialogOpen(false)}
       />
     </>
   );

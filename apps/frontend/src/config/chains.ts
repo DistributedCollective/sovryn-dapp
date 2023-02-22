6bd1da022b729c0407048a5483431571a74dcb7e
tiltom <tilnak.tomas@gmail.com>
2023-02-22 16:32:50 +0100

    Fix chains configuration + small review fixes

--- apps/frontend/src/config/chains.ts
+++ apps/frontend/src/config/chains.ts
@@ -1,17 +1,20 @@
 import setup, { Chain, ChainIds } from '@sovryn/ethers-provider';
 
+import { isMainnet } from '../utils/helpers';
+
 export enum Chains {
   RSK = 'rsk',
   BSC = 'bsc',
 }
 
-export const defaultChainId =
-  process.env.REACT_APP_CHAIN_ID || ChainIds.RSK_TESTNET;
+export const defaultChainId = (
+  isMainnet() ? ChainIds.RSK_MAINNET : ChainIds.RSK_TESTNET
+) as string;
 
 // @dev: temp solution for hardware wallets to connect to the correct chain
 // good enough for now, but should be refactored when cross-chain support is needed
 export const chains: Chain[] = [
-  defaultChainId === ChainIds.RSK_MAINNET
+  isMainnet()
     ? {
         id: ChainIds.RSK_MAINNET,
         label: 'Rootstock',

fb87d05f4a9e5507d6c48d689493e543b3fcc0f7
Pietro Maximoff <pietro.maxim.official@protonmail.com>
2023-03-02 17:34:56 +0100

    chore: replace to toLowerCase

--- apps/frontend/src/app/3_organisms/LOCChart/hooks/useGetUserOpenTrove.ts
+++ apps/frontend/src/app/3_organisms/LOCChart/hooks/useGetUserOpenTrove.ts
@@ -6,7 +6,7 @@ export const useGetUserOpenTrove = () => {
   const { account } = useAccount();
 
   const { data, loading } = useGetUserOpenTroveQuery({
-    variables: { user: account.toLocaleLowerCase() },
+    variables: { user: account.toLowerCase() },
     client: zeroClient,
   });
 

6e2e6a080dd3fa6b1c27005c6e90e5fc5acff747
soulBit <its.soulBit@gmail.com>
2023-03-04 23:54:14 +0000

    feat: mainnet DLLR/MYNT contract addresses

--- packages/contracts/src/contracts/tokens/rsk.ts
+++ packages/contracts/src/contracts/tokens/rsk.ts
@@ -6,8 +6,8 @@ export const rsk: Record<string, string> = {
   rbtc: constants.AddressZero,
   sov: '0xEFc78fc7d48b64958315949279Ba181c2114ABBd',
   xusd: '0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F',
-  dllr: '0x007b3AA69A846cB1f76b60b3088230A52D2A83AC', //TODO replace with new address
-  mynt: '0xac2d05A148aB512EDEDc7280c00292ED33d31f1A', //TODO replace with new address
+  dllr: '0xc1411567d2670e24d9C4DaAa7CdA95686e1250AA',
+  mynt: '0x5F777270259E32F79589fe82269DB6209F7b7582',
   doc: '0xE700691Da7B9851F2F35f8b8182C69C53ccad9DB',
   babelfish: '0x1440d19436bEeaF8517896bffB957a88EC95a00F',
   zusd: addresses.zusdToken,

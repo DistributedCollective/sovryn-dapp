5b20547585b6666d5e951fde786f1b3d8c0a5d67
soulBit <its.soulBit@gmail.com>
2023-03-13 23:49:12 +0000

    feat: add 404 page, update error page localisations

--- apps/frontend/src/app/5_pages/ErrorPage/ErrorPage.tsx
+++ apps/frontend/src/app/5_pages/ErrorPage/ErrorPage.tsx
@@ -1,16 +1,26 @@
 import React, { FC } from 'react';
 
 import { t } from 'i18next';
+import { Helmet } from 'react-helmet-async';
 import { useRouteError } from 'react-router-dom';
 
 import { Footer, Header } from '../../3_organisms';
 import { translations } from '../../../locales/i18n';
+import { NotFoundPage } from '../NotFoundPage/NotFoundPage';
 
 export const ErrorPage: FC = () => {
-  const error = useRouteError() as Error & { statusText?: string };
+  const error = useRouteError() as Error & {
+    statusText?: string;
+    status?: number;
+  };
 
-  return (
+  return error?.status === 404 ? (
+    <NotFoundPage />
+  ) : (
     <>
+      <Helmet>
+        <title>{t(translations.errorPage.meta.title)}</title>
+      </Helmet>
       <Header />
       <div className="container flex flex-col flex-grow items-center justify-center">
         <h1>{t(translations.errorPage.title)}</h1>

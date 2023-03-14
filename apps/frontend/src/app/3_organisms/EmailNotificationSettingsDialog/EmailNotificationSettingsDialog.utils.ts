ef40b118d7289a765305294fa944c4a813d4ede3
soulBit <its.soulBit@gmail.com>
2023-03-14 12:55:52 +0000

    chore: update comment

--- apps/frontend/src/app/3_organisms/EmailNotificationSettingsDialog/EmailNotificationSettingsDialog.utils.ts
+++ apps/frontend/src/app/3_organisms/EmailNotificationSettingsDialog/EmailNotificationSettingsDialog.utils.ts
@@ -9,7 +9,7 @@ export const isSubscribedToGroup = (
   subscriptions: Notification[],
 ) => {
   if (subscriptions.length === 0) {
-    // default value for notification settings
+    // default value for notification settings - see SOV-1762 for more details
     return true;
   }
 

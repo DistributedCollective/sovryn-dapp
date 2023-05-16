521640bd530d7a8132995917df4980d02fc4ec32
Peiman <25097709+Rickk137@users.noreply.github.com>
2023-05-16 06:33:48 -0700

    SOV-2234: LOC max debt and collateral loading (#373)
    * fix: LOC max debt and collateral loading
    
    * fix: resolve PR comments
    
    * fix: revert usage error
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: soulBit <its.soulBit@gmail.com>

--- apps/frontend/src/app/3_organisms/ZeroLocForm/components/OpenCreditLine.tsx
+++ apps/frontend/src/app/3_organisms/ZeroLocForm/components/OpenCreditLine.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState, FC, useEffect } from 'react';
+import React, { useCallback, useMemo, useState, FC } from 'react';
 
 import { t } from 'i18next';
 
@@ -7,7 +7,6 @@ import { ErrorLevel } from '@sovryn/ui';
 import { Decimal } from '@sovryn/utils';
 
 import { BORROW_ASSETS } from '../../../5_pages/ZeroPage/constants';
-import { useLiquityBaseParams } from '../../../5_pages/ZeroPage/hooks/useLiquityBaseParams';
 import {
   BITCOIN,
   BTC_RENDER_PRECISION,
@@ -42,7 +41,6 @@ export const OpenCreditLine: FC<OpenCreditLineProps> = ({
   rbtcPrice,
   borrowingRate,
 }) => {
-  const { maxBorrowingFeeRate } = useLiquityBaseParams();
   const {
     tcr,
     liquidationReserve,
@@ -216,12 +214,6 @@ export const OpenCreditLine: FC<OpenCreditLineProps> = ({
       } as CreditLineSubmitValue),
     [collateralAmount, debtAmount, debtToken, maxOriginationFeeRate, onSubmit],
   );
-
-  useEffect(() => {
-    if (maxBorrowingFeeRate) {
-      setMaxOriginationFeeRate(maxBorrowingFeeRate.mul(100).toString());
-    }
-  }, [maxBorrowingFeeRate]);
 
   return (
     <FormContent

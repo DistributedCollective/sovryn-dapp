f268ed6c41d840c1b2ec310ab455899f97bcd6c0
Pietro Maximoff <74987028+pietro-maximoff@users.noreply.github.com>
2023-05-16 15:30:44 +0200

    SOV-2294: Pull Zero fee rates directly from contracts - reimplementation (#372)
    * Revert "SOV-2294: revert pulling fees from SC (#355)"
    
    This reverts commit e1cb996fbe9946742d80195261f3bce71b9cfce5.
    
    * added console.log
    
    * cleanups
    
    * chore: updates from comments
    
    * chore: update types
    
    * chore: update origination fee in history table

--- apps/frontend/src/app/5_pages/ZeroPage/utils/trove-manager.ts
+++ apps/frontend/src/app/5_pages/ZeroPage/utils/trove-manager.ts
@@ -9,14 +9,12 @@ import {
 } from '@sovryn-zero/lib-base';
 import { SupportedTokens } from '@sovryn/contracts';
 
-import { decimalic } from '../../../../utils/math';
 import { getLiquityBaseParams } from './';
 import { getZeroProvider } from './zero-provider';
 
 export const openTrove = async (
   token: SupportedTokens,
   params: Partial<TroveCreationParams<Decimalish>>,
-  maxBorrowingRate?: string,
 ) => {
   const normalized = _normalizeTroveCreation(params);
   const { depositCollateral, borrowZUSD } = normalized;
@@ -33,14 +31,10 @@ export const openTrove = async (
 
   const hints = await ethers.populate.findHints(newTrove);
 
-  const borrowingRate = maxBorrowingRate
-    ? decimalic(maxBorrowingRate).div(100).toHexString()
-    : fees?.maxBorrowingFeeRate.toHexString();
-
   return {
     value: value.hex,
     fn: token === SupportedTokens.dllr ? 'openNueTrove' : 'openTrove',
-    args: [borrowingRate, borrowZUSD.hex, ...hints],
+    args: [fees?.maxBorrowingFeeRate.toHexString(), borrowZUSD.hex, ...hints],
   };
 };
 
@@ -48,7 +42,6 @@ export const adjustTrove = async (
   token: SupportedTokens,
   address: string,
   params: Partial<TroveAdjustmentParams<Decimalish>>,
-  maxBorrowingRate?: string,
 ) => {
   const normalized = _normalizeTroveAdjustment(params);
   const { depositCollateral, withdrawCollateral, borrowZUSD, repayZUSD } =
@@ -70,15 +63,11 @@ export const adjustTrove = async (
 
   const hints = await ethers.populate.findHints(finalTrove);
 
-  const borrowingRate = maxBorrowingRate
-    ? decimalic(maxBorrowingRate).div(100).toHexString()
-    : fees?.maxBorrowingFeeRate?.toHexString() ?? Decimal.ZERO.hex;
-
   return {
     value: value.hex,
     fn: token === SupportedTokens.dllr ? 'adjustNueTrove' : 'adjustTrove',
     args: [
-      borrowingRate,
+      fees?.maxBorrowingFeeRate.toHexString() ?? Decimal.ZERO.hex,
       (withdrawCollateral ?? Decimal.ZERO).hex,
       (borrowZUSD ?? repayZUSD ?? Decimal.ZERO).hex,
       !!borrowZUSD,

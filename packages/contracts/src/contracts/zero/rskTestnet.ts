9074c334aaf4f307f029a59a3ea1221620a55c01
Peiman <peymannaderi3@yahoo.com>
2023-05-31 09:52:45 +0300

    feat: Pull Zero contract details from @sovryn-zero/contracts package

--- packages/contracts/src/contracts/zero/rskTestnet.ts
+++ packages/contracts/src/contracts/zero/rskTestnet.ts
@@ -6,31 +6,46 @@ export const rskTestnet: Record<string, AsyncContractConfigData> = {
   borrowerOperations: {
     address: addresses.borrowerOperations,
     getAbi: async () =>
-      (await import('@sovryn-zero/lib-ethers/dist/abi/BorrowerOperations.json'))
-        .default,
+      (
+        await import(
+          '@sovryn-zero/contracts/deployment/deployments/rskSovrynTestnet/BorrowerOperations.json'
+        )
+      ).default,
   },
   troveManager: {
     address: addresses.troveManager,
     getAbi: async () =>
-      (await import('@sovryn-zero/lib-ethers/dist/abi/TroveManager.json'))
-        .default,
+      (
+        await import(
+          '@sovryn-zero/contracts/deployment/deployments/rskSovrynTestnet/TroveManager.json'
+        )
+      ).default,
   },
   stabilityPool: {
     address: addresses.stabilityPool,
     getAbi: async () =>
-      (await import('@sovryn-zero/lib-ethers/dist/abi/StabilityPool.json'))
-        .default,
+      (
+        await import(
+          '@sovryn-zero/contracts/deployment/deployments/rskSovrynTestnet/StabilityPool.json'
+        )
+      ).default,
   },
   liquityBaseParams: {
     address: addresses.liquityBaseParams,
     getAbi: async () =>
-      (await import('@sovryn-zero/lib-ethers/dist/abi/LiquityBaseParams.json'))
-        .default,
+      (
+        await import(
+          '@sovryn-zero/contracts/deployment/deployments/rskSovrynTestnet/LiquityBaseParams.json'
+        )
+      ).default,
   },
   communityIssuance: {
     address: addresses.communityIssuance,
     getAbi: async () =>
-      (await import('@sovryn-zero/lib-ethers/dist/abi/CommunityIssuance.json'))
-        .default,
+      (
+        await import(
+          '@sovryn-zero/contracts/deployment/deployments/rskSovrynTestnet/CommunityIssuance.json'
+        )
+      ).default,
   },
 };

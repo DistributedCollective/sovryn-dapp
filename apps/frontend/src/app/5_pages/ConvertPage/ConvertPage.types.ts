38a1aef7f5298aa67bf72093d7b245da05fd06a4
soulBit <its.soulBit@gmail.com>
2023-06-30 16:01:48 +0100

    chore: disable all routes except MoC

--- apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
@@ -1,10 +1,13 @@
 import { SupportedTokens } from '@sovryn/contracts';
 import { getProvider } from '@sovryn/ethers-provider';
-import { DEFAULT_SWAP_ROUTES, SmartRouter } from '@sovryn/sdk';
+import { SmartRouter, smartRoutes } from '@sovryn/sdk';
 
 const provider = getProvider();
 
-export const smartRouter = new SmartRouter(provider, DEFAULT_SWAP_ROUTES);
+//export const smartRouter = new SmartRouter(provider, DEFAULT_SWAP_ROUTES);
+export const smartRouter = new SmartRouter(provider, [
+  smartRoutes.mocIntegrationSwapRoute,
+]);
 
 export const stableCoins = [
   SupportedTokens.zusd,

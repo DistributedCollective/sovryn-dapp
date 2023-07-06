7d2a1b3966401392a4b3898f3a4b55786949b68f
soulBit <its.soulBit@gmail.com>
2023-07-06 13:12:41 +0100

    chore: remove zero swap route

--- packages/sdk/src/swaps/smart-router/config.ts
+++ packages/sdk/src/swaps/smart-router/config.ts
@@ -1,10 +1,8 @@
 import { ammSwapRoute } from './routes/amm-swap-route';
 import { myntBassetRoute } from './routes/mynt-basset-route';
-import { zeroRedemptionSwapRoute } from './routes/zero-redemption-route';
 import { SwapRouteFunction } from './types';
 
 export const DEFAULT_SWAP_ROUTES: SwapRouteFunction[] = [
   ammSwapRoute,
   myntBassetRoute,
-  zeroRedemptionSwapRoute,
 ];

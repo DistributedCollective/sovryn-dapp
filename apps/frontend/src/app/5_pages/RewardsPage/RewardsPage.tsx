e7382c76d2503811058e0768276bbea988a8e53a
Peiman <peymannaderi3@yahoo.com>
2023-07-17 16:04:38 +0330

    feat: New Rewards page maintenance mode states

--- apps/frontend/src/app/5_pages/RewardsPage/RewardsPage.tsx
+++ apps/frontend/src/app/5_pages/RewardsPage/RewardsPage.tsx
@@ -1,10 +1,19 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 
 import { t } from 'i18next';
 import { Helmet } from 'react-helmet-async';
 
-import { SelectOption, Select, Tabs, Heading, HeadingType } from '@sovryn/ui';
+import {
+  SelectOption,
+  Select,
+  Tabs,
+  Heading,
+  HeadingType,
+  ErrorBadge,
+  ErrorLevel,
+} from '@sovryn/ui';
 
+import { useMaintenance } from '../../../hooks/useMaintenance';
 import { translations } from '../../../locales/i18n';
 import styles from './RewardsPage.module.css';
 import { StabilityPool } from './components/StabilityPool/StabilityPool';
@@ -14,46 +23,65 @@ import { Vesting } from './components/Vesting/Vesting';
 
 const ACTIVE_CLASSNAME = 'border-t-primary-30';
 
-const tabItems = [
-  {
-    label: t(translations.rewardPage.tabs.titles.stabilityPool),
-    content: (
-      <div className="px-0 py-4 lg:p-4">
-        <StabilityPool />
-      </div>
-    ),
-    activeClassName: ACTIVE_CLASSNAME,
-    dataAttribute: 'stabilityPoolRewards',
-  },
-  {
-    label: t(translations.rewardPage.tabs.titles.staking),
-    content: (
-      <div className="px-0 py-4 lg:p-4">
-        <Staking />
-      </div>
-    ),
-    activeClassName: ACTIVE_CLASSNAME,
-    dataAttribute: 'stakingRewards',
-  },
-  {
-    label: t(translations.rewardPage.tabs.titles.vesting),
-    content: (
-      <div className="px-0 py-4 lg:p-4">
-        <Vesting />
-      </div>
-    ),
-    activeClassName: ACTIVE_CLASSNAME,
-    dataAttribute: 'vestingRewards',
-  },
-];
-
-const mobileSelectOptions: SelectOption[] = tabItems.map((item, index) => ({
-  value: String(index),
-  label: item.label,
-}));
-
 const RewardsPage: FC = () => {
+  const { checkMaintenance, States } = useMaintenance();
+  const rewardsLocked = checkMaintenance(States.REWARDS_FULL);
+  const stabilityLocked = checkMaintenance(States.ZERO_STABILITY_CLAIM);
+  const stakingLocked = checkMaintenance(States.REWARDS_STAKING);
   const [index, setIndex] = useState(0);
+
+  const items = useMemo(() => {
+    return [
+      {
+        label: t(translations.rewardPage.tabs.titles.stabilityPool),
+        content: (
+          <div className="px-0 py-4 lg:p-4">
+            <StabilityPool />
+          </div>
+        ),
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'stabilityPoolRewards',
+        disabled: stabilityLocked,
+      },
+      {
+        label: t(translations.rewardPage.tabs.titles.staking),
+        content: (
+          <div className="px-0 py-4 lg:p-4">
+            <Staking />
+          </div>
+        ),
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'stakingRewards',
+        disabled: stakingLocked,
+      },
+      {
+        label: t(translations.rewardPage.tabs.titles.vesting),
+        content: (
+          <div className="px-0 py-4 lg:p-4">
+            <Vesting />
+          </div>
+        ),
+        activeClassName: ACTIVE_CLASSNAME,
+        dataAttribute: 'vestingRewards',
+        disabled: false,
+      },
+    ];
+  }, [stabilityLocked, stakingLocked]);
+
+  const mobileSelectOptions: SelectOption[] = useMemo(
+    () =>
+      items.map((item, index) => ({
+        value: String(index),
+        label: item.label,
+      })),
+    [items],
+  );
+
+  useEffect(() => {
+    if (items[index].disabled) {
+      setIndex(items.findIndex(item => !item.disabled));
+    }
+  }, [items, index]);
 
   return (
     <>
@@ -74,23 +102,32 @@ const RewardsPage: FC = () => {
             {t(translations.rewardPage.tabs.title)}
           </Heading>
 
-          <div className={styles.mobileSelect}>
-            <Select
-              className="min-w-[12rem]"
-              options={mobileSelectOptions}
-              value={String(index)}
-              onChange={value => setIndex(Number(value))}
+          {!rewardsLocked ? (
+            <>
+              <div className={styles.mobileSelect}>
+                <Select
+                  className="min-w-[12rem]"
+                  options={mobileSelectOptions}
+                  value={String(index)}
+                  onChange={value => setIndex(Number(value))}
+                />
+              </div>
+              <div className={styles.desktop}>
+                <Tabs
+                  items={items}
+                  onChange={setIndex}
+                  index={index}
+                  className="w-full"
+                />
+              </div>
+              <div className={styles.mobile}>{items[index].content}</div>
+            </>
+          ) : (
+            <ErrorBadge
+              level={ErrorLevel.Warning}
+              message={t(translations.maintenanceMode.featureDisabled)}
             />
-          </div>
-          <div className={styles.desktop}>
-            <Tabs
-              items={tabItems}
-              onChange={setIndex}
-              index={index}
-              className="w-full"
-            />
-          </div>
-          <div className={styles.mobile}>{tabItems[index].content}</div>
+          )}
         </div>
       </div>
     </>

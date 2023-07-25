b00b491a8a1be3f166bd016d874f54c6dedd879a
tiltom <tiltom.defi@protonmail.com>
2023-07-25 10:12:56 +0200

    SOV-2613: Vesting rewards (#436)
    * Create short-spoons-report.md
    
    * Display vesting contracts data
    
    * WIP: Calculate unlocked balance
    
    * Fix unlocked balance calculation
    
    * Add withdraw functionality
    
    * Vesting schedule initial implementation
    
    * Fix generated.tsx
    
    * Calculate unlocked balance from graph
    
    * Table design adjustments
    
    * Vesting schedule adjustments
    
    * Add pagination to the vestings table
    
    * Code cleanup
    
    * Disable withdraw

---
+++ apps/frontend/src/app/5_pages/RewardsPage/components/Vesting/Vestings.utils.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { t } from 'i18next';
+
+import { Paragraph, ParagraphSize, TransactionId } from '@sovryn/ui';
+
+import { AmountRenderer } from '../../../../2_molecules/AmountRenderer/AmountRenderer';
+import {
+  SOV,
+  TOKEN_RENDER_PRECISION,
+} from '../../../../../constants/currencies';
+import { translations } from '../../../../../locales/i18n';
+import { VestingContractType } from '../../../../../utils/graphql/rsk/generated';
+import { getRskExplorerUrl } from '../../../../../utils/helpers';
+import { VestingContractTableRecord } from './Vesting.types';
+
+const rskExplorerUrl = getRskExplorerUrl();
+
+export const renderBalance = (balance: string | undefined) => {
+  if (!balance || balance === '') {
+    return '-';
+  }
+
+  return (
+    <AmountRenderer
+      value={balance}
+      suffix={SOV}
+      precision={TOKEN_RENDER_PRECISION}
+      dataAttribute="vesting-rewards-current-balance"
+    />
+  );
+};
+
+export const renderContractAddress = (item: VestingContractTableRecord) => (
+  <TransactionId
+    href={`${rskExplorerUrl}/address/${item.address}`}
+    value={item.address}
+    dataAttribute="vesting-rewards-address"
+  />
+);
+
+export const vestingTypeToTitleMapping = (item: VestingContractType) => {
+  switch (item) {
+    case VestingContractType.Team:
+      return t(translations.rewardPage.vesting.vestingTypeTitles.team);
+    case VestingContractType.Rewards:
+      return t(
+        translations.rewardPage.vesting.vestingTypeTitles.liquidityMining,
+      );
+    case VestingContractType.FourYearVesting:
+      return t(translations.rewardPage.vesting.vestingTypeTitles.investor);
+    default:
+      return item;
+  }
+};
+
+export const generateRowTitle = (item: VestingContractTableRecord) => (
+  <div className="flex flex-col items-start gap-1 font-medium">
+    <Paragraph size={ParagraphSize.small} className="text-left text-gray-40">
+      {item.type}
+    </Paragraph>
+    {renderBalance(item.currentBalance)}
+  </div>
+);
---
+++ apps/frontend/src/app/5_pages/RewardsPage/components/Vesting/hooks/useGetUnlockSchedule.ts
@@ -0,0 +1,59 @@
+import { useMemo } from 'react';
+
+import dayjs from 'dayjs';
+
+import { rskClient } from '../../../../../../utils/clients';
+import { useGetVestingHistoryQuery } from '../../../../../../utils/graphql/rsk/generated';
+import {
+  VestingContractTableRecord,
+  VestingHistoryItem,
+} from '../Vesting.types';
+
+const MAXIMUM_UNLOCKED_DATES = 2;
+
+export const useGetUnlockSchedule = (
+  item: VestingContractTableRecord,
+): VestingHistoryItem[] | undefined => {
+  const { data } = useGetVestingHistoryQuery({
+    variables: { vestingAddress: item.address },
+    client: rskClient,
+  });
+
+  const result = useMemo(() => {
+    const stakeHistoryItems = data?.vestingContracts[0]?.stakeHistory?.map(
+      item => ({
+        amount: item.amount,
+        lockedUntil: item.lockedUntil,
+      }),
+    );
+
+    if (!stakeHistoryItems) {
+      return undefined;
+    }
+
+    const normalizedStakeHistoryItems = stakeHistoryItems.reduce((map, obj) => {
+      const { lockedUntil, amount } = obj;
+      map.set(lockedUntil, (map.get(lockedUntil) || 0) + Number(amount));
+      return map;
+    }, new Map());
+
+    const unlockDates = Array.from(
+      normalizedStakeHistoryItems,
+      ([lockedUntil, amount]) => ({
+        lockedUntil,
+        amount,
+        isUnlocked: lockedUntil <= dayjs().unix(),
+      }),
+    ).sort((a, b) => a.lockedUntil - b.lockedUntil);
+
+    const pastDatesLength = unlockDates?.filter(item => item.isUnlocked).length;
+
+    if (!pastDatesLength || pastDatesLength < MAXIMUM_UNLOCKED_DATES) {
+      return unlockDates;
+    }
+
+    return unlockDates.slice(pastDatesLength - MAXIMUM_UNLOCKED_DATES);
+  }, [data?.vestingContracts]);
+
+  return result;
+};

f5287d95f18b5299a01d2c83b26a353637f77d80
tiltom <tilnak.tomas@gmail.com>
2023-07-26 11:13:21 +0200

    Resolve token symbol

--- apps/frontend/src/app/3_organisms/EarnHistoryFrame/components/LendingHistoryFrame/LendingHistoryFrame.constants.tsx
+++ apps/frontend/src/app/3_organisms/EarnHistoryFrame/components/LendingHistoryFrame/LendingHistoryFrame.constants.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import { t } from 'i18next';
 
-import { AmountRenderer } from '../../../../2_molecules/AmountRenderer/AmountRenderer';
 import { TransactionIdRenderer } from '../../../../2_molecules/TransactionIdRenderer/TransactionIdRenderer';
 import { translations } from '../../../../../locales/i18n';
-import { LendingHistoryType } from '../../../../../utils/graphql/rsk/generated';
 import { dateFormat } from '../../../../../utils/helpers';
-import { decimalic } from '../../../../../utils/math';
 import { LendingEvent } from './LendingHistoryFrame.types';
 import { getTransactionType } from './LendingHistoryFrame.utils';
+import { BalanceChange } from './components/BalanceChange';
 
 export const COLUMNS_CONFIG = [
   {
@@ -26,17 +24,7 @@ export const COLUMNS_CONFIG = [
   {
     id: 'balanceChange',
     title: t(translations.earnHistory.lendingHistory.columns.balanceChange),
-    cellRenderer: (item: LendingEvent) => (
-      <>
-        <AmountRenderer
-          value={
-            item.type === LendingHistoryType.UnLend
-              ? decimalic(item.amount).mul(-1)
-              : decimalic(item.amount)
-          }
-        />
-      </>
-    ),
+    cellRenderer: (item: LendingEvent) => <BalanceChange item={item} />,
   },
   {
     id: 'transactionId',

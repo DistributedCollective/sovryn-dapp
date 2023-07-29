1c7ca829e37591df2ffa6b08e4fdd384fbc443ed
tiltom <tilnak.tomas@gmail.com>
2023-07-29 16:14:14 +0200

    Borrow assets table initial implementation

--- apps/frontend/src/app/5_pages/BorrowPage/BorrowPage.tsx
+++ apps/frontend/src/app/5_pages/BorrowPage/BorrowPage.tsx
@@ -3,7 +3,10 @@ import React, { FC } from 'react';
 import { t } from 'i18next';
 import { Helmet } from 'react-helmet-async';
 
+import { Heading, Paragraph, ParagraphSize } from '@sovryn/ui';
+
 import { translations } from '../../../locales/i18n';
+import { BorrowAssetsTable } from './components/BorrowAssetsTable/BorrowAssetsTable';
 
 const BorrowPage: FC = () => {
   return (
@@ -12,7 +15,22 @@ const BorrowPage: FC = () => {
         <title>{t(translations.fixedInterestPage.meta.title)}</title>
       </Helmet>
 
-      <div>Borrow page</div>
+      <div className="px-0 container md:mx-9 mx-0 md:mb-2 mt-4 mb-7">
+        <Heading className="text-center mb-3 lg:text-2xl">
+          {t(translations.fixedInterestPage.title)}
+        </Heading>
+
+        <Paragraph
+          className="text-center mb-6 lg:mb-10"
+          size={ParagraphSize.base}
+        >
+          {t(translations.fixedInterestPage.subtitle)}
+        </Paragraph>
+
+        <div className="w-full md:bg-gray-90 md:py-7 md:px-6 rounded mb-6">
+          <BorrowAssetsTable />
+        </div>
+      </div>
     </>
   );
 };
---
+++ apps/frontend/src/app/5_pages/BorrowPage/components/NewLoanButton/NewLoanButton.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useState } from 'react';
+
+import { t } from 'i18next';
+
+import { Button, ButtonStyle } from '@sovryn/ui';
+
+import { useAccount } from '../../../../../hooks/useAccount';
+import { translations } from '../../../../../locales/i18n';
+import { NewLoanDialog } from '../NewLoanDialog/NewLoanDialog';
+
+export const NewLoanButton: FC = () => {
+  const { account } = useAccount();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        text={t(translations.fixedInterestPage.borrowAssetsTable.action)}
+        style={ButtonStyle.primary}
+        onClick={() => setIsOpen(true)}
+        disabled={!account}
+      />
+
+      <NewLoanDialog isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
+  );
+};

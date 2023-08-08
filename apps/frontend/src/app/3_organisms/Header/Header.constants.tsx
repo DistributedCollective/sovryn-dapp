2d4fb36a88b7a79d6e51d58c5942c4cc8e7ae8f8
Victor Creed <victor@sovryn.app>
2023-08-08 10:43:11 +0300

    feat: add dropdown earn menu

---
+++ apps/frontend/src/app/3_organisms/Header/Header.constants.tsx
@@ -0,0 +1,30 @@
+import { t } from 'i18next';
+
+import { translations } from '../../../locales/i18n';
+
+export const menuItemsMapping = [
+  {
+    text: t(translations.header.nav.borrow),
+    url: '/',
+  },
+  {
+    text: t(translations.header.nav.earn.title),
+    url: '/earn',
+    submenu: [
+      {
+        text: t(translations.header.nav.earn.subMenu.stabilityPool),
+        label: t(translations.header.nav.earn.subMenu.stabilityPoolDescription),
+        url: '/earn/stability-pool',
+      },
+      {
+        text: t(translations.header.nav.earn.subMenu.staking),
+        label: t(translations.header.nav.earn.subMenu.stakingDescription),
+        url: '/earn/staking',
+      },
+    ],
+  },
+  {
+    text: t(translations.header.nav.convert),
+    url: '/convert',
+  },
+];

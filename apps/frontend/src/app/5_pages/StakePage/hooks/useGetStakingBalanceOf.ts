86cb919e950ecb4e4d0097b8dc4d2de34280b144
Peiman <peymannaderi3@yahoo.com>
2023-08-09 14:01:30 +0330

    resolve PR issue

--- apps/frontend/src/app/5_pages/StakePage/hooks/useGetStakingBalanceOf.ts
+++ apps/frontend/src/app/5_pages/StakePage/hooks/useGetStakingBalanceOf.ts
@@ -6,11 +6,8 @@ export const useGetStakingBalanceOf = (address: string) => {
 
   const { value: balance } = useCacheCall(
     `staking/${stakingContract?.address}/${address}/balanceOf`,
-    async () => {
-      return address && stakingContract
-        ? stakingContract.balanceOf(address)
-        : '0';
-    },
+    async () =>
+      address && stakingContract ? stakingContract.balanceOf(address) : '0',
     [address],
     '0',
   );

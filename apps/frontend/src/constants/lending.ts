2b45ebfc95018f8f47055a55489202e53283d50d
tiltom <tilnak.tomas@gmail.com>
2023-08-09 15:16:29 +0200

    Fix min collateral ratio for lending pools

---
+++ apps/frontend/src/constants/lending.ts
@@ -0,0 +1,5 @@
+import { decimalic } from '../utils/math';
+
+// TODO: This will be read from the subgraph/smart contracts in the future
+export const MINIMUM_COLLATERAL_RATIO_LENDING_POOLS = decimalic(1.5); // 150%
+export const MINIMUM_COLLATERAL_RATIO_LENDING_POOLS_SOV = decimalic(3); // 300%

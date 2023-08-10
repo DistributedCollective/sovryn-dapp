35a28aeb3295e3b62780afd79b2cd408e136b3c5
tiltom <tilnak.tomas@gmail.com>
2023-08-10 15:42:09 +0200

    Adjust naming

--- apps/frontend/src/app/5_pages/BorrowPage/components/BorrowAssetsTable/BorrowAssetsTable.constants.tsx
+++ apps/frontend/src/app/5_pages/BorrowPage/components/BorrowAssetsTable/BorrowAssetsTable.constants.tsx
@@ -5,7 +5,7 @@ import { t } from 'i18next';
 import { HelperButton } from '@sovryn/ui';
 
 import { AssetRenderer } from '../../../../2_molecules/AssetRenderer/AssetRenderer';
-import { MINIMUM_COLLATERAL_RATIO_BORROWING } from '../../../../../constants/lending';
+import { MINIMUM_COLLATERAL_RATIO_BORROWING_MAINTENANCE } from '../../../../../constants/lending';
 import { translations } from '../../../../../locales/i18n';
 import { LendingPool } from '../../../../../utils/LendingPool';
 import { AcceptedCollateral } from '../../../LendPage/components/AcceptedCollateral/AcceptedCollateral';
@@ -62,7 +62,9 @@ export const COLUMNS_CONFIG = [
       </span>
     ),
     cellRenderer: () => (
-      <div>{MINIMUM_COLLATERAL_RATIO_BORROWING.mul(100).toString()}%</div>
+      <div>
+        {MINIMUM_COLLATERAL_RATIO_BORROWING_MAINTENANCE.mul(100).toString()}%
+      </div>
     ),
   },
   {
--- apps/frontend/src/constants/lending.ts
+++ apps/frontend/src/constants/lending.ts
@@ -3,4 +3,4 @@ import { decimalic } from '../utils/math';
 // TODO: This will be read from the subgraph/smart contracts in the future
 export const MINIMUM_COLLATERAL_RATIO_LENDING_POOLS = decimalic(1.5); // 150%
 export const MINIMUM_COLLATERAL_RATIO_LENDING_POOLS_SOV = decimalic(3); // 300%
-export const MINIMUM_COLLATERAL_RATIO_BORROWING = decimalic(1.15); // 115%
+export const MINIMUM_COLLATERAL_RATIO_BORROWING_MAINTENANCE = decimalic(1.15); // 115%

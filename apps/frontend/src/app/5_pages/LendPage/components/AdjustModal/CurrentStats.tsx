1451bc9b2e286fd55b400bea0c5ae9488338d363
soulBit <its.soulBit@gmail.com>
2023-08-11 13:17:07 +0100

    feat: add asset display

--- apps/frontend/src/app/5_pages/LendPage/components/AdjustModal/CurrentStats.tsx
+++ apps/frontend/src/app/5_pages/LendPage/components/AdjustModal/CurrentStats.tsx
@@ -3,11 +3,14 @@ import React, { FC } from 'react';
 import { t } from 'i18next';
 
 import { SupportedTokens } from '@sovryn/contracts';
+import { Paragraph } from '@sovryn/ui';
 import { Decimal } from '@sovryn/utils';
 
 import { AmountRenderer } from '../../../../2_molecules/AmountRenderer/AmountRenderer';
+import { AssetRenderer } from '../../../../2_molecules/AssetRenderer/AssetRenderer';
 import { getTokenDisplayName } from '../../../../../constants/tokens';
 import { translations } from '../../../../../locales/i18n';
+import styles from './CurrentStats.module.css';
 
 export type CurrentStatsProps = {
   apy: Decimal;
@@ -20,18 +23,25 @@ export const CurrentStats: FC<CurrentStatsProps> = ({
   balance,
   symbol,
 }) => (
-  <div className="flex flex-row justify-between mb-8">
-    <div>
-      <div>{t(translations.lendingAdjust.apy)}</div>
-      <div>
+  <>
+    <AssetRenderer
+      logoClassName={styles.assetLogo}
+      showAssetLogo
+      asset={symbol}
+    />
+    <div className="flex gap-8">
+      <div className="mt-6 flex flex-col gap-2">
+        <Paragraph className="font-medium text-gray-30">
+          {t(translations.lendingAdjust.apy)}
+        </Paragraph>
         <AmountRenderer value={apy} suffix="%" />
       </div>
-    </div>
-    <div>
-      <div>{t(translations.lendingAdjust.currentBalance)}</div>
-      <div>
+      <div className="mt-6 flex flex-col gap-2">
+        <Paragraph className="font-medium text-gray-30">
+          {t(translations.lendingAdjust.currentBalance)}
+        </Paragraph>
         <AmountRenderer value={balance} suffix={getTokenDisplayName(symbol)} />
       </div>
     </div>
-  </div>
+  </>
 );

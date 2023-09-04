45f865209202622e2893943220e57fb80a069125
Victor Creed <victor@sovryn.app>
2023-09-04 14:40:51 +0300

    chore: update lib-ethers package

--- apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
@@ -1,10 +1,13 @@
 import { SupportedTokens } from '@sovryn/contracts';
 import { getProvider } from '@sovryn/ethers-provider';
-import { DEFAULT_SWAP_ROUTES, SmartRouter } from '@sovryn/sdk';
+import { SmartRouter, smartRoutes } from '@sovryn/sdk';
 
 const provider = getProvider();
 
-export const smartRouter = new SmartRouter(provider, DEFAULT_SWAP_ROUTES);
+export const smartRouter = new SmartRouter(provider, [
+  smartRoutes.zeroRedemptionSwapRoute,
+]);
+// export const smartRouter = new SmartRouter(provider, DEFAULT_SWAP_ROUTES);
 
 export const stableCoins = [
   SupportedTokens.zusd,

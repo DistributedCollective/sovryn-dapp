7168585380a9c7b3537f13a400e707a91205654f
Peiman <peymannaderi3@yahoo.com>
2023-09-05 15:28:34 +0330

    feat: borrow extend loan modal

--- apps/frontend/src/app/5_pages/BorrowPage/components/OpenLoansTable/components/ExtendLoanButton/ExtendLoanButton.tsx
+++ apps/frontend/src/app/5_pages/BorrowPage/components/OpenLoansTable/components/ExtendLoanButton/ExtendLoanButton.tsx
@@ -1,24 +1,59 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useCallback, useReducer } from 'react';
 
 import { t } from 'i18next';
 
-import { Button, ButtonStyle } from '@sovryn/ui';
+import {
+  Button,
+  ButtonStyle,
+  Dialog,
+  DialogBody,
+  DialogHeader,
+  DialogSize,
+} from '@sovryn/ui';
 
+import { ExtendLoanForm } from '../../../../../../3_organisms/BorrowLoan/components/ExtendLoanForm/ExtendLoanForm';
+import { useAccount } from '../../../../../../../hooks/useAccount';
 import { translations } from '../../../../../../../locales/i18n';
-import { ExtendLoanDialog } from '../ExtendLoanDialog/ExtendLoanDialog';
+import { LoanItem } from '../../OpenLoansTable.types';
 
-export const ExtendLoanButton: FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+type ExtendLoanButtonProps = {
+  loan: LoanItem;
+};
 
+export const ExtendLoanButton: FC<ExtendLoanButtonProps> = ({ loan }) => {
+  const { account } = useAccount();
+  const [openExtendLoanDialog, toggleExtendLoanDialog] = useReducer(
+    v => !v,
+    false,
+  );
+
+  const onSuccess = useCallback(
+    () => toggleExtendLoanDialog(),
+    [toggleExtendLoanDialog],
+  );
   return (
     <>
       <Button
         text={t(translations.fixedInterestPage.openLoansTable.actions.extend)}
         style={ButtonStyle.secondary}
-        onClick={() => setIsOpen(true)}
+        onClick={toggleExtendLoanDialog}
+        disabled={!account}
       />
 
-      <ExtendLoanDialog isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <Dialog
+        isOpen={openExtendLoanDialog}
+        dataAttribute="extend-loan-dialog"
+        width={DialogSize.md}
+        disableFocusTrap
+      >
+        <DialogHeader
+          title={t(translations.fixedInterestPage.extendLoanDialog.title)}
+          onClose={toggleExtendLoanDialog}
+        />
+        <DialogBody
+          children={<ExtendLoanForm loan={loan} onSuccess={onSuccess} />}
+        />
+      </Dialog>
     </>
   );
 };

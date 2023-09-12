f88ff42b130f8d6f2d94c43018df0ad5f290f8e3
Pietro Maximoff <74987028+pietro-maximoff@users.noreply.github.com>
2023-09-12 15:34:47 +0200

    SOV-2754: Update token amount output component (#571)
    * SOV-2754: Update token amount output component
    
    * Create fair-jobs-agree.md
    
    * chore: optimization
    
    ---------
    
    Co-authored-by: soulBit <its.soulBit@gmail.com>

---
+++ .changeset/neat-kiwis-invent.md
@@ -0,0 +1,5 @@
+---
+"frontend": patch
+---
+
+SOV-2862: Fix delegate address display reset after extending Stake
---
+++ apps/frontend/src/app/2_molecules/AmountRenderer/AmountRenderer.utils.tsx
@@ -0,0 +1,18 @@
+import { Decimalish } from '@sovryn/utils';
+
+import { decimalic } from '../../../utils/math';
+
+export const isValueBetweenZeroAndOne = (value: number) =>
+  value > 0 && value < 1;
+
+export const calculateDecimalPlaces = (
+  value: Decimalish,
+  precision: number,
+) => {
+  const decimalValue = decimalic(value).toString();
+  const [, decimals = ''] = decimalValue.split('.');
+  const nonZeroIndex = decimals.search(/[^0]/);
+  return nonZeroIndex !== -1
+    ? Math.max(nonZeroIndex + 1, precision)
+    : precision;
+};

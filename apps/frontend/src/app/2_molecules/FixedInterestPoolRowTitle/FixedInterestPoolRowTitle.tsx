2f3a95d1b7a353362d4e15e960619765d92b1141
Victor Creed <victor@sovryn.app>
2023-09-13 12:02:46 +0300

    fix: mobile APR for borrow

--- apps/frontend/src/app/2_molecules/FixedInterestPoolRowTitle/FixedInterestPoolRowTitle.tsx
+++ apps/frontend/src/app/2_molecules/FixedInterestPoolRowTitle/FixedInterestPoolRowTitle.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 import { t } from 'i18next';
 
 import { AssetRenderer } from '../../2_molecules/AssetRenderer/AssetRenderer';
+import { NextBorrowInterestRate } from '../../5_pages/BorrowPage/components/BorrowAssetsTable/components/NextBorrowInterestRate/NextBorrowInterestRate';
 import { NextSupplyInterestRate } from '../../5_pages/LendPage/components/NextSupplyInterestRate/NextSupplyInterestRate';
 import { translations } from '../../../locales/i18n';
 import { LendingPool } from '../../../utils/LendingPool';
@@ -24,7 +25,11 @@ export const FixedInterestPoolRowTitle: FC<FixedInterestPoolRowTitleProps> = ({
       className="mr-1"
     />
     <div className="pl-1 flex items-center">
-      <NextSupplyInterestRate asset={pool.getAsset()} />
+      {isBorrow ? (
+        <NextBorrowInterestRate asset={pool.getAsset()} />
+      ) : (
+        <NextSupplyInterestRate asset={pool.getAsset()} />
+      )}
       <span className="text-gray-30 ml-1 font-medium">
         {isBorrow
           ? t(translations.fixedInterestPage.borrowAssetsTable.borrowAprMobile)

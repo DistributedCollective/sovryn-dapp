4045f522b15477900b4a2ed732e86b7ab72034b3
tiltom <tilnak.tomas@gmail.com>
2023-09-19 10:02:41 +0200

    Unpin all content prior to build upload

--- .github/workflows/pinata_unpin_content/index.js
+++ .github/workflows/pinata_unpin_content/index.js
@@ -1,15 +1,10 @@
 const core = require('@actions/core');
 const pinataSDK = require('@pinata/sdk');
 
-function getPinList(pinata, pinName, pinEnd) {
-  console.log(
-    `Calling API to get pin list with pinName ${pinName} before ${pinEnd}`,
-  );
+function getPinList(pinata) {
   return pinata.pinList({
     status: 'pinned',
     pageLimit: 1000,
-    metadata: { name: pinName },
-    pinEnd: pinEnd,
   });
 }
 
@@ -25,15 +20,10 @@ try {
   // inputs are defined in action metadata file
   const pinataKey = core.getInput('pinataKey');
   const pinataSecret = core.getInput('pinataSecret');
-  const pinName = core.getInput('pinName');
-  const pinEndTimestamp = core.getInput('pinEndTimestamp');
-  console.log('pinEndTimestamp');
-  console.log(pinEndTimestamp);
-  const pinEndDate = new Date(pinEndTimestamp * 1000);
-  const pinEnd = pinEndDate.toISOString();
+
   const pinata = new pinataSDK(pinataKey, pinataSecret);
-  console.log(`Pin Name: ${pinName}`);
-  getPinList(pinata, pinName, pinEnd).then(pins => {
+
+  getPinList(pinata).then(pins => {
     console.log(`Processing a total of ${pins.rows.length}`);
     unpinList(pinata, pins);
   });

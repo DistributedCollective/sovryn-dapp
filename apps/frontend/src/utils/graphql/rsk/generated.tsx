1f606bd5d74126144fee04ed2d1b62c197abb4e5
Peiman <peymannaderi3@yahoo.com>
2023-09-19 14:02:11 +0330

    feat: resolve PR comments

--- apps/frontend/src/utils/graphql/rsk/generated.tsx
+++ apps/frontend/src/utils/graphql/rsk/generated.tsx
@@ -15159,7 +15159,6 @@ export type GetProposalsQuery = {
     countVotersFor: number;
     countVotersAgainst: number;
     quorum: string;
-    proposer: string;
     majorityPercentage: string;
     eta?: number | null;
     proposalId: number;
@@ -16482,7 +16481,6 @@ export const GetProposalsDocument = gql`
       countVotersFor
       countVotersAgainst
       quorum
-      proposer
       majorityPercentage
       eta
       proposalId

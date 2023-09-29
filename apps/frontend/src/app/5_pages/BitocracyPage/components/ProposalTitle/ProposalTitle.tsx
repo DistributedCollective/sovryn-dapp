4f860923e26465b43d14224ce5326c38481c5e99
Peiman <peymannaderi3@yahoo.com>
2023-09-29 14:26:34 +0330

    fix: bitocracy proposal title

--- apps/frontend/src/app/5_pages/BitocracyPage/components/ProposalTitle/ProposalTitle.tsx
+++ apps/frontend/src/app/5_pages/BitocracyPage/components/ProposalTitle/ProposalTitle.tsx
@@ -1,19 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Tooltip, TooltipTrigger } from '@sovryn/ui';
 
 import { ProposalProps } from '../../BitocracyPage.types';
 
-export const ProposalTitle: FC<ProposalProps> = ({ proposal }) => (
-  <Tooltip
-    trigger={TooltipTrigger.click}
-    className="sm:text-base cursor-pointer"
-    tooltipClassName="break-words"
-    content={<>{proposal.description}</>}
-    children={
-      <div>
-        <p className="truncate max-w-[20rem] m-0">{proposal.description}</p>
-      </div>
+export const ProposalTitle: FC<ProposalProps> = ({ proposal }) => {
+  const title = useMemo(() => {
+    let title = proposal?.description.slice(0, 50);
+    if (proposal && proposal?.description.length > 50) {
+      title += '...';
     }
-  />
-);
+
+    return title;
+  }, [proposal]);
+
+  return (
+    <Tooltip
+      trigger={TooltipTrigger.click}
+      className="sm:text-base cursor-pointer"
+      tooltipClassName="break-words"
+      content={<>{proposal.description}</>}
+      children={
+        <div>
+          <p className="max-w-[20rem] m-0">{title}</p>
+        </div>
+      }
+    />
+  );
+};

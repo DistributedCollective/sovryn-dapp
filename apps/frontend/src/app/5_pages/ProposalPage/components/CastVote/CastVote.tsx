227d55a592becd3a6989780980fffe5e866a5daa
Peiman <peymannaderi3@yahoo.com>
2023-10-02 11:58:05 +0330

    fix: hide when loading

--- apps/frontend/src/app/5_pages/ProposalPage/components/CastVote/CastVote.tsx
+++ apps/frontend/src/app/5_pages/ProposalPage/components/CastVote/CastVote.tsx
@@ -43,7 +43,7 @@ export const CastVote: FC<CastVoteProps> = ({
   const { votingPower } = useGetPersonalStakingStatistics();
   const { account } = useAccount();
   const status = useProposalStatus(proposal);
-  const { vote } = useGetUserVote(proposal.id);
+  const { vote, loading } = useGetUserVote(proposal.id);
 
   const { submit } = useVote(proposal, refetch);
 
@@ -71,151 +71,153 @@ export const CastVote: FC<CastVoteProps> = ({
       <Heading className="text-sm font-medium">
         {t(pageTranslations.castVote)}
       </Heading>
-      <div className="mt-8">
-        {ineligibleVote && (
-          <div className="flex items-center justify-between">
+      {!loading && (
+        <div className="mt-8">
+          {ineligibleVote && (
+            <div className="flex items-center justify-between">
+              <Paragraph
+                size={ParagraphSize.base}
+                className="text-xs italic font-medium leading-relaxed"
+              >
+                {t(pageTranslations.ineligibleVote)}
+                <br />
+                {t(pageTranslations.eligibleVote)}
+              </Paragraph>
+
+              <Link to="/earn/staking" className="no-underline">
+                <Button
+                  style={ButtonStyle.secondary}
+                  size={ButtonSize.large}
+                  text={t(pageTranslations.stakeSOV)}
+                />
+              </Link>
+            </div>
+          )}
+
+          {!ineligibleVote && noVote && (
             <Paragraph
               size={ParagraphSize.base}
               className="text-xs italic font-medium leading-relaxed"
             >
-              {t(pageTranslations.ineligibleVote)}
-              <br />
-              {t(pageTranslations.eligibleVote)}
+              <Trans i18nKey={pageTranslations.noVote} />
             </Paragraph>
+          )}
 
-            <Link to="/earn/staking" className="no-underline">
+          {!account && (
+            <Paragraph
+              size={ParagraphSize.base}
+              className="text-xs italic font-medium leading-relaxed"
+            >
+              <Trans i18nKey={pageTranslations.connectMessage} />
+            </Paragraph>
+          )}
+          {!!vote && (
+            <div className="flex items-center gap-8">
+              <Tooltip
+                content={
+                  <span className="font-medium">
+                    {t(pageTranslations.supportText)}
+                  </span>
+                }
+                trigger={TooltipTrigger.click}
+                disabled={!vote.support}
+              >
+                <div>
+                  <Button
+                    className={classNames('w-52', {
+                      'bg-gray-50': !!vote.support,
+                      'hover:bg-gray-80 focus:bg-gray-80 cursor-default':
+                        !vote.support,
+                    })}
+                    style={ButtonStyle.secondary}
+                    size={ButtonSize.large}
+                    text={
+                      <span className="flex items-center">
+                        <Icon
+                          className="mr-2.5 text-positive"
+                          size={14}
+                          icon={IconNames.CHECK}
+                        />
+                        {t(pageTranslations.support)}
+                      </span>
+                    }
+                  />
+                </div>
+              </Tooltip>
+
+              <Tooltip
+                content={
+                  <span className="font-medium">
+                    {t(pageTranslations.rejectText)}
+                  </span>
+                }
+                trigger={TooltipTrigger.click}
+                disabled={!!vote.support}
+              >
+                <div>
+                  <Button
+                    className={classNames('w-52', {
+                      'bg-gray-50': !vote.support,
+                      'hover:bg-gray-80 focus:bg-gray-80 cursor-default':
+                        !!vote.support,
+                    })}
+                    style={ButtonStyle.secondary}
+                    size={ButtonSize.large}
+                    text={
+                      <span className="flex items-center">
+                        <Icon
+                          className="mr-2.5 text-negative"
+                          size={12}
+                          icon={IconNames.X_MARK}
+                        />
+                        {t(pageTranslations.reject)}
+                      </span>
+                    }
+                  />
+                </div>
+              </Tooltip>
+            </div>
+          )}
+
+          {!vote && status === ProposalState.Active && (
+            <div className="flex items-center gap-8">
               <Button
+                className="w-52"
                 style={ButtonStyle.secondary}
                 size={ButtonSize.large}
-                text={t(pageTranslations.stakeSOV)}
+                onClick={handleSupport}
+                text={
+                  <span className="flex items-center">
+                    <Icon
+                      className="mr-2.5 text-positive"
+                      size={14}
+                      icon={IconNames.CHECK}
+                    />
+                    {t(pageTranslations.support)}
+                  </span>
+                }
               />
-            </Link>
-          </div>
-        )}
 
-        {!ineligibleVote && noVote && (
-          <Paragraph
-            size={ParagraphSize.base}
-            className="text-xs italic font-medium leading-relaxed"
-          >
-            <Trans i18nKey={pageTranslations.noVote} />
-          </Paragraph>
-        )}
-
-        {!account && (
-          <Paragraph
-            size={ParagraphSize.base}
-            className="text-xs italic font-medium leading-relaxed"
-          >
-            <Trans i18nKey={pageTranslations.connectMessage} />
-          </Paragraph>
-        )}
-        {!!vote && (
-          <div className="flex items-center gap-8">
-            <Tooltip
-              content={
-                <span className="font-medium">
-                  {t(pageTranslations.supportText)}
-                </span>
-              }
-              trigger={TooltipTrigger.click}
-              disabled={!vote.support}
-            >
-              <div>
-                <Button
-                  className={classNames('w-52', {
-                    'bg-gray-50': !!vote.support,
-                    'hover:bg-gray-80 focus:bg-gray-80 cursor-default':
-                      !vote.support,
-                  })}
-                  style={ButtonStyle.secondary}
-                  size={ButtonSize.large}
-                  text={
-                    <span className="flex items-center">
-                      <Icon
-                        className="mr-2.5 text-positive"
-                        size={14}
-                        icon={IconNames.CHECK}
-                      />
-                      {t(pageTranslations.support)}
-                    </span>
-                  }
-                />
-              </div>
-            </Tooltip>
-
-            <Tooltip
-              content={
-                <span className="font-medium">
-                  {t(pageTranslations.rejectText)}
-                </span>
-              }
-              trigger={TooltipTrigger.click}
-              disabled={!!vote.support}
-            >
-              <div>
-                <Button
-                  className={classNames('w-52', {
-                    'bg-gray-50': !vote.support,
-                    'hover:bg-gray-80 focus:bg-gray-80 cursor-default':
-                      !!vote.support,
-                  })}
-                  style={ButtonStyle.secondary}
-                  size={ButtonSize.large}
-                  text={
-                    <span className="flex items-center">
-                      <Icon
-                        className="mr-2.5 text-negative"
-                        size={12}
-                        icon={IconNames.X_MARK}
-                      />
-                      {t(pageTranslations.reject)}
-                    </span>
-                  }
-                />
-              </div>
-            </Tooltip>
-          </div>
-        )}
-
-        {!vote && status === ProposalState.Active && (
-          <div className="flex items-center gap-8">
-            <Button
-              className="w-52"
-              style={ButtonStyle.secondary}
-              size={ButtonSize.large}
-              onClick={handleSupport}
-              text={
-                <span className="flex items-center">
-                  <Icon
-                    className="mr-2.5 text-positive"
-                    size={14}
-                    icon={IconNames.CHECK}
-                  />
-                  {t(pageTranslations.support)}
-                </span>
-              }
-            />
-
-            <Button
-              className="w-52"
-              style={ButtonStyle.secondary}
-              size={ButtonSize.large}
-              onClick={handleReject}
-              text={
-                <span className="flex items-center">
-                  <Icon
-                    className="mr-2.5 text-negative"
-                    size={12}
-                    icon={IconNames.X_MARK}
-                  />
-                  {t(pageTranslations.reject)}
-                </span>
-              }
-            />
-          </div>
-        )}
-      </div>
+              <Button
+                className="w-52"
+                style={ButtonStyle.secondary}
+                size={ButtonSize.large}
+                onClick={handleReject}
+                text={
+                  <span className="flex items-center">
+                    <Icon
+                      className="mr-2.5 text-negative"
+                      size={12}
+                      icon={IconNames.X_MARK}
+                    />
+                    {t(pageTranslations.reject)}
+                  </span>
+                }
+              />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };

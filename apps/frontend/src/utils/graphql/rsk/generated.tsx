37426f63b770166ae45599563b48ab2cd653aec2
Pietro Maximoff <74987028+pietro-maximoff@users.noreply.github.com>
2023-10-05 12:32:31 +0200

    SOV-3011: Voting - Proposal CSV export (#627)
    * SOV-3011: add CSV export
    
    * chore: update text in proposal CSV file

--- apps/frontend/src/utils/graphql/rsk/generated.tsx
+++ apps/frontend/src/utils/graphql/rsk/generated.tsx
@@ -15180,6 +15180,7 @@ export type GetProposalQuery = {
       __typename?: 'VoteCast';
       votes: string;
       support: boolean;
+      timestamp: number;
       voter: { __typename?: 'User'; id: string };
       transaction: { __typename?: 'Transaction'; id: string };
     }> | null;
@@ -16740,6 +16741,7 @@ export const GetProposalDocument = gql`
         transaction {
           id
         }
+        timestamp
       }
       emittedBy {
         id

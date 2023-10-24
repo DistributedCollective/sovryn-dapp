4ff1c045cf799528999c1b5d9454a08bda9ca5b9
pietro-maximoff <pietro.maxim.official@protonmail.com>
2023-10-24 15:28:52 +0200

    chore: added testnet addresses

--- apps/frontend/src/app/5_pages/BitocracyPage/components/NewProposalForm/components/TreasuryStep/TreasuryStep.constants.tsx
+++ apps/frontend/src/app/5_pages/BitocracyPage/components/NewProposalForm/components/TreasuryStep/TreasuryStep.constants.tsx
@@ -1,18 +1,40 @@
 import { t } from 'i18next';
 
 import { SupportedTokens } from '@sovryn/contracts';
+import { ChainIds } from '@sovryn/ethers-provider';
+
+import { defaultChainId } from '../../../../../../../config/chains';
 
 import { translations } from '../../../../../../../locales/i18n';
 import { ProposalCreationParameter } from '../../../../contexts/ProposalContext.types';
 
+const governorVaultOwnerAddresses = {
+  [ChainIds.RSK_MAINNET]: '0x05f4f068DF59a5aA7911f57cE4f41ebFBcB8E247',
+  [ChainIds.RSK_TESTNET]: '0x4b38E84628B9AF558076F683Aee548a0B26dc759',
+};
+
+const governorVaultAdminAddresses = {
+  [ChainIds.RSK_MAINNET]: '0x51C754330c6cD04B810014E769Dab0343E31409E',
+  [ChainIds.RSK_TESTNET]: '0x9110896F01150BeC29ca5346d96Ab71Bf8b062dB',
+};
+
+const adoptionFundAddresses = {
+  [ChainIds.RSK_MAINNET]: '0x0f31cfd6aAb4d378668Ad74DeFa89d3f4DB26633',
+  [ChainIds.RSK_TESTNET]: '0x7A06E28e9504aE3D71D1b98085985b6144351e70',
+};
+
+const developmentFundAddresses = {
+  [ChainIds.RSK_MAINNET]: '0x617866cC4a089c3653ddC31a618b078291839AeB',
+  [ChainIds.RSK_TESTNET]: '0x9235600F36eEc91c94862b27bf21699eb8BdD33c',
+};
+
 export const GOVERNOR_VAULT_OWNER_ADDRESS =
-  '0x05f4f068DF59a5aA7911f57cE4f41ebFBcB8E247';
+  governorVaultOwnerAddresses[defaultChainId];
 export const GOVERNOR_VAULT_ADMIN_ADDRESS =
-  '0x51C754330c6cD04B810014E769Dab0343E31409E';
-export const ADOPTION_FUND_ADDRESS =
-  '0x0f31cfd6aAb4d378668Ad74DeFa89d3f4DB26633';
+  governorVaultAdminAddresses[defaultChainId];
+export const ADOPTION_FUND_ADDRESS = adoptionFundAddresses[defaultChainId];
 export const DEVELOPMENT_FUND_ADDRESS =
-  '0x617866cC4a089c3653ddC31a618b078291839AeB';
+  developmentFundAddresses[defaultChainId];
 
 export const TREASURY_OPTIONS = [
   {

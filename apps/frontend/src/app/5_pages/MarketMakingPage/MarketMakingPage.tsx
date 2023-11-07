b3af9346c27af82ec690ca11371cbd777fba8793
Peiman <25097709+Rickk137@users.noreply.github.com>
2023-11-07 19:10:17 +0330

    SOV-3391: promo card component (#674)
    * feat: promo card component
    
    * Create neat-onions-push.md
    
    * feat: Pair Renderer component
    
    * feat: promo card update

--- apps/frontend/src/app/5_pages/MarketMakingPage/MarketMakingPage.tsx
+++ apps/frontend/src/app/5_pages/MarketMakingPage/MarketMakingPage.tsx
@@ -3,6 +3,11 @@ import React, { FC } from 'react';
 import { t } from 'i18next';
 import { Helmet } from 'react-helmet-async';
 
+import { SupportedTokens } from '@sovryn/contracts';
+
+import { AmountRenderer } from '../../2_molecules/AmountRenderer/AmountRenderer';
+import { PromoCard } from '../../2_molecules/PromoCard/PromoCard';
+import { getTokenDisplayName } from '../../../constants/tokens';
 import { translations } from '../../../locales/i18n';
 
 const MarketMakingPage: FC = () => (
@@ -13,7 +18,23 @@ const MarketMakingPage: FC = () => (
 
     <div className="px-0 container md:mx-9 mx-0 md:mb-2 mt-4 mb-7">
       <div className="w-full md:bg-gray-90 md:py-7 md:px-6 rounded mb-6">
-        content
+        <PromoCard
+          asset1={SupportedTokens.bpro}
+          asset2={SupportedTokens.rbtc}
+          label1={t('promotion.weeklyRewards')}
+          child1={
+            <AmountRenderer
+              value="15000"
+              suffix={getTokenDisplayName(SupportedTokens.sov)}
+              isAnimated
+              precision={0}
+            />
+          }
+          label2={t(translations.promotion.currentAPY)}
+          child2={
+            <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
+          }
+        />
       </div>
     </div>
   </>

124a29a1407b11a1028c4e102d4d726fb1c8ec9e
pietro-maximoff <pietro.maxim.official@protonmail.com>
2023-11-23 14:57:14 +0100

    chore: updates from comments

--- apps/frontend/src/app/5_pages/MarketMakingPage/hooks/useGetTradeVolume.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/hooks/useGetTradeVolume.ts
@@ -1,25 +1,35 @@
 import { useMemo } from 'react';
 
+import dayjs from 'dayjs';
+
 import { SECONDS_IN_DAY } from '../../../../constants/general';
 import { rskClient } from '../../../../utils/clients';
-import {
-  GetTradeVolumeQuery,
-  useGetTradeVolumeQuery,
-} from '../../../../utils/graphql/rsk/generated';
+import { useGetTradeVolumeQuery } from '../../../../utils/graphql/rsk/generated';
 
 export const useGetTradeVolume = (pool: string) => {
-  const { loading, data, refetch } = useGetTradeVolumeQuery({
+  const timestamp = useMemo(
+    () => dayjs().subtract(SECONDS_IN_DAY, 'seconds').unix(),
+    [],
+  );
+  const { loading, data, error, refetch } = useGetTradeVolumeQuery({
     client: rskClient,
     variables: {
       pool: pool,
-      timestamp: SECONDS_IN_DAY,
+      timestamp: timestamp,
     },
   });
 
-  const poolVolume = useMemo(
-    () => data?.poolVolumeItems[0].btcAmount as GetTradeVolumeQuery | undefined,
-    [data],
-  );
+  if (error) {
+    console.error('Error fetching trade volume:', error);
+  }
+
+  const poolVolume = useMemo(() => {
+    const poolVolumeItems = data?.poolVolumeItems ?? [];
+    return poolVolumeItems.reduce(
+      (sum, item) => sum + (+item.btcAmount || 0),
+      0,
+    );
+  }, [data]);
 
   return { loading, poolVolume, refetch };
 };

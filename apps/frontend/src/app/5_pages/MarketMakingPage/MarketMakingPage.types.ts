7e3a6d5c556526d84979a05d2733906fb1971b9e
Rick <peymannaderi3@gmail.com>
2023-11-28 15:18:46 +0330

    feat: promo card integration

--- apps/frontend/src/app/5_pages/MarketMakingPage/MarketMakingPage.types.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/MarketMakingPage.types.ts
@@ -1,6 +1,45 @@
+import { SupportedTokens } from '@sovryn/contracts';
 import { Decimal } from '@sovryn/utils';
 
 export type UserInfo = {
   amount: Decimal;
   reward: Decimal;
+};
+
+export type PoolData = {
+  pool_token: string;
+  activity_date: string;
+  APY_fees_pc: string;
+  APY_rewards_pc: string;
+  APY_pc: string;
+};
+
+export type PoolBalanceData = {
+  activity_date: string;
+  balance_btc: number;
+  pool: string;
+};
+
+export type AmmHistory = {
+  pool: string;
+  data: {
+    [key: string]: PoolData[];
+  };
+  balanceHistory: PoolBalanceData[];
+};
+
+export type PromotionData = {
+  rewardAmount: number;
+  type: 'AMM' | 'LENDING';
+  poolTokenA: string;
+  asset1: SupportedTokens;
+  asset2: SupportedTokens;
+  linkAsset?: string;
+  ammData: AmmHistory;
+  apy: string;
+};
+
+export type PromotionsDataResponse = {
+  data: PromotionData[];
+  loading: boolean;
 };
--- apps/frontend/src/app/5_pages/MarketMakingPage/components/Promotions/Promotions.tsx
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/Promotions/Promotions.tsx
@@ -10,113 +10,88 @@ import { BannersCarousel } from '../../../../2_molecules/BannersCarousel/Banners
 import { PromoCard } from '../../../../2_molecules/PromoCard/PromoCard';
 import { getTokenDisplayName } from '../../../../../constants/tokens';
 import { translations } from '../../../../../locales/i18n';
+import { useGetPromotionsData } from '../../hooks/useGetPromotionsData';
 
-export const Promotions: FC = () => (
-  <div className="w-full md:bg-gray-90 rounded md:py-7">
-    <div className="md:px-6">
-      <Paragraph className="mb-1 text-gray-30" size={ParagraphSize.base}>
-        {t(translations.marketMakingPage.promotions.p1)}
-      </Paragraph>
-      <Paragraph className="mb-6 text-gray-30" size={ParagraphSize.base}>
-        {t(translations.marketMakingPage.promotions.p2)}
-        <Button
-          text={t(translations.stakePage.stakingRewards.learnMoreLink)}
-          href={''}
-          className="ml-1"
-          style={ButtonStyle.ghost}
-          hrefExternal
-        />
-      </Paragraph>
+export const Promotions: FC = () => {
+  const { data, loading } = useGetPromotionsData();
+
+  console.log({
+    data,
+    loading,
+  });
+  return (
+    <div className="w-full md:bg-gray-90 rounded md:py-7">
+      <div className="md:px-6">
+        <Paragraph className="mb-1 text-gray-30" size={ParagraphSize.base}>
+          {t(translations.marketMakingPage.promotions.p1)}
+        </Paragraph>
+        <Paragraph className="mb-6 text-gray-30" size={ParagraphSize.base}>
+          {t(translations.marketMakingPage.promotions.p2)}
+          <Button
+            text={t(translations.stakePage.stakingRewards.learnMoreLink)}
+            href={''}
+            className="ml-1"
+            style={ButtonStyle.ghost}
+            hrefExternal
+          />
+        </Paragraph>
+      </div>
+      <BannersCarousel className="-mx-4 sm:mx-2">
+        {!loading &&
+          data.map(item => (
+            <PromoCard
+              key={item.asset1}
+              asset1={item.asset1}
+              asset2={item.asset2}
+              label1={t('promotion.weeklyRewards')}
+              child1={
+                <AmountRenderer
+                  value={item.rewardAmount}
+                  suffix={getTokenDisplayName(SupportedTokens.sov)}
+                  isAnimated
+                  precision={0}
+                />
+              }
+              label2={t(translations.promotion.currentAPY)}
+              child2={
+                <AmountRenderer
+                  value={item.apy}
+                  suffix="%"
+                  isAnimated
+                  precision={2}
+                />
+              }
+            />
+          ))}
+
+        {/* <PromotionCard
+                key={item.asset1}
+                appSection={AppSection.YieldFarm}
+                backgroundColor={item.promotionColor}
+                title={t(translations.promotions.title, {
+                  rewardAmount: `${item.rewardAmount / 1000}K`,
+                })}
+                text={t(translations.promotions.text, {
+                  asset1: item.asset1,
+                  asset2: item.asset2,
+                  rewardAmount: item.rewardAmount.toLocaleString(
+                    navigator.language,
+                  ),
+                })}
+                duration={
+                  customDurationText || t(translations.promotions.duration)
+                }
+                learnMoreLink={learnMoreYieldFarming}
+                logoAsset1={item.asset1}
+                logoAsset2={item.asset2}
+                linkAsset={item.linkAsset}
+                linkDataActionId={`${dataAttributePrefix}-${item.asset1}`}
+                className={cardClassName}
+                imageClassName={cardImageClassName}
+                ammData={item.ammData}
+                poolTokenA={item.poolTokenA}
+              /> */}
+      </BannersCarousel>
     </div>
-    <BannersCarousel className="-mx-4 sm:mx-2">
-      <PromoCard
-        asset1={SupportedTokens.bpro}
-        asset2={SupportedTokens.rbtc}
-        label1={t('promotion.weeklyRewards')}
-        child1={
-          <AmountRenderer
-            value="15000"
-            suffix={getTokenDisplayName(SupportedTokens.sov)}
-            isAnimated
-            precision={0}
-          />
-        }
-        label2={t(translations.promotion.currentAPY)}
-        child2={
-          <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
-        }
-      />
-      <PromoCard
-        asset1={SupportedTokens.dllr}
-        asset2={SupportedTokens.rbtc}
-        label1={t('promotion.weeklyRewards')}
-        child1={
-          <AmountRenderer
-            value="15000"
-            suffix={getTokenDisplayName(SupportedTokens.sov)}
-            isAnimated
-            precision={0}
-          />
-        }
-        label2={t(translations.promotion.currentAPY)}
-        child2={
-          <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
-        }
-      />
-
-      <PromoCard
-        asset1={SupportedTokens.doc}
-        asset2={SupportedTokens.rbtc}
-        label1={t('promotion.weeklyRewards')}
-        child1={
-          <AmountRenderer
-            value="15000"
-            suffix={getTokenDisplayName(SupportedTokens.sov)}
-            isAnimated
-            precision={0}
-          />
-        }
-        label2={t(translations.promotion.currentAPY)}
-        child2={
-          <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
-        }
-      />
-
-      <PromoCard
-        asset1={SupportedTokens.eths}
-        asset2={SupportedTokens.rbtc}
-        label1={t('promotion.weeklyRewards')}
-        child1={
-          <AmountRenderer
-            value="15000"
-            suffix={getTokenDisplayName(SupportedTokens.sov)}
-            isAnimated
-            precision={0}
-          />
-        }
-        label2={t(translations.promotion.currentAPY)}
-        child2={
-          <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
-        }
-      />
-
-      <PromoCard
-        asset1={SupportedTokens.doc}
-        asset2={SupportedTokens.rbtc}
-        label1={t('promotion.weeklyRewards')}
-        child1={
-          <AmountRenderer
-            value="15000"
-            suffix={getTokenDisplayName(SupportedTokens.sov)}
-            isAnimated
-            precision={0}
-          />
-        }
-        label2={t(translations.promotion.currentAPY)}
-        child2={
-          <AmountRenderer value="7.06" suffix="%" isAnimated precision={2} />
-        }
-      />
-    </BannersCarousel>
-  </div>
-);
+  );
+};

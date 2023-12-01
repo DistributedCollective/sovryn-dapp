ab52a5aec01d44fb0000738fc9a2fba1f9c934d6
tiltom <tilnak.tomas@gmail.com>
2023-12-01 13:24:31 +0100

    Refresh adjust dialog user info

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AdjustAndDepositModal/components/CurrentBalance/CurrentBalance.tsx
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AdjustAndDepositModal/components/CurrentBalance/CurrentBalance.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { Decimal } from '@sovryn/utils';
+
 import { AmountRenderer } from '../../../../../../2_molecules/AmountRenderer/AmountRenderer';
 import {
   BITCOIN,
@@ -7,35 +9,37 @@ import {
   TOKEN_RENDER_PRECISION,
 } from '../../../../../../../constants/currencies';
 import { renderTokenSymbol } from '../../../../../../../utils/helpers';
-import { useGetUserInfo } from '../../../../hooks/useGetUserInfo';
 import { AmmLiquidityPool } from '../../../../utils/AmmLiquidityPool';
 
 type CurrentBalanceProps = {
   pool: AmmLiquidityPool;
+  balanceA: Decimal;
+  balanceB: Decimal;
 };
 
-export const CurrentBalance: FC<CurrentBalanceProps> = ({ pool }) => {
-  const { balanceA, balanceB } = useGetUserInfo(pool);
-  return (
-    <>
-      <div className="leading-none">
-        {
-          <AmountRenderer
-            value={balanceA}
-            suffix={renderTokenSymbol(pool.assetA)}
-            precision={TOKEN_RENDER_PRECISION}
-          />
-        }
-      </div>
-      <div className="leading-none">
-        {
-          <AmountRenderer
-            value={balanceB}
-            suffix={BITCOIN}
-            precision={BTC_RENDER_PRECISION}
-          />
-        }
-      </div>
-    </>
-  );
-};
+export const CurrentBalance: FC<CurrentBalanceProps> = ({
+  pool,
+  balanceA,
+  balanceB,
+}) => (
+  <>
+    <div className="leading-none">
+      {
+        <AmountRenderer
+          value={balanceA}
+          suffix={renderTokenSymbol(pool.assetA)}
+          precision={TOKEN_RENDER_PRECISION}
+        />
+      }
+    </div>
+    <div className="leading-none">
+      {
+        <AmountRenderer
+          value={balanceB}
+          suffix={BITCOIN}
+          precision={BTC_RENDER_PRECISION}
+        />
+      }
+    </div>
+  </>
+);

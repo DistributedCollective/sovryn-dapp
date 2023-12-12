4b901463cdf457dbe28b3da39d0c3355cbe35b6e
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2023-12-12 14:18:37 +0200

    SOV-3560: disable permit functionality (#705)
    * feat: remoeve permits from MOC integration smart route
    
    * Revert "feat: remoeve permits from MOC integration smart route"
    
    This reverts commit f17de1b6f07a4a501f03a26cf45ecc1e54a10176.
    
    * feat: disable eip 2612
    
    * fix: allow borrowing dllr
    
    * fix: debt token
    
    * fix: remove unsuded props
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: soulBit <its.soulBit@gmail.com>

--- apps/frontend/src/app/3_organisms/ZeroLocForm/components/AdjustCreditLine.tsx
+++ apps/frontend/src/app/3_organisms/ZeroLocForm/components/AdjustCreditLine.tsx
@@ -6,7 +6,6 @@ import { SupportedTokens } from '@sovryn/contracts';
 import { ErrorLevel } from '@sovryn/ui';
 import { Decimal } from '@sovryn/utils';
 
-import { BORROW_ASSETS } from '../../../5_pages/ZeroPage/constants';
 import { useLiquityBaseParams } from '../../../5_pages/ZeroPage/hooks/useLiquityBaseParams';
 import {
   BITCOIN,
@@ -62,7 +61,9 @@ export const AdjustCreditLine: FC<AdjustCreditLineProps> = ({
   const [collateralAmountInput, setCollateralAmount, collateralAmount] =
     useAmountInput('');
   const [debtAmountInput, setDebtAmount, debtAmount] = useAmountInput('');
-  const [debtToken, setDebtToken] = useState<SupportedTokens>(BORROW_ASSETS[0]);
+  const [debtToken, setDebtToken] = useState<SupportedTokens>(
+    SupportedTokens.zusd,
+  );
   const [maxOriginationFeeRate, setMaxOriginationFeeRate] = useState('0');
 
   const debtSize = useMemo(() => decimalic(debtAmount), [debtAmount]);
--- apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.types.ts
@@ -1,10 +1,17 @@
 import { SupportedTokens } from '@sovryn/contracts';
 import { getProvider } from '@sovryn/ethers-provider';
-import { DEFAULT_SWAP_ROUTES, SmartRouter } from '@sovryn/sdk';
+import { SmartRouter, smartRoutes } from '@sovryn/sdk';
 
 const provider = getProvider();
 
-export const smartRouter = new SmartRouter(provider, DEFAULT_SWAP_ROUTES);
+const SWAP_ROUTES = [
+  smartRoutes.ammSwapRoute,
+  smartRoutes.myntBassetRoute,
+  smartRoutes.myntFixedRateRoute,
+  // smartRoutes.mocIntegrationSwapRoute,
+];
+
+export const smartRouter = new SmartRouter(provider, SWAP_ROUTES);
 
 export const stableCoins = [
   SupportedTokens.zusd,

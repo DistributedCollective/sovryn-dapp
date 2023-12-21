d4013865ac1619e28f67fed82ad683460f895e0a
tiltom <tiltom.defi@protonmail.com>
2023-12-21 16:05:37 +0200

    SOV-3535: Show correct values with fixed conversion rate routes (#724)
    * Show right values with fixed conversion rate routes
    
    * Create tiny-queens-smoke.md

---
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.constants.ts
@@ -0,0 +1,3 @@
+export const FIXED_RATE_ROUTES = ['MyntBasset', 'MyntFixedRate'];
+
+export const FIXED_MYNT_RATE = '0.004723550439442834'; // We need it here as well because ConvertPage slightly rounds maximum price

c216056031747a63dd5fe75eb4f480ad03e64e15
Rick <peymannaderi3@gmail.com>
2024-01-19 16:13:42 +0330

    fix: resolve comments

--- apps/frontend/src/app/5_pages/PortfolioPage/components/ProtocolSection/components/DepositRow/DepositRow.tsx
+++ apps/frontend/src/app/5_pages/PortfolioPage/components/ProtocolSection/components/DepositRow/DepositRow.tsx
@@ -9,7 +9,7 @@ type DepositRowProps = {
 };
 
 export const DepositRow: FC<DepositRowProps> = ({ title, value, cta }) => (
-  <div className="grid items-center grid-cols-2 lg:grid-cols-3 bg-gray-80 rounded py-[1.125rem] px-4">
+  <div className="grid items-center grid-cols-2 lg:grid-cols-3 bg-gray-80 rounded py-[1.125rem] px-4 text-gray-10 font-medium">
     <Paragraph className="text-left">{title}</Paragraph>
     <Paragraph className="text-right">{value}</Paragraph>
     <div className="hidden lg:block text-right">{cta}</div>

1a946701400d3f3b4596129f25c9d69af2255b08
Victor Creed <victor@sovryn.app>
2024-01-22 12:14:45 +0200

    fix: SOV-3729 amount user sends

--- apps/frontend/src/app/3_organisms/FastBtcDialog/components/BoltzReceiveFlow/components/TransferPolicies.tsx
+++ apps/frontend/src/app/3_organisms/FastBtcDialog/components/BoltzReceiveFlow/components/TransferPolicies.tsx
@@ -17,8 +17,8 @@ export const TransferPolicies: React.FC<TransferPoliciesProps> = ({
   const { limits, fees } = useContext(DepositBoltzContext);
 
   const conversionRate = useMemo(
-    () => decimalic(fees.percentageSwapIn),
-    [fees.percentageSwapIn],
+    () => decimalic(fees.percentage),
+    [fees.percentage],
   );
 
   const conversionFee = useMemo(() => {
@@ -35,7 +35,7 @@ export const TransferPolicies: React.FC<TransferPoliciesProps> = ({
       maximumAmount={decimalic(limits.maximal).div(BTC_IN_SATOSHIS)}
       conversionRate={decimalic(fees.percentageSwapIn)}
       conversionFee={conversionFee}
-      networkFee={decimalic(fees.minerFees.baseAsset.normal).div(
+      networkFee={decimalic(fees.minerFees.baseAsset.reverse.lockup).div(
         BTC_IN_SATOSHIS,
       )}
       className="mt-8"

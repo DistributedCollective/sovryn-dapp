0c51d0265e27b6e2541657354de5d4617e8e2728
tiltom <tilnak.tomas@gmail.com>
2024-02-19 19:08:29 +0100

    Fix redash links

--- apps/frontend/src/app/5_pages/LeaderboardPage/components/Leaderboard/components/TradingLeaderboard/TradingLeaderboard.constants.tsx
+++ apps/frontend/src/app/5_pages/LeaderboardPage/components/Leaderboard/components/TradingLeaderboard/TradingLeaderboard.constants.tsx
@@ -8,11 +8,11 @@ import { Badges } from '../../Badges/Badges';
 import { User } from '../../Leaderboard.types';
 
 export const TRADING_LEADERBOARD_URL =
-  'http://redash.sovryn.app/api/queries/544/results.json?api_key=52Jy2PGF5HZVye97NCG9e8nNDDPZ1iFo65Hfo1sk';
+  'https://redash.sovryn.app/api/queries/544/results.json?api_key=52Jy2PGF5HZVye97NCG9e8nNDDPZ1iFo65Hfo1sk';
 
 // TODO: Move somewhere else
 export const STAKING_LEADERBOARD_URL =
-  'http://redash.sovryn.app/api/queries/545/results.json?api_key=sjTLMq48pU0yHJlDBWrFiQQS2x0jTtk7BChYTC8J';
+  'https://redash.sovryn.app/api/queries/545/results.json?api_key=sjTLMq48pU0yHJlDBWrFiQQS2x0jTtk7BChYTC8J';
 
 export const COLUMNS_CONFIG = [
   {

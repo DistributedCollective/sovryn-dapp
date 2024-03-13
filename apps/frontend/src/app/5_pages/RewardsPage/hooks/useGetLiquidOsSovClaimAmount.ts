98c549766d4c25ea7eb4bf18dc9daf429b4fe2b4
pietro-maximoff <pietro.maxim.official@protonmail.com>
2024-03-13 07:51:12 +0100

    chore: update nextWithdrawTimestamp logic

--- apps/frontend/src/app/5_pages/RewardsPage/hooks/useGetLiquidOsSovClaimAmount.ts
+++ apps/frontend/src/app/5_pages/RewardsPage/hooks/useGetLiquidOsSovClaimAmount.ts
@@ -28,17 +28,47 @@ export const useGetLiquidOsSovClaimAmount = () => {
       };
     }
 
-    let result;
-    let nextWithdrawTimestamp;
+    let result = await stakingRewardsOs.getStakerCurrentReward(true, 0);
+    let nextWithdrawTimestamp = 0;
+    let checkpointCount = 15;
+
+    // Calls getStakerCurrentReward(true, 0) until either:
+    // a) The user's reward amount is non-zero or nextWithdrawTimestamp is outside the locked period
+    // b) The maximum checkpoint count is reached
 
     try {
-      while (true) {
-        result = await stakingRewardsOs.getStakerCurrentReward(true, 0);
+      while (
+        result.amount.isZero() &&
+        result.nextWithdrawTimestamp >= 0 &&
+        result.nextWithdrawTimestamp < lockDate &&
+        checkpointCount > 0
+      ) {
         nextWithdrawTimestamp = result.nextWithdrawTimestamp.toNumber();
-        if (!result.amount.isZero() || nextWithdrawTimestamp >= lockDate) {
+
+        if (nextWithdrawTimestamp === 0 || nextWithdrawTimestamp >= lockDate) {
           break;
         }
+
+        result = await stakingRewardsOs.getStakerCurrentReward(true, 0);
+
+        if (
+          !result.amount.isZero() ||
+          nextWithdrawTimestamp === 0 ||
+          nextWithdrawTimestamp >= lockDate
+        ) {
+          break;
+        }
+
+        checkpointCount--;
       }
+
+      return {
+        amount: result.amount.isZero()
+          ? Decimal.ZERO
+          : Decimal.fromBigNumberString(result.amount),
+        nextWithdrawTimestamp,
+        loading: false,
+      };
     } catch (error) {
       console.error('Error fetching rewards:', error);
       return {
@@ -47,19 +77,12 @@ export const useGetLiquidOsSovClaimAmount = () => {
         loading: false,
       };
     }
-
-    return {
-      amount: result.amount.isZero()
-        ? Decimal.ZERO
-        : Decimal.fromBigNumberString(result.amount),
-      nextWithdrawTimestamp,
-      loading: false,
-    };
   }, [account, stakingRewardsOs, lockDate]);
 
-  const updateRewards = useCallback(async () => {
-    const rewards = await getRewards();
-    setValue(rewards);
+  const updateRewards = useCallback(() => {
+    getRewards().then(({ amount, nextWithdrawTimestamp, loading }) => {
+      setValue({ amount, nextWithdrawTimestamp, loading });
+    });
   }, [getRewards]);
 
   useEffect(() => {

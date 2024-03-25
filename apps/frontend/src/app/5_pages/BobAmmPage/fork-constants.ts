d636835e86333449921d4325855dfac03481b417
Victor Creed <victor@sovryn.app>
2024-03-25 15:26:40 +0200

    wip: forked sepolia network

---
+++ apps/frontend/src/app/5_pages/BobAmmPage/fork-constants.ts
@@ -0,0 +1,6 @@
+import { ethers } from 'ethers';
+
+export const ETH_TOKEN = ethers.constants.AddressZero;
+export const USDC_TOKEN = '0x60bBA138A74C5e7326885De5090700626950d509';
+export const WBTC_TOKEN = '0xCA97CC9c1a1dfA54A252DaAFE9b5Cd1E16C81328';
+export const OKB_TOKEN = '0x3F4B6664338F23d2397c953f2AB4Ce8031663f80';
--- apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
+++ apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
@@ -1,15 +1,9 @@
-import { CrocEnv } from '@sovryn/ambient-sdk';
-import { BOB_CHAIN_ID } from '../../../config/chains';
-import { findAsset } from '../../../utils/asset';
+import { CrocEnv, encodeCrocPrice } from '@sovryn/ambient-sdk';
 
 import { OrderDirective } from '@sovryn/ambient-sdk/src/encoding/longform';
 import { parseEther } from 'ethers/lib/utils';
+import { ETH_TOKEN, USDC_TOKEN } from './fork-constants';
 
-export const ETH = findAsset('ETH', BOB_CHAIN_ID).address;
-export const SOV = findAsset('SOV', BOB_CHAIN_ID).address;
-export const GLD = findAsset('GLD', BOB_CHAIN_ID).address;
-
-const POOL_INDEX = 3600;
 const SLIPPDAGE = 0.1;
 
 // Reference
@@ -18,35 +12,69 @@ const SLIPPDAGE = 0.1;
 // https://github.com/CrocSwap/ambient-ts-app/blob/aa39b19b2c6e1b86c28d37ffa3a74f5031adadf0/src/pages/Trade/Reposition/Reposition.tsx#L135
 // https://github.com/CrocSwap/CrocSwap-protocol/blob/8a273515be92dd4e28e4c51a86097e8d35bc48ad/test/TestEncoding.ts#L8
 
+const ENTRY = ETH_TOKEN;
+const NEXT = USDC_TOKEN;
+
 // SOV -> GLD
 export const multiSwap = async (env: CrocEnv) => {
+  const ENTRY_AMOUNT = parseEther('0.01');
+  const POOL_INDEX = (await env.context).chain.poolIndex;
+
   // entry
-  const directive = new OrderDirective(SOV);
+  const directive = new OrderDirective(ENTRY);
 
   // SOV->ETH
-  directive.appendHop(ETH);
+  directive.appendHop(NEXT);
 
-  const SOV_ETH = getPool(env, SOV, ETH);
-  const price = await SOV_ETH.spotPrice();
+  const POOL_1 = getPool(env, ENTRY, NEXT);
+  const price = await POOL_1.displayPrice();
+
+  console.log({ price });
+
+  const POOL_1_INFO = await POOL_1.context;
+  const POOL_1_CURVE = await POOL_1_INFO.query.queryCurve(
+    ENTRY,
+    NEXT,
+    POOL_INDEX,
+  );
+
+  console.log({ price, poolInfo: POOL_1_INFO, curve: POOL_1_CURVE });
 
   const pool1 = directive.appendPool(POOL_INDEX);
   // pool1.chain.swapDefer = true;
   // pool1.swap.rollType = 4;
   pool1.swap.isBuy = false;
-  pool1.swap.qty = parseEther('0.000001');
+  pool1.swap.qty = ENTRY_AMOUNT;
 
-  const priceMult = 1 - SLIPPDAGE; // or 1 - SLIPPDAGE
+  const priceMult = 1 + SLIPPDAGE; // or 1 - SLIPPDAGE
 
-  pool1.swap.limitPrice = await SOV_ETH.baseToken.normQty(price * priceMult);
+  const limitPrice = await POOL_1.baseToken.normQty(price * priceMult);
+
+  pool1.swap.limitPrice = limitPrice;
   // pool1.swap.limitPrice = encodeCrocPrice(price * priceMult);
 
   // // ETH->GLD
   // directive.appendHop(GLD);
   // directive.appendPool(3600);
 
+  console.log({
+    directive,
+    limitPrice: limitPrice.toString(),
+    priceRoot_: POOL_1_CURVE.priceRoot_.toString(),
+  });
+
   const bytes = directive.encodeBytes();
 
-  return (await env.context).dex?.userCmd(4, bytes, { gasLimit: 1_000_000 });
+  const tx = await (
+    await env.context
+  ).dex?.userCmd(4, bytes, { gasLimit: 6_000_000, value: ENTRY_AMOUNT });
+  console.log({ tx });
+
+  const result = await tx?.wait();
+
+  console.log(result.status ? true : false, result);
+
+  return result;
 };
 
 const getPool = (env: CrocEnv, base: string, quote: string) =>

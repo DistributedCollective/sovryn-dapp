32326cc59b639cb207047e97a7eb4928ae5ba548
Victor Creed <victor@sovryn.app>
2024-03-26 19:13:22 +0200

    wip: forked sepolia network (working with dex amounts)

--- apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
+++ apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
@@ -1,13 +1,14 @@
-import { CrocEnv, encodeCrocPrice } from '@sovryn/ambient-sdk';
+import { CrocEnv, MAX_SQRT_PRICE, MIN_SQRT_PRICE } from '@sovryn/ambient-sdk';
 
 import { OrderDirective } from '@sovryn/ambient-sdk/src/encoding/longform';
-import { parseEther, parseUnits } from 'ethers/lib/utils';
 import { ETH_TOKEN, USDC_TOKEN, WBTC_TOKEN } from './fork-constants';
 import { BigNumber } from 'ethers';
+import { Decimal } from '@sovryn/utils';
 
 const SLIPPDAGE = 0.1;
 
 // Reference
+// https://github.com/busimus/ars/blob/main/frontend/src/App.vue#L537-L562
 // https://github.com/CrocSwap/sdk/blob/main/src/recipes/reposition.ts
 // https://github.com/DistributedCollective/Ambient-DEX/blob/feat/deployment-init/test/FacadePool.ts#L606
 // https://github.com/CrocSwap/ambient-ts-app/blob/aa39b19b2c6e1b86c28d37ffa3a74f5031adadf0/src/pages/Trade/Reposition/Reposition.tsx#L135
@@ -19,7 +20,7 @@ const END = WBTC_TOKEN;
 
 // SOV -> GLD
 export const multiSwap = async (env: CrocEnv) => {
-  const ENTRY_AMOUNT = await env.token(ENTRY).normQty(1);
+  const ENTRY_AMOUNT = await env.token(NEXT).normQty(1);
 
   console.log({ ENTRY_AMOUNT: ENTRY_AMOUNT.toString() });
 
@@ -32,37 +33,37 @@ export const multiSwap = async (env: CrocEnv) => {
 
   // entry
   const order = new OrderDirective(ENTRY);
-  // order.open.useSurplus = true;
+  order.open.useSurplus = true;
 
   // HOP 1:
   const hop = order.appendHop(NEXT);
-  // hop.settlement.useSurplus = true;
+  hop.settlement.useSurplus = true;
 
-  const POOL_1 = getPool(env, ENTRY, NEXT);
-  const PRICE_1 = await POOL_1.displayPrice();
-  const POOL_1_INFO = await POOL_1.context;
-  const POOL_1_CURVE = await POOL_1_INFO.query.queryCurve(
+  const pool = order.appendPool(POOL_INDEX);
+
+  const buy = false;
+
+  pool.swap.isBuy = buy;
+  pool.swap.inBaseQty = buy;
+  pool.swap.qty = ENTRY_AMOUNT;
+
+  const { finalPrice } = await calcImpact(
+    env,
     ENTRY,
     NEXT,
     POOL_INDEX,
+    pool.swap.isBuy,
+    pool.swap.inBaseQty,
+    ENTRY_AMOUNT,
   );
 
-  console.log({ price: PRICE_1 });
-
-  const pool = order.appendPool(POOL_INDEX);
-  // pool.swap.isBuy = true;
-  // pool.swap.inBaseQty = false;
-  pool.swap.qty = ENTRY_AMOUNT;
-  pool.swap.limitPrice = encodeCrocPrice(PRICE_1 * (1 + SLIPPDAGE));
+  pool.swap.limitPrice = Decimal.fromBigNumberString(finalPrice)
+    .mul(buy ? 1 + SLIPPDAGE : 1 - SLIPPDAGE)
+    .toBigNumber();
   // @dev https://github.com/CrocSwap/CrocSwap-protocol/blob/8a273515be92dd4e28e4c51a86097e8d35bc48ad/contracts/libraries/Chaining.sol#L28
   pool.swap.rollType = 0;
 
-  console.log(
-    'price',
-    pool.swap.limitPrice.toString(),
-    'curve',
-    POOL_1_CURVE.priceRoot_.toString(),
-  );
+  console.log('price', pool.swap.limitPrice.toString());
 
   pool.chain.rollExit = true;
 
@@ -139,7 +140,7 @@ export const multiSwap = async (env: CrocEnv) => {
 
   const tx = await (
     await env.context
-  ).dex?.userCmd(PATH, bytes, { gasLimit: 6_000_000, value });
+  ).dex?.userCmd(PATH, bytes, { gasLimit: 6_000_000 });
   console.log({ tx });
 
   const result = await tx?.wait();
@@ -151,3 +152,28 @@ export const multiSwap = async (env: CrocEnv) => {
 
 const getPool = (env: CrocEnv, base: string, quote: string) =>
   env.pool(base, quote);
+
+const initialLimitPrice = (isBuy: boolean) =>
+  isBuy ? MAX_SQRT_PRICE : MIN_SQRT_PRICE;
+
+const calcImpact = async (
+  env: CrocEnv,
+  base: string,
+  quote: string,
+  poolIdx: number,
+  isBuy: boolean,
+  inBaseQty: boolean,
+  qty: BigNumber,
+) => {
+  const context = await env.context;
+  return await context.slipQuery.calcImpact(
+    base,
+    quote,
+    poolIdx,
+    isBuy,
+    inBaseQty,
+    qty,
+    0,
+    initialLimitPrice(isBuy),
+  );
+};

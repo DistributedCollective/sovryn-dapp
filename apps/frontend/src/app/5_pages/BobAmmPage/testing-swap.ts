ff7d8b945af03ed60e5673507d4adda0999215ba
Victor Creed <victor@sovryn.app>
2024-04-02 14:42:09 +0300

    wip: second hop working

--- apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
+++ apps/frontend/src/app/5_pages/BobAmmPage/testing-swap.ts
@@ -21,13 +21,17 @@ export const multiSwap = async (
   signer: string,
   isBuy: boolean, // true = use ethereum (base), false = use token (quote)
 ) => {
-  const useSurplus = true;
+  const useSurplus = false;
 
-  const ENTRY_AMOUNT = await env.token(USDC_TOKEN).normQty(20);
+  const ENTRY_AMOUNT_1 = await env.token(USDC_TOKEN).normQty(20);
 
-  console.log({ ENTRY_AMOUNT: ENTRY_AMOUNT.toString() });
+  console.log({ ENTRY_AMOUNT: ENTRY_AMOUNT_1.toString() });
 
-  await testAllowance(signer, env.tokens.materialize(USDC_TOKEN), ENTRY_AMOUNT);
+  await testAllowance(
+    signer,
+    env.tokens.materialize(USDC_TOKEN),
+    ENTRY_AMOUNT_1,
+  );
 
   const POOL_INDEX = (await env.context).chain.poolIndex;
   const PATH = (await env.context).chain.proxyPaths.long;
@@ -37,37 +41,85 @@ export const multiSwap = async (
   order.open.useSurplus = useSurplus;
 
   // HOP 1:
-  const hop = order.appendHop(USDC_TOKEN);
-  hop.settlement.useSurplus = useSurplus;
+  const hop_1 = order.appendHop(USDC_TOKEN);
+  hop_1.settlement.useSurplus = useSurplus;
 
-  const pool = order.appendPool(POOL_INDEX);
+  const pool_1 = order.appendPool(POOL_INDEX);
 
   const isBuy_1 = false;
 
-  pool.swap.isBuy = isBuy_1;
-  pool.swap.inBaseQty = isBuy_1;
-  pool.swap.qty = ENTRY_AMOUNT;
+  pool_1.swap.isBuy = isBuy_1;
+  pool_1.swap.inBaseQty = isBuy_1;
+  pool_1.swap.qty = ENTRY_AMOUNT_1;
 
-  const { finalPrice } = await calcImpact(
+  const impact_1 = await calcImpact(
     env,
     ETH_TOKEN,
     USDC_TOKEN,
     POOL_INDEX,
-    pool.swap.isBuy,
-    pool.swap.inBaseQty,
-    ENTRY_AMOUNT,
+    pool_1.swap.isBuy,
+    pool_1.swap.inBaseQty,
+    ENTRY_AMOUNT_1,
   );
 
-  pool.swap.limitPrice = Decimal.fromBigNumberString(finalPrice)
+  console.log({ impact_1 });
+
+  pool_1.swap.limitPrice = Decimal.fromBigNumberString(impact_1.finalPrice)
     .mul(isBuy_1 ? 1 + SLIPPDAGE : 1 - SLIPPDAGE)
     .toBigNumber();
   // @dev https://github.com/CrocSwap/CrocSwap-protocol/blob/8a273515be92dd4e28e4c51a86097e8d35bc48ad/contracts/libraries/Chaining.sol#L28
-  pool.swap.rollType = 0;
+  pool_1.swap.rollType = 0;
 
-  console.log('price', pool.swap.limitPrice.toString());
+  console.log('price', pool_1.swap.limitPrice.toString());
 
-  pool.chain.rollExit = true;
+  pool_1.chain.rollExit = true;
   // pool.chain.rollType = 0;
+
+  console.log(
+    'out',
+    Decimal.fromBigNumberString(impact_1.baseFlow).abs().toBigNumber(),
+  );
+
+  const OUT_1 = Decimal.fromBigNumberString(impact_1.baseFlow)
+    .abs()
+    .toBigNumber();
+
+  console.log('out1', OUT_1.toString());
+
+  // HOP 2:
+
+  order.appendHop(ETH_TOKEN);
+  const hop_2 = order.appendHop(OKB_TOKEN);
+  hop_2.settlement.useSurplus = useSurplus;
+  // hop_2.settlement.token = ETH_TOKEN;
+
+  const pool_2 = order.appendPool(POOL_INDEX);
+
+  const isBuy_2 = true;
+
+  pool_2.swap.isBuy = isBuy_2;
+  pool_2.swap.inBaseQty = isBuy_2;
+  pool_2.swap.qty = OUT_1;
+
+  const { finalPrice: finalPrice2 } = await calcImpact(
+    env,
+    ETH_TOKEN,
+    OKB_TOKEN,
+    POOL_INDEX,
+    pool_2.swap.isBuy,
+    pool_2.swap.inBaseQty,
+    OUT_1,
+  );
+
+  pool_2.swap.limitPrice = Decimal.fromBigNumberString(finalPrice2)
+    .mul(isBuy_2 ? 1 + SLIPPDAGE : 1 - SLIPPDAGE)
+    .toBigNumber();
+  // @dev https://github.com/CrocSwap/CrocSwap-protocol/blob/8a273515be92dd4e28e4c51a86097e8d35bc48ad/contracts/libraries/Chaining.sol#L28
+  pool_2.swap.rollType = 0;
+
+  console.log('price 2:', pool_2.swap.limitPrice.toString());
+
+  pool_2.chain.rollExit = true;
 
   console.log('ORDER: ', order);
 

6ed292e3959aa6363269a8092e38bd3e6cbe0ef6
soulBit <its.soulBit@gmail.com>
2024-04-04 16:26:51 +0100

    feat: add BoB token icons

--- packages/contracts/src/contracts/assets/bobTestnet.ts
+++ packages/contracts/src/contracts/assets/bobTestnet.ts
@@ -9,7 +9,7 @@ export const bobTestnet: Array<AssetDetails> = [
     name: 'Ether',
     isNative: true,
     decimals: 18,
-    getIcon: async () => (await import('./icons/rsk/eths')).default,
+    getIcon: async () => (await import('./icons/bob/eth')).default,
     description: 'BOB Ether',
   },
   {
@@ -17,7 +17,7 @@ export const bobTestnet: Array<AssetDetails> = [
     address: '0x3E610F32806e09C2Ba65b8c88A6E4f777c8Cb559',
     name: 'Sovryn',
     decimals: 18,
-    getIcon: async () => (await import('./icons/rsk/sov')).default,
+    getIcon: async () => (await import('./icons/bob/sov')).default,
     description: 'Sovryn Token',
   },
   {
@@ -25,20 +25,20 @@ export const bobTestnet: Array<AssetDetails> = [
     address: '0xEf6495e4D07Fa58e473C5CC3a2e3ebB8876CC798',
     name: 'USDC',
     decimals: 18,
-    getIcon: async () => (await import('./icons/unknown')).default,
+    getIcon: async () => (await import('./icons/bob/usdc')).default,
   },
   {
     symbol: 'USDT',
     address: '0xfCDaC6196C22908ddA4CE84fb595B1C7986346bF',
     name: 'USDT',
     decimals: 18,
-    getIcon: async () => (await import('./icons/unknown')).default,
+    getIcon: async () => (await import('./icons/bob/usdt')).default,
   },
   {
     symbol: 'DAI',
     address: '0x0D7423380F4b6f96D8188CCCfB086aBEC84A0934',
     name: 'DAI',
     decimals: 18,
-    getIcon: async () => (await import('./icons/unknown')).default,
+    getIcon: async () => (await import('./icons/bob/dai')).default,
   },
 ];

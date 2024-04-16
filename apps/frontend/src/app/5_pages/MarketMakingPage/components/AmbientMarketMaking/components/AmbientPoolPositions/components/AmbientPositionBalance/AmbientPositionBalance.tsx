fc27efd4c966fbafae141a19efff163a4a8efe9d
Rick <peymannaderi3@gmail.com>
2024-04-16 18:09:17 +0300

    fix: ambient position balance

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/components/AmbientPoolPositions/components/AmbientPositionBalance/AmbientPositionBalance.tsx
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/components/AmbientPoolPositions/components/AmbientPositionBalance/AmbientPositionBalance.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 
 import { AmountRenderer } from '../../../../../../../../2_molecules/AmountRenderer/AmountRenderer';
+import { useCurrentChain } from '../../../../../../../../../hooks/useChainStore';
+import { useTokenDetailsByAsset } from '../../../../../../../../../hooks/useTokenDetailsByAsset';
+import { decimalic, fromWei } from '../../../../../../../../../utils/math';
 import { AmbientPosition } from '../../../../AmbientMarketMaking.types';
 import { AmbientLiquidityPool } from '../../../../utils/AmbientLiquidityPool';
 import { useAmbientPositionBalance } from '../../hooks/useAmbientPositionBalance';
@@ -14,12 +17,30 @@ export const AmbientPositionBalance: FC<AmbientPositionBalanceProps> = ({
   position,
   pool,
 }) => {
+  const chainId = useCurrentChain();
+  const baseToken = useTokenDetailsByAsset(pool.base, chainId);
+  const quoteToken = useTokenDetailsByAsset(pool.quote, chainId);
   const result = useAmbientPositionBalance(pool, position);
-  console.log({ result, position });
+  if (!result) {
+    return null;
+  }
+
   return (
     <div className="flex flex-col">
-      <AmountRenderer value="1" suffix={pool.base} />
-      <AmountRenderer value="0" suffix={pool.quote} />
+      <AmountRenderer
+        value={fromWei(
+          decimalic(result?.positionLiqBase || '0').toString(),
+          baseToken?.decimals,
+        )}
+        suffix={pool.base}
+      />
+      <AmountRenderer
+        value={fromWei(
+          decimalic(result?.positionLiqQuote || '0').toString(),
+          quoteToken?.decimals,
+        )}
+        suffix={pool.quote}
+      />
     </div>
   );
 };

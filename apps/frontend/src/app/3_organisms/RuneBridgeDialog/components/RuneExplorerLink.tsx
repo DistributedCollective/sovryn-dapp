11b90f618e927107798c8cddc8269caba2e1f7c2
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-17 12:59:13 +0300

    RuneExplorerLink review changes

--- apps/frontend/src/app/3_organisms/RuneBridgeDialog/components/RuneExplorerLink.tsx
+++ apps/frontend/src/app/3_organisms/RuneBridgeDialog/components/RuneExplorerLink.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
 import { Environments } from '../../../../types/global';
+import { currentNetwork } from '../../../../utils/helpers';
 
 export interface RuneExplorerLinkProps {
   rune: string;
 }
 
 const EXPLORER_BASE_URL =
-  process.env.REACT_APP_NETWORK === Environments.Testnet
+  currentNetwork === Environments.Testnet
     ? 'https://testnet.ordinals.com'
     : 'https://ordinals.com';
 
-export const RuneExplorerLink = ({ rune }: RuneExplorerLinkProps) => {
-  return (
-    <a
-      href={`${EXPLORER_BASE_URL}/rune/${rune}`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {rune}
-    </a>
-  );
-};
+export const RuneExplorerLink = ({ rune }: RuneExplorerLinkProps) => (
+  <a
+    href={`${EXPLORER_BASE_URL}/rune/${rune}`}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {rune}
+  </a>
+);

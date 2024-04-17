c48797ac85a7b1c1a40d84a1f484e2c9f716d025
soulBit <its.soulBit@gmail.com>
2024-04-17 18:35:47 +0100

    feat: add market making network banner

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/AmbientMarketMaking.tsx
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/AmbientMarketMaking.tsx
@@ -5,9 +5,12 @@ import { Helmet } from 'react-helmet-async';
 
 import { Heading, Paragraph, ParagraphSize } from '@sovryn/ui';
 
+import { RSK_CHAIN_ID } from '../../../../../config/chains';
+
+import { MarketMakingNetworkBanner } from '../../../../2_molecules/MarketMakingNetworkBanner/MarketMakingNetworkBanner';
+import { BOB_STORAGE_KEY } from '../../../../2_molecules/MarketMakingNetworkBanner/MarketMakingNetworkBanner.constants';
 import { translations } from '../../../../../locales/i18n';
 import { AmbientPoolsTable } from './components/AmbientPoolsTable/AmbientPoolsTable';
-import { RSKBanner } from './components/RSKBanner/RSKBanner';
 
 export const AmbientMarketMaking: FC = () => {
   return (
@@ -16,21 +19,25 @@ export const AmbientMarketMaking: FC = () => {
         <title>{t(translations.ambientMarketMaking.meta.title)}</title>
       </Helmet>
 
-      <div className="w-full flex flex-col items-center text-gray-10 mt-6 mb-4 sm:mt-9 max-w-[74.75rem]">
-        <Heading className="text-center mb-1 lg:mb-3 text-base lg:text-2xl">
-          {t(translations.ambientMarketMaking.title)}
-        </Heading>
+      <div className="w-full max-w-[74.75rem]">
+        <MarketMakingNetworkBanner
+          requiredChainId={RSK_CHAIN_ID}
+          storageKey={BOB_STORAGE_KEY}
+        />
+        <div className="text-gray-10 mt-6 mb-4 sm:mt-9">
+          <Heading className="text-center mb-1 lg:mb-3 text-base lg:text-2xl">
+            {t(translations.ambientMarketMaking.title)}
+          </Heading>
 
-        <Paragraph
-          className="text-center mb-5 lg:mb-9"
-          size={ParagraphSize.base}
-        >
-          {t(translations.ambientMarketMaking.subtitle)}
-        </Paragraph>
+          <Paragraph
+            className="text-center mb-5 lg:mb-9"
+            size={ParagraphSize.base}
+          >
+            {t(translations.ambientMarketMaking.subtitle)}
+          </Paragraph>
 
-        <RSKBanner />
-
-        <AmbientPoolsTable />
+          <AmbientPoolsTable />
+        </div>
       </div>
     </>
   );

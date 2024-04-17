33f618998ea5f7f36b7b63e21e2775bd56d43d69
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-17 12:29:01 +0300

    Remove unused ExplorerNetworkURI

--- apps/frontend/src/types/global.ts
+++ apps/frontend/src/types/global.ts
@@ -2,10 +2,7 @@ export enum Environments {
   Testnet = 'testnet',
   Mainnet = 'mainnet',
 }
-export enum ExplorerNetworkURI {
-  Testnet = 'https://explorer.testnet.rsk.co',
-  Mainnet = 'https://explorer.rsk.co',
-}
+
 export enum Services {
   Maintenance = 'maintenance',
   Notification = 'notification',

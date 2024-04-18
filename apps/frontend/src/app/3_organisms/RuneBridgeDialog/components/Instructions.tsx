7d4fa5861e220259f6954905f2489f540fe1640c
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-18 21:23:47 +0300

    Nicer instructions

--- apps/frontend/src/app/3_organisms/RuneBridgeDialog/components/Instructions.tsx
+++ apps/frontend/src/app/3_organisms/RuneBridgeDialog/components/Instructions.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { t } from 'i18next';
 import { Trans } from 'react-i18next';
@@ -7,50 +7,83 @@ import { Heading, HeadingType, Link } from '@sovryn/ui';
 
 import { HELPDESK_LINK } from '../../../../constants/links';
 import { translations } from '../../../../locales/i18n';
-import { MIN_POSTAGE_SATS, MIN_POSTAGE_BTC } from '../constants';
+import {
+  MIN_POSTAGE_SATS,
+  MIN_POSTAGE_BTC,
+  ORD_WALLET_LINK,
+} from '../constants';
 
 type InstructionsProps = {
   isReceive?: boolean;
 };
 
 export const Instructions: React.FC<InstructionsProps> = ({ isReceive }) => {
-  const translation = useMemo(
-    () =>
-      isReceive
-        ? translations.runeBridge.receiveInstructions
-        : translations.runeBridge.sendInstructions,
-    [isReceive],
-  );
-
   return (
     <>
       <Heading type={HeadingType.h2} className="font-medium leading-[1.375rem]">
-        {t(translation.title)}:
+        {t(translations.runeBridge.instructions.title)}:
       </Heading>
 
       <ul className="list-disc list-inside text-xs leading-5 font-medium text-gray-30 mt-4 mb-12">
-        <li className="mb-4">{t(translation.line1)}</li>
         {isReceive ? (
-          <li className="mb-4">
-            {t(translations.runeBridge.receiveInstructions.postage, {
-              postageSat: MIN_POSTAGE_SATS,
-              postageBtc: MIN_POSTAGE_BTC,
-            })}
-          </li>
+          <>
+            <li className="mb-4">
+              {t(translations.runeBridge.instructions.receive.onlyRunes)}
+            </li>
+            <li className="mb-4">
+              {t(
+                translations.runeBridge.instructions.receive.onlySupportedRunes,
+              )}
+            </li>
+            <li className="mb-4">
+              {t(translations.runeBridge.instructions.receive.postage, {
+                postageSat: MIN_POSTAGE_SATS,
+                postageBtc: MIN_POSTAGE_BTC,
+              })}
+            </li>
+          </>
         ) : (
-          <li className="mb-4">
-            {t(translations.runeBridge.sendInstructions.supportedFormats)}
-          </li>
+          <>
+            <li className="mb-4">
+              <Trans
+                i18nKey={t(translations.runeBridge.instructions.send.wallet)}
+                components={[
+                  <Link
+                    text={t(
+                      translations.runeBridge.instructions.send.walletCta,
+                    )}
+                    href={ORD_WALLET_LINK}
+                  />,
+                ]}
+              />
+            </li>
+            <li className="mb-4">
+              {t(translations.runeBridge.instructions.send.supportedFormats)}
+            </li>
+            <li className="mb-4">
+              {t(translations.runeBridge.instructions.send.minAmount)}
+            </li>
+            <li className="mb-4">
+              {t(translations.runeBridge.instructions.send.maxAmount)}
+            </li>
+          </>
         )}
-        <li className="mb-4">{t(translation.line2)}</li>
-        <li className="mb-4">{t(translation.line3)}</li>
-        <li className="mb-4">{t(translation.line4, { hours: 1.5 })}</li>
+        <li className="mb-4">
+          {t(translations.runeBridge.instructions.upToNHours, { hours: 1.5 })}
+        </li>
         <li>
           <Trans
-            i18nKey={t(translation.line5)}
+            i18nKey={t(
+              translations.runeBridge.instructions.createSupportTicket,
+            )}
             tOptions={{ hours: 1.5 }}
             components={[
-              <Link text={t(translation.line5cta)} href={HELPDESK_LINK} />,
+              <Link
+                text={t(
+                  translations.runeBridge.instructions.createSupportTicketCta,
+                )}
+                href={HELPDESK_LINK}
+              />,
             ]}
           />
         </li>
--- apps/frontend/src/app/3_organisms/RuneBridgeDialog/constants.ts
+++ apps/frontend/src/app/3_organisms/RuneBridgeDialog/constants.ts
@@ -7,3 +7,5 @@ export const MIN_POSTAGE_SATS = 10_000;
 export const MIN_POSTAGE_BTC = MIN_POSTAGE_SATS / 1e8;
 
 export const GAS_LIMIT_RUNE_BRIDGE_WITHDRAW = 200000;
+
+export const ORD_WALLET_LINK = 'https://github.com/ordinals/ord';

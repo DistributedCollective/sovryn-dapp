2e9e7c124b944a36251e97162955fb51f5d6aaf2
Victor Creed <victor@sovryn.app>
2024-04-19 15:49:55 +0300

    chore: add merkle distributor contract

--- packages/contracts/src/contracts/protocol/fork.ts
+++ packages/contracts/src/contracts/protocol/fork.ts
@@ -2,7 +2,7 @@ import type { AsyncContractConfigData } from '../../types';
 
 export const fork: Record<string, AsyncContractConfigData> = {
   merkleDistributor: {
-    address: '0xe193c995a9f631144441e0cf8d37583ef7b35fe3',
+    address: '0xcce97e181a355939248afe2aab9587b46de56396',
     getAbi: async () =>
       (await import('../../abis/merkleDistributor.json')).default,
   },

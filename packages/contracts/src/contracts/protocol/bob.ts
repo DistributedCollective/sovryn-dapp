530602c6beee13482c007a7f18e8dd9ff61b49f1
soulBit <its.soulBit@gmail.com>
2024-04-20 18:18:09 +0100

    chore: add stubs for rune bridge contracts

--- packages/contracts/src/contracts/protocol/bob.ts
+++ packages/contracts/src/contracts/protocol/bob.ts
@@ -2,7 +2,7 @@ import { AsyncContractConfigData } from '../../types';
 
 export const bobTestnet: Record<string, AsyncContractConfigData> = {
   staking: {
-    address: '0x1139B088887237ed256687d9E1499275e37d9f2d',
+    address: '',
     getAbi: async () => (await import('../../abis/staking.json')).default,
   },
   runeBridge: {

8b156bd9a452474313e01187b2b599308b6dfbcd
Victor Creed <victor@sovryn.app>
2024-04-26 15:10:50 +0300

    chore: mainnet mock pools

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/AmbientMarketMaking.constants.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/AmbientMarketMaking.constants.ts
@@ -4,16 +4,15 @@ import { AmbientLiquidityPool } from './utils/AmbientLiquidityPool';
 
 export const MAINNET_AMM = [
   // BOB mock pools
-  // new AmbientLiquidityPool('ETH', 'mWSTETH', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mDLLR', 'mSOV', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('ETH', 'mSOV', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mUSDC', 'mUSDT', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mDLLR', 'mDAI', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mWBTC', 'mTBTC', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mSOV', 'mPOWA', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mDLLR', 'mUSDT', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('ETH', 'mRETH', ChainIds.BOB_MAINNET, 400),
-  // new AmbientLiquidityPool('mWBTC', 'mSOV', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('USDC', 'USDT', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('DAI', 'DLLR', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('USDT', 'DLLR', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('TBTC', 'WBTC', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('RETH', 'ETH', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('WSTETH', 'ETH', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('DLLR', 'SOV', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('ETH', 'SOV', ChainIds.BOB_MAINNET, 400),
+  new AmbientLiquidityPool('POWA', 'SOV', ChainIds.BOB_MAINNET, 400),
 ];
 
 export const TESTNET_AMM = [

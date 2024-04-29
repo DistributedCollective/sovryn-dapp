6be290f63b27961253b987cada61534743eb21ff
longnguyen <long.nguyendr@gmail.com>
2024-04-29 15:48:08 +0300

    Enhanced the useRuneBridgeLocked hook to support multi-chain environments by integrating the useCurrentChain hook.

--- apps/frontend/src/app/3_organisms/RuneBridgeDialog/hooks/useRuneBridgeLocked.ts
+++ apps/frontend/src/app/3_organisms/RuneBridgeDialog/hooks/useRuneBridgeLocked.ts
@@ -1,6 +1,19 @@
+import React from 'react';
+
+import { ChainIds } from '@sovryn/ethers-provider';
+
+import { useCurrentChain } from '../../../../hooks/useChainStore';
 import { useMaintenance } from '../../../../hooks/useMaintenance';
 
 export const useRuneBridgeLocked = () => {
   const { checkMaintenance, States } = useMaintenance();
-  return checkMaintenance(States.D2_RUNE_BRIDGE_RSK);
+  const chainId = useCurrentChain();
+  return React.useMemo(() => {
+    if (
+      [ChainIds.BOB_MAINNET, ChainIds.BOB_TESTNET].includes(chainId as ChainIds)
+    ) {
+      return checkMaintenance(States.D2_RUNE_BRIDGE_BOB);
+    }
+    return checkMaintenance(States.D2_RUNE_BRIDGE_RSK);
+  }, [States, chainId, checkMaintenance]);
 };

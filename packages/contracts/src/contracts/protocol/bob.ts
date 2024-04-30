6e71361629c7c2d57d557e577d239d53d09ef0eb
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-30 17:25:09 +0300

    Rename BoB mainnet exported constant to bob (from bobTestnet)

--- packages/contracts/src/contracts/protocol/bob.ts
+++ packages/contracts/src/contracts/protocol/bob.ts
@@ -1,6 +1,6 @@
 import { AsyncContractConfigData } from '../../types';
 
-export const bobTestnet: Record<string, AsyncContractConfigData> = {
+export const bob: Record<string, AsyncContractConfigData> = {
   staking: {
     address: '0x5fA95212825a474E2C75676e8D833330F261CaeD',
     getAbi: async () => (await import('../../abis/staking.json')).default,

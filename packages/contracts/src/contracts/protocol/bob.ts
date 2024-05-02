18cff3cd86a77f856dfa9f2d75c25e3c9aaa4ac1
Victor Creed <victor@sovryn.app>
2024-05-02 14:34:41 +0300

    chore: wip add merkle distributor settings

--- packages/contracts/src/contracts/protocol/bob.ts
+++ packages/contracts/src/contracts/protocol/bob.ts
@@ -5,6 +5,11 @@ export const bob: Record<string, AsyncContractConfigData> = {
     address: '0xc17C6462cEAFE9A8819258c6bA168BEF5544Fc21',
     getAbi: async () => (await import('../../abis/staking.json')).default,
   },
+  merkleDistributor: {
+    address: '0x0', // todo: update address
+    getAbi: async () =>
+      (await import('../../abis/merkleDistributor.json')).default,
+  },
   runeBridge: {
     address: '',
     getAbi: async () => (await import('../../abis/RuneBridge.json')).default,

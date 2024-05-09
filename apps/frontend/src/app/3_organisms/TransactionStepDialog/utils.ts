c2ef429fdac5e9de575bb7315a9b53f6b739f79f
Victor Creed <victor@sovryn.app>
2024-05-09 14:21:01 +0300

    fix: nonce for hw wallets

--- apps/frontend/src/app/3_organisms/TransactionStepDialog/utils.ts
+++ apps/frontend/src/app/3_organisms/TransactionStepDialog/utils.ts
@@ -55,6 +55,9 @@ export const sendOrSimulateTx = async (
   args: string[],
   config: TransactionConfig = {},
 ): Promise<TransactionResponse> => {
+  const nonce = await request.contract.provider.getTransactionCount(
+    await request.contract.signer.getAddress(),
+  );
   const gasLimit = config.gasLimit ? config.gasLimit?.toString() : undefined;
   const gasPrice = config.gasPrice
     ? parseUnits(config.gasPrice?.toString() || '0', 9)
@@ -92,6 +95,7 @@ export const sendOrSimulateTx = async (
     value: request.value ?? '0',
     gasPrice,
     gasLimit,
+    nonce,
   });
 };
 

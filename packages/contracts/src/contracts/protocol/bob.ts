bb92629039aead4b34fae0219edd162107208897
soulBit <its.soulBit@gmail.com>
2024-05-15 16:17:42 +0100

    feat: update final POWA launch changes, stakingrewardsOS contract address

--- packages/contracts/src/contracts/protocol/bob.ts
+++ packages/contracts/src/contracts/protocol/bob.ts
@@ -10,6 +10,11 @@ export const bob: Record<string, AsyncContractConfigData> = {
     getAbi: async () =>
       (await import('../../abis/merkleDistributor.json')).default,
   },
+  stakingRewardsOs: {
+    address: '0xFdC57Cb52264209afd1559E7E3Db0F28351E9422',
+    getAbi: async () =>
+      (await import('../../abis/StakingRewardsOs.json')).default,
+  },
   runeBridge: {
     address: '0x8989E07E565966463C73dadE4c095DaC991e1dD2',
     getAbi: async () => (await import('../../abis/RuneBridge.json')).default,

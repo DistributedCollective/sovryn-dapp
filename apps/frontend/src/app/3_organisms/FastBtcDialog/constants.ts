76dae6eedbbee87bcc03f0b22dac59711b263eda
Rainer Koirikivi <rainer@koirikivi.fi>
2024-05-23 17:11:23 +0300

    Bump fastbtc-in fees (#922)
    * Bump fastbtc-in fees
    
    * chore: add changeset
    
    * Restore fastbtc-in base fee
    
    ---------
    
    Co-authored-by: soulBit <its.soulBit@gmail.com>

--- apps/frontend/src/app/3_organisms/FastBtcDialog/constants.ts
+++ apps/frontend/src/app/3_organisms/FastBtcDialog/constants.ts
@@ -3,7 +3,7 @@ export const DEPOSIT_FEE_SATS = 5000;
 
 // hardcoded 0.2% dynamic fee
 // https://github.com/DistributedCollective/FastBTC/blob/development/controller/rskCtrl.js#L64
-export const DEPOSIT_FEE_DYNAMIC = 20;
+export const DEPOSIT_FEE_DYNAMIC = 75;
 
 // https://github.com/DistributedCollective/bidirectional-fastbtc/blob/master/packages/fastbtc-contracts/contracts/FastBTCBridge.sol#L105
 export const DYNAMIC_FEE_DIVISOR = 10000;

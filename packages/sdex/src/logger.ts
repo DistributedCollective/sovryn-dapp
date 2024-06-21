3b5badbba2ac3173f7afbc883f9ee13568dbc728
Rick <peymannaderi3@gmail.com>
2024-06-21 11:20:48 +0330

    fix: remove log

--- packages/sdex/src/logger.ts
+++ packages/sdex/src/logger.ts
@@ -1,6 +1,5 @@
 import { datadogLogs } from '@datadog/browser-logs';
 
-console.log('datadoghq: ', process.env.REACT_APP_DATADOG_CLIENT_TOKEN);
 datadogLogs.init({
   clientToken: process.env.REACT_APP_DATADOG_CLIENT_TOKEN || '',
   site: 'datadoghq.com',

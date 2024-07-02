1835755905e9cdd4efcf7c531af4f2c93d20c70d
tiltom <tilnak.tomas@gmail.com>
2024-07-02 10:42:27 +0200

    Use live data

--- apps/frontend/src/app/5_pages/LeaderboardPointsPage/components/LeaderboardPointsSeason2/hooks/useGetPoints.ts
+++ apps/frontend/src/app/5_pages/LeaderboardPointsPage/components/LeaderboardPointsSeason2/hooks/useGetPoints.ts
@@ -1,21 +1,37 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { useAccount } from '../../../../../../hooks/useAccount';
 import { areAddressesEqual } from '../../../../../../utils/helpers';
 import { UserPoints } from '../../../LeaderboardPointsPage.types';
-import usersPointsList from '../data/usersPoints.json';
-
-const data: UserPoints[] = usersPointsList.map(userPoint => ({
-  wallet: userPoint.toAddress,
-  points: parseFloat(userPoint.points.replace(/,/g, '')),
-}));
+import { DATA_ENDPOINT_URL } from '../LeaderboardPointsSeason2.constants';
 
 export const useGetPoints = () => {
   const { account } = useAccount();
 
+  const [data, setData] = useState<UserPoints[]>([]);
+
+  useEffect(() => {
+    if (!data || data.length === 0) {
+      fetch(DATA_ENDPOINT_URL)
+        .then(response => response?.json())
+        .then(data => {
+          if (!data) {
+            return;
+          }
+
+          const result: UserPoints[] = data.map(user => ({
+            wallet: user.toAddress,
+            points: parseFloat(user.points.replace(/,/g, '')),
+          }));
+
+          setData(result);
+        });
+    }
+  }, [data]);
+
   const sortedUsers = useMemo(() => {
     return data.sort((a, b) => b.points - a.points);
-  }, []);
+  }, [data]);
 
   const userIndex = useMemo(() => {
     return sortedUsers.findIndex(user =>

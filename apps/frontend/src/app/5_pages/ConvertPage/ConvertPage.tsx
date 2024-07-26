e8f694380e64d5224047e30dd6a8a21fff250ff9
Pietro <pietro.maxim.official@protonmail.com>
2024-07-26 14:37:17 +0200

    chore: remove price divergence

--- apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.tsx
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.tsx
@@ -382,28 +382,6 @@ const ConvertPage: FC = () => {
     [quote],
   );
 
-  const divergence = useMemo(() => {
-    if (!quote || !price || decimalic(price).isZero()) {
-      return '';
-    }
-
-    const quoteDecimal = decimalic(quote);
-    const amountDecimal = decimalic(amount);
-    let currentPrice;
-
-    if (priceToken === destinationToken) {
-      currentPrice = quoteDecimal.div(amountDecimal);
-    } else {
-      currentPrice = amountDecimal.div(quoteDecimal);
-    }
-
-    const priceDecimal = decimalic(price);
-
-    return Number(
-      currentPrice.sub(priceDecimal).div(priceDecimal).mul(100),
-    ).toFixed(2);
-  }, [quote, price, amount, priceToken, destinationToken]);
-
   const renderPriceAmount = useMemo(() => {
     if (price) {
       return (
@@ -414,12 +392,11 @@ const ConvertPage: FC = () => {
             precision={TOKEN_RENDER_PRECISION}
             trigger={TooltipTrigger.hover}
           />
-          {divergence && ` (${divergence}%)`}
         </>
       );
     }
     return t(commonTranslations.na);
-  }, [price, priceToken, divergence]);
+  }, [price, priceToken]);
 
   const togglePriceQuote = useCallback(
     () => setPriceQuote(value => !value),
@@ -574,51 +551,49 @@ const ConvertPage: FC = () => {
             </div>
           </div>
 
-          {sourceToken && destinationToken && quote ? (
-            <>
-              <Accordion
-                className="mt-4 mb-3 text-xs"
-                label={t(translations.common.advancedSettings)}
-                open={showAdvancedSettings}
-                onClick={() => setShowAdvancedSettings(!showAdvancedSettings)}
-                dataAttribute="convert-settings"
-              >
-                <div className="mt-2 mb-4">
-                  <AmountInput
-                    value={slippageTolerance}
-                    onChange={e => setSlippageTolerance(e.target.value)}
-                    label={t(pageTranslations.slippageTolerance)}
-                    className="max-w-none w-full"
-                    unit="%"
-                    step="0.01"
-                    decimalPrecision={2}
-                    placeholder="0"
-                    max="100"
-                  />
-                </div>
-              </Accordion>
+          <Accordion
+            className="mt-4 mb-3 text-xs"
+            label={t(translations.common.advancedSettings)}
+            open={showAdvancedSettings}
+            onClick={() => setShowAdvancedSettings(!showAdvancedSettings)}
+            dataAttribute="convert-settings"
+          >
+            <div className="mt-2 mb-4">
+              <AmountInput
+                value={slippageTolerance}
+                onChange={e => setSlippageTolerance(e.target.value)}
+                label={t(pageTranslations.slippageTolerance)}
+                className="max-w-none w-full"
+                unit="%"
+                step="0.01"
+                decimalPrecision={2}
+                placeholder="0"
+                max="100"
+              />
+            </div>
+          </Accordion>
 
-              <SimpleTable className="mt-3">
-                <SimpleTableRow
-                  label={t(pageTranslations.minimumReceived)}
-                  valueClassName="text-primary-10"
-                  value={
-                    <AmountRenderer
-                      value={minimumReceived}
-                      suffix={getTokenDisplayName(destinationToken)}
-                      precision={TOKEN_RENDER_PRECISION}
-                    />
-                  }
-                />
-                <SimpleTableRow
-                  label={t(pageTranslations.maximumPrice)}
-                  valueClassName="text-primary-10"
-                  className="cursor-pointer"
-                  onClick={togglePriceQuote}
-                  value={renderPriceAmount}
-                />
-              </SimpleTable>
-            </>
+          {sourceToken && destinationToken && quote ? (
+            <SimpleTable className="mt-3">
+              <SimpleTableRow
+                label={t(pageTranslations.minimumReceived)}
+                valueClassName="text-primary-10"
+                value={
+                  <AmountRenderer
+                    value={minimumReceived}
+                    suffix={getTokenDisplayName(destinationToken)}
+                    precision={TOKEN_RENDER_PRECISION}
+                  />
+                }
+              />
+              <SimpleTableRow
+                label={t(pageTranslations.maximumPrice)}
+                valueClassName="text-primary-10"
+                className="cursor-pointer"
+                onClick={togglePriceQuote}
+                value={renderPriceAmount}
+              />
+            </SimpleTable>
           ) : null}
 
           {hasQuoteError && (

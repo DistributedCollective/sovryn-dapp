bf2886e1c9dfab3fb904739470696e80050a5d65
Victor Creed <victor@sovryn.app>
2024-08-01 09:17:59 +0300

    chore: add SAT icon

--- packages/contracts/src/contracts/assets/bob.ts
+++ packages/contracts/src/contracts/assets/bob.ts
@@ -87,6 +87,6 @@ export const bob: Array<AssetDetails> = [
     address: '0x78fea795cbfcc5ffd6fb5b845a4f53d25c283bdb',
     name: 'Satoshi Stablecoin',
     decimals: 18,
-    getIcon: async () => (await import('./icons/unknown')).default,
+    getIcon: async () => (await import('./icons/bob/sat')).default,
   },
 ];

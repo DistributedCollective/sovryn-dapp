1e8f3c4d9afcc9fd99635aea09ab7f8ea574445a
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2024-08-02 11:52:26 +0300

    fix: LM Vesting crashes rewards page (#972)

--- apps/frontend/src/app/5_pages/RewardsPage/components/Vesting/components/UnlockSchedule/UnlockSchedule.tsx
+++ apps/frontend/src/app/5_pages/RewardsPage/components/Vesting/components/UnlockSchedule/UnlockSchedule.tsx
@@ -20,7 +20,9 @@ export const UnlockSchedule = (item: VestingContractTableRecord) => {
   );
 
   useEffect(() => {
-    update(state => (state.item = item));
+    update(state => {
+      state.item = item;
+    });
   }, [item, update]);
 
   return (

ee9b6ed5a0c61b5fbef225c1909cf1089e44a126
Rick <peymannaderi3@gmail.com>
2024-08-07 15:29:17 +0330

    fix: ambient position value

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/components/AmbientPoolPositions/hooks/useAmbientPositionValue.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AmbientMarketMaking/components/AmbientPoolPositions/hooks/useAmbientPositionValue.ts
@@ -2,12 +2,8 @@ import { useMemo } from 'react';
 
 import { useCurrentChain } from '../../../../../../../../hooks/useChainStore';
 import { useDollarValue } from '../../../../../../../../hooks/useDollarValue';
-import { findAsset } from '../../../../../../../../utils/asset';
-import {
-  bigNumberic,
-  decimalic,
-  toWei,
-} from '../../../../../../../../utils/math';
+import { useTokenDetailsByAsset } from '../../../../../../../../hooks/useTokenDetailsByAsset';
+import { decimalic, toWei } from '../../../../../../../../utils/math';
 import { AmbientPosition } from '../../../AmbientMarketMaking.types';
 import { AmbientLiquidityPool } from '../../../utils/AmbientLiquidityPool';
 import { useAmbientPositionBalance } from './useAmbientPositionBalance';
@@ -18,33 +14,40 @@ export const useAmbientPositionValue = (
 ) => {
   const chainId = useCurrentChain();
   const result = useAmbientPositionBalance(pool, position);
+  const baseToken = useTokenDetailsByAsset(pool.base, pool.chainId);
+  const quoteToken = useTokenDetailsByAsset(pool.quote, pool.chainId);
 
-  const { usdValue: baseValue } = useDollarValue(
+  const { usdPrice: baseUsdPrice } = useDollarValue(
     pool.base,
-    toWei(
-      bigNumberic(result?.positionLiqBase || '0'),
-      18 - findAsset(pool.base, chainId).decimals,
-    ).toString(),
+    toWei(1).toString(),
     chainId,
   );
-  const { usdValue: quoteValue } = useDollarValue(
+  const { usdPrice: quoteUsdPrice } = useDollarValue(
     pool.quote,
-    toWei(
-      bigNumberic(result?.positionLiqQuote || '0'),
-      18 - findAsset(pool.quote, chainId).decimals,
-    ).toString(),
+    toWei(1).toString(),
     chainId,
   );
 
-  const value = useMemo(
-    () =>
-      result
-        ? decimalic(baseValue || 0)
-            .add(quoteValue || 0)
-            .toNumber()
-        : null,
-    [baseValue, quoteValue, result],
-  );
+  const value = useMemo(() => {
+    const baseValue = decimalic(result?.positionLiqBase || '0')
+      .toUnits(baseToken?.decimals)
+      .mul(baseUsdPrice);
+    const quoteValue = decimalic(result?.positionLiqQuote || '0')
+      .toUnits(quoteToken?.decimals)
+      .mul(quoteUsdPrice);
+
+    return result
+      ? decimalic(baseValue || 0)
+          .add(quoteValue || 0)
+          .toNumber()
+      : null;
+  }, [
+    baseToken?.decimals,
+    baseUsdPrice,
+    quoteToken?.decimals,
+    quoteUsdPrice,
+    result,
+  ]);
 
   return value;
 };

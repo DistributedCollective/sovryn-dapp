3f436315f4ced4a5c18e39aa19f095486c0fc3f4
matzapata <matuzapata@gmail.com>
2024-08-09 22:55:53 +0300

    lend modal

--- apps/frontend/src/app/5_pages/AavePage/components/LendAssetsList/components/LendAssetAction/LendAssetAction.tsx
+++ apps/frontend/src/app/5_pages/AavePage/components/LendAssetsList/components/LendAssetAction/LendAssetAction.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { t } from 'i18next';
 
@@ -6,23 +6,41 @@ import { Button, ButtonStyle } from '@sovryn/ui';
 
 import { translations } from '../../../../../../../locales/i18n';
 import { LendPoolDetails } from '../../LendAssetsList.types';
+import { LendModalContainer } from '../LendModal/LendModalContainer';
 
 type LendAssetActionProps = {
   pool: LendPoolDetails;
 };
 
 export const LendAssetAction: FC<LendAssetActionProps> = () => {
+  const [isLendModalOpen, setIsLendModalOpen] = useState<boolean>(false);
+
+  const handleLendClick = () => {
+    setIsLendModalOpen(true);
+  };
+
+  const handleLendClose = () => {
+    setIsLendModalOpen(false);
+  };
+
   return (
     <div className="flex items-center justify-center lg:justify-end space-x-2">
-      {/* TODO: these should be modal triggers */}
       <Button
         className="flex-grow"
         text={t(translations.aavePage.common.lend)}
+        onClick={handleLendClick}
       />
+
+      {/* TODO: Add action for details */}
       <Button
         className="flex-grow"
         text={t(translations.aavePage.common.details)}
         style={ButtonStyle.secondary}
+      />
+
+      <LendModalContainer
+        handleCloseModal={handleLendClose}
+        isOpen={isLendModalOpen}
       />
     </div>
   );

87b4049dd4169b553a00eddfb0a3155719d7f342
matzapata <matuzapata@gmail.com>
2024-08-09 21:04:09 +0300

    repay modals

---
+++ apps/frontend/src/app/5_pages/AavePage/components/BorrowPositionsList/components/RepayModal/RepayModalContainer.tsx
@@ -0,0 +1,77 @@
+import React, { FC, useState } from 'react';
+
+import { t } from 'i18next';
+
+import {
+  Dialog,
+  DialogBody,
+  DialogHeader,
+  Paragraph,
+  ParagraphSize,
+  Tabs,
+  TabType,
+} from '@sovryn/ui';
+
+import { translations } from '../../../../../../../locales/i18n';
+import { RepayWithCollateralForm } from './RepayWithCollateralForm';
+import { RepayWithWalletBalanceForm } from './RepayWithWalletBalanceForm';
+
+type RepayModalContainerProps = {
+  isOpen: boolean;
+  handleCloseModal: () => unknown;
+};
+
+enum PaymentTabs {
+  BALANCE = 0,
+  COLLATERAL,
+}
+
+export const RepayModalContainer: FC<RepayModalContainerProps> = ({
+  isOpen,
+  handleCloseModal,
+}) => {
+  const [activeTab, setActiveTab] = useState<PaymentTabs>(PaymentTabs.BALANCE);
+
+  return (
+    <Dialog disableFocusTrap isOpen={isOpen}>
+      <DialogHeader
+        title={t(translations.aavePage.repayModal.title)}
+        onClose={handleCloseModal}
+      />
+      <DialogBody className="space-y-3">
+        <Paragraph
+          size={ParagraphSize.small}
+          className="font-medium text-gray-30"
+        >
+          {t(translations.aavePage.repayModal.repayWith)}
+        </Paragraph>
+
+        <Tabs
+          className="flex-grow-0"
+          contentClassName="p-4"
+          index={activeTab}
+          onChange={setActiveTab}
+          items={[
+            {
+              activeClassName: 'text-primary-20',
+              dataAttribute: 'wallet-balance',
+              label: t(translations.aavePage.repayModal.walletBalance),
+            },
+            {
+              activeClassName: 'text-primary-20',
+              dataAttribute: 'collateral',
+              label: t(translations.aavePage.common.collateral),
+            },
+          ]}
+          type={TabType.secondary}
+        />
+
+        {activeTab === PaymentTabs.BALANCE ? (
+          <RepayWithWalletBalanceForm onSuccess={handleCloseModal} />
+        ) : (
+          <RepayWithCollateralForm onSuccess={handleCloseModal} />
+        )}
+      </DialogBody>
+    </Dialog>
+  );
+};
---
+++ apps/frontend/src/app/5_pages/AavePage/components/BorrowPositionsList/components/RepayModal/RepayWithWalletBalanceForm.tsx
@@ -0,0 +1,132 @@
+import React, { FC, useMemo, useState } from 'react';
+
+import { t } from 'i18next';
+
+import {
+  Button,
+  ErrorBadge,
+  ErrorLevel,
+  SimpleTable,
+  SimpleTableRow,
+} from '@sovryn/ui';
+import { Decimal } from '@sovryn/utils';
+
+import { AmountTransition } from '../../../../../../2_molecules/AmountTransition/AmountTransition';
+import { AssetAmountInput } from '../../../../../../2_molecules/AssetAmountInput/AssetAmountInput';
+import { AssetRenderer } from '../../../../../../2_molecules/AssetRenderer/AssetRenderer';
+import { useDecimalAmountInput } from '../../../../../../../hooks/useDecimalAmountInput';
+import { translations } from '../../../../../../../locales/i18n';
+import { CollateralRatioHealthBar } from '../../../CollateralRatioHealthBar/CollateralRatioHealthBar';
+
+const pageTranslations = translations.aavePage;
+
+type RepayWithWalletBalanceFormProps = {
+  onSuccess: () => unknown;
+};
+
+export const RepayWithWalletBalanceForm: FC<
+  RepayWithWalletBalanceFormProps
+> = () => {
+  const totalBorrowed = Decimal.from(10); // TODO: this is mocked data. Replace with proper hook
+  const collateralToLoanRate = Decimal.from(10); // TODO: this is mocked data. Replace with proper hook
+  const collateralSize = Decimal.from(10); // TODO: this is mockd data. Replace with proper hook
+  const assetsToRepay = useMemo(() => ['BTC', 'SOV'], []); // TODO: this is mocked data. Replace with proper hook
+  const [maximumRepayAmount] = useState(Decimal.from(10)); // TODO: this is mocked data. Replace with proper hook
+  const [repayAsset, setRepayAsset] = useState<string>(assetsToRepay[0]);
+  const [repayAmount, setRepayAmount, repaySize] = useDecimalAmountInput('');
+
+  const repayAssetsOptions = useMemo(
+    () =>
+      assetsToRepay.map(token => ({
+        value: token,
+        label: (
+          <AssetRenderer
+            showAssetLogo
+            asset={token}
+            assetClassName="font-medium"
+          />
+        ),
+      })),
+    [assetsToRepay],
+  );
+
+  const isValidRepayAmount = useMemo(
+    () => (repaySize.gt(0) ? repaySize.lte(maximumRepayAmount) : true),
+    [repaySize, maximumRepayAmount],
+  );
+
+  const collateralRatio = useMemo(() => {
+    if ([collateralSize, totalBorrowed, repaySize].some(v => v.isZero())) {
+      return Decimal.from(0);
+    }
+
+    return collateralSize.mul(collateralToLoanRate).div(totalBorrowed).mul(100);
+  }, [collateralSize, totalBorrowed, repaySize, collateralToLoanRate]);
+
+  const submitButtonDisabled = useMemo(
+    () => !isValidRepayAmount || repaySize.lte(0),
+    [isValidRepayAmount, repaySize],
+  );
+
+  return (
+    <form className="flex flex-col gap-6 relative">
+      <div className="space-y-3">
+        <AssetAmountInput
+          maxAmount={maximumRepayAmount}
+          amountLabel={t(translations.common.amount)}
+          amountValue={repayAmount}
+          onAmountChange={setRepayAmount}
+          invalid={!isValidRepayAmount}
+          assetValue={repayAsset}
+          onAssetChange={setRepayAsset}
+          assetOptions={repayAssetsOptions}
+        />
+
+        {!isValidRepayAmount && (
+          <ErrorBadge
+            level={ErrorLevel.Critical}
+            message={t(pageTranslations.repayModal.invalidAmountError)}
+            dataAttribute="repay-amount-error"
+          />
+        )}
+      </div>
+
+      <CollateralRatioHealthBar ratio={collateralRatio} />
+
+      <SimpleTable>
+        <SimpleTableRow
+          label={t(translations.aavePage.repayModal.remainingDebt)}
+          value={
+            <AmountTransition
+              className="justify-end"
+              from={{ value: 0.002429, suffix: '%' }}
+              to={{ value: 0, suffix: '%', className: 'text-primary-10' }}
+            />
+          }
+        />
+        <SimpleTableRow
+          label={t(translations.aavePage.repayModal.newCollateralBalance)}
+          value={
+            <div className="space-y-1">
+              <AmountTransition
+                className="justify-end"
+                from={{ value: 10, suffix: 'USDT' }}
+                to={{ value: 10, suffix: 'USDT' }}
+              />
+              <AmountTransition
+                className="justify-end text-gray-40"
+                from={{ value: 10, prefix: '$' }}
+                to={{ value: 10, prefix: '$' }}
+              />
+            </div>
+          }
+        />
+      </SimpleTable>
+
+      <Button
+        disabled={submitButtonDisabled}
+        text={t(translations.common.buttons.confirm)}
+      />
+    </form>
+  );
+};

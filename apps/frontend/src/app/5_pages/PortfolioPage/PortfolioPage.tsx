847698105f906e4adb0124aeb532146efbbf984f
tiltom <tilnak.tomas@gmail.com>
2024-08-12 17:22:34 +0200

    Fix portfolio responsive issues

--- apps/frontend/src/app/5_pages/PortfolioPage/PortfolioPage.tsx
+++ apps/frontend/src/app/5_pages/PortfolioPage/PortfolioPage.tsx
@@ -13,12 +13,12 @@ const PortfolioPage: FC = () => (
       <title>{t(translations.portfolioPage.meta.title)}</title>
     </Helmet>
 
-    <div className="px-0 container md:mx-9 mx-0 md:mb-2 mt-4 md:mt-12 mb-7">
-      <div className="grid grid-cols-9 items-start gap-10 md:gap-12 lg:gap-20 w-full mb-6">
-        <div className="col-span-9 md:col-span-4 lg:col-span-3 md:bg-gray-90 rounded md:py-7 md:px-6">
+    <div className="px-0 container xl:mx-9 mx-0.25 md:mb-2 mt-4 md:mt-12 mb-7">
+      <div className="grid grid-cols-9 items-start gap-4 2xl:gap-12 3xl:gap-20 w-full mb-6">
+        <div className="col-span-9 md:col-span-4 lg:col-span-3 md:bg-gray-90 rounded md:py-7 md:px-2 xl:px-6">
           <ProtocolSection />
         </div>
-        <div className="col-span-9 md:col-span-5 lg:col-span-6 md:bg-gray-90 rounded md:py-7 md:px-6">
+        <div className="col-span-9 md:col-span-5 lg:col-span-6 md:bg-gray-90 rounded md:py-7 md:px-2 xl:px-6">
           <AssetSection />
         </div>
       </div>

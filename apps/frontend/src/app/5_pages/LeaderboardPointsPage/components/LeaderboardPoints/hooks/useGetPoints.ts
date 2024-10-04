973291307343797713ccee6e7907ea51a0209a46
Rick <81699526+rick23p@users.noreply.github.com>
2024-10-04 12:30:52 +0330

    SOV-4467: Spice Leaderboard adjustments for Season 3 (#1016)
    * feat: spice season 3 update
    
    * Create spotty-poems-happen.md

--- apps/frontend/src/app/5_pages/LeaderboardPointsPage/components/LeaderboardPoints/hooks/useGetPoints.ts
+++ apps/frontend/src/app/5_pages/LeaderboardPointsPage/components/LeaderboardPoints/hooks/useGetPoints.ts
@@ -1,66 +1,102 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
+import { RSK_CHAIN_ID } from '../../../../../../config/chains';
+
+import { useCacheCall } from '../../../../../../hooks';
 import { useAccount } from '../../../../../../hooks/useAccount';
 import { areAddressesEqual } from '../../../../../../utils/helpers';
 import { UserPoints } from '../../../LeaderboardPointsPage.types';
-import { DATA_ENDPOINT_URL } from '../LeaderboardPoints.constants';
+import {
+  DATA_ENDPOINT_URL,
+  S3_DATA_ENDPOINT_URL,
+} from '../LeaderboardPoints.constants';
 
 export const useGetPoints = () => {
   const { account } = useAccount();
 
-  const [data, setData] = useState<UserPoints[]>([]);
+  const { value: data } = useCacheCall(
+    'bob-lp-points',
+    RSK_CHAIN_ID,
+    async () => {
+      const [data, s3Data] = await Promise.all([
+        fetch(DATA_ENDPOINT_URL).then(response => response?.json()),
+        fetch(S3_DATA_ENDPOINT_URL).then(response => response?.json()),
+      ]);
 
-  useEffect(() => {
-    if (!data || data.length === 0) {
-      fetch(DATA_ENDPOINT_URL)
-        .then(response => response?.json())
-        .then(data => {
-          if (!data) {
-            return;
-          }
+      const result: { [key: string]: UserPoints } = {};
 
-          const result: UserPoints[] = data.map(user => ({
+      data.forEach(user => {
+        const points = parseFloat(user.points.replace(/,/g, ''));
+
+        result[user.toAddress.toLowerCase()] = {
+          wallet: user.toAddress,
+          points,
+          s3Points: 0,
+          total: points,
+        };
+      });
+
+      s3Data.forEach(user => {
+        const s3Points = parseFloat(user.points.replace(/,/g, ''));
+        const key = user.toAddress.toLowerCase();
+
+        if (!(key in result)) {
+          result[key] = {
             wallet: user.toAddress,
-            points: parseFloat(user.points.replace(/,/g, '')),
-          }));
+            points: 0,
+            s3Points,
+            total: s3Points,
+          };
+        } else {
+          const points = result[key].points;
 
-          setData(result);
-        });
-    }
-  }, [data]);
+          result[key] = {
+            wallet: user.toAddress,
+            points,
+            s3Points,
+            total: points + s3Points,
+          };
+        }
+      });
 
-  const sortedUsers = useMemo(() => {
-    return data.sort((a, b) => b.points - a.points);
-  }, [data]);
+      return Object.keys(result)
+        .map(key => ({ ...result[key] }))
+        .sort((user1, user2) => (user1.total < user2.total ? 1 : -1));
+    },
+    [],
+    [],
+  );
 
   const userIndex = useMemo(() => {
-    return sortedUsers.findIndex(user =>
-      areAddressesEqual(user.wallet, account),
-    );
-  }, [sortedUsers, account]);
+    return data.findIndex(user => areAddressesEqual(user.wallet, account));
+  }, [data, account]);
 
   const connectedWalletPoints = useMemo(() => {
     if (userIndex !== -1) {
-      const { wallet, points } = sortedUsers[userIndex];
+      const { wallet, points, s3Points, total } = data[userIndex];
       return [
         {
           id: userIndex + 1,
           wallet,
           points,
+          s3Points,
+          total,
         },
       ];
     }
     return [];
-  }, [sortedUsers, userIndex]);
+  }, [data, userIndex]);
 
   const points = useMemo(
     () =>
-      sortedUsers.map(({ wallet, points }, index) => ({
+      data.map(({ wallet, points, s3Points, total }, index) => ({
         id: index + 1,
         wallet,
         points,
+        s3Points,
+        total,
       })),
-    [sortedUsers],
+    [data],
   );
 
   return {

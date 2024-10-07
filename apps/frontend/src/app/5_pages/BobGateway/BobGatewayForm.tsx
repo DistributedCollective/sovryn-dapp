5f77df527b56943a3ad4be785dce4b0d86914e04
Rick <peymannaderi3@gmail.com>
2024-10-07 12:42:07 +0330

    feat: update bob gateway

--- apps/frontend/src/app/5_pages/BobGateway/BobGatewayForm.tsx
+++ apps/frontend/src/app/5_pages/BobGateway/BobGatewayForm.tsx
@@ -1,12 +1,19 @@
-import { GatewayQuoteParams, Token } from '@gobob/bob-sdk/dist/gateway/types';
-import { useAccount, useConnect, useDisconnect } from '@gobob/sats-wagmi';
-import { base64 } from '@scure/base';
-import { Transaction } from '@scure/btc-signer';
+import { Token } from '@gobob/bob-sdk/dist/gateway/types';
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useBalance,
+  useSendGatewayTransaction,
+} from '@gobob/sats-wagmi';
 
 import React, { FC, useEffect, useState } from 'react';
 
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
 import { Button, Input, Paragraph, Select } from '@sovryn/ui';
 
+import { useAccount as useEvmAccount } from '../../../hooks/useAccount';
 import { bobGateway } from './BobGateway.utils';
 
 export const BobGatewayForm: FC = () => {
@@ -15,36 +22,80 @@ export const BobGatewayForm: FC = () => {
   const [tokens, setTokens] = useState<Token[]>([]);
   const { connectors, connect } = useConnect();
   const { disconnect } = useDisconnect();
-  const { address } = useAccount();
+  const { address: btcAddress } = useAccount();
+  const { account } = useEvmAccount();
+  const { data } = useBalance();
+  const {
+    // data: hash,
+    // error,
+    // isPending,
+    sendGatewayTransaction,
+  } = useSendGatewayTransaction({
+    toChain: 'bob-sepolia',
+  });
 
   useEffect(() => {
     bobGateway.getTokens().then(setTokens);
   }, []);
 
   const onSbumit = async () => {
-    const quoteParams: GatewayQuoteParams = {
-      fromToken: '',
-      fromChain: 'Bitcoin',
-      fromUserAddress: 'bc1qafk4yhqvj4wep57m62dgrmutldusqde8adh20d',
-      toChain: 'BOB',
-      toUserAddress: '',
-      toToken: '', // or "tBTC"
-      amount: 10000000, // 0.1 BTC
-      gasRefill: 10000, // 0.0001 BTC
-    };
-    const quote = await bobGateway.getQuote(quoteParams);
+    const toToken = tokens.find(t => t.address === token);
 
-    const { uuid, psbtBase64 } = await bobGateway.startOrder(
-      quote,
-      quoteParams,
+    if (!account || !amount || !toToken) {
+      return;
+    }
+    console.log({
+      account,
+      amount,
+      toToken,
+    });
+
+    sendGatewayTransaction(
+      {
+        toToken: 'tBTC',
+        evmAddress: account,
+        value: BigInt(parseUnits(amount, 8).toString()),
+      },
+      {
+        onError: error => console.log({ error }),
+      },
     );
-
-    // NOTE: up to implementation to sign PSBT here!
-    const tx = Transaction.fromPSBT(base64.decode(psbtBase64!));
-    // NOTE: relayer broadcasts the tx
-    await bobGateway.finalizeOrder(uuid, tx.hex);
   };
-  if (!address) {
+  // const onSbumit1 = async () => {
+  //   const quoteParams: GatewayQuoteParams = {
+  //     fromToken: '',
+  //     fromChain: 'Bitcoin',
+  //     fromUserAddress: 'bc1qafk4yhqvj4wep57m62dgrmutldusqde8adh20d',
+  //     toChain: 'bob-sepolia',
+  //     toUserAddress: '',
+  //     toToken: '', // or "tBTC"
+  //     amount: 10000000, // 0.1 BTC
+  //     gasRefill: 10000, // 0.0001 BTC
+  //   };
+  //   // const quoteParams: GatewayQuoteParams = {
+  //   //   fromToken: "BTC",
+  //   //   fromChain: "Bitcoin",
+  //   //   fromUserAddress: "bc1qafk4yhqvj4wep57m62dgrmutldusqde8adh20d",
+  //   //   toChain: "BOB",
+  //   //   toUserAddress: "0x2D2E86236a5bC1c8a5e5499C517E17Fb88Dbc18c",
+  //   //   toToken: "tBTC", // or e.g. "SolvBTC"
+  //   //   amount: 10000000, // 0.1 BTC
+  //   //   gasRefill: 10000, // 0.0001 BTC. The amount of BTC to swap for ETH for tx fees.
+  //   // };
+
+  //   const quote = await bobGateway.getQuote(quoteParams);
+
+  //   const { uuid, psbtBase64 } = await bobGateway.startOrder(
+  //     quote,
+  //     quoteParams,
+  //   );
+
+  //   // NOTE: up to implementation to sign PSBT here!
+  //   const tx = Transaction.fromPSBT(base64.decode(psbtBase64!));
+  //   // NOTE: relayer broadcasts the tx
+  //   await bobGateway.finalizeOrder(uuid, tx.hex);
+  // };
+  if (!btcAddress) {
     return (
       <>
         {connectors.map(connector => (
@@ -58,10 +109,21 @@ export const BobGatewayForm: FC = () => {
 
   return (
     <div className="px-0 container md:mx-9 mx-0 md:mb-2 mb-7">
-      <Paragraph>{address}</Paragraph>
-      <button onClick={() => disconnect()}>Disconnect</button>
-      Amount:
-      <Input value={amount} onChangeText={setAmount} className="my-4" />
+      <br />
+      <Paragraph>BTC Wallet: {btcAddress}</Paragraph>
+      <br />
+      <Button onClick={() => disconnect()} text="Disconnect" />
+      <br />
+      <br />
+      Amount: (Balance: {formatUnits(data?.value.toString() || '0', 8)} BTC)
+      <Input
+        placeholder="Amount (BTC)"
+        step="0.00000001"
+        value={amount}
+        onChangeText={setAmount}
+        className="mb-4"
+        type="number"
+      />
       <br />
       <Select
         value={token}
@@ -81,6 +143,7 @@ export const BobGatewayForm: FC = () => {
         }))}
         className="min-w-36 w-full lg:w-auto"
       />
+      <br />
       <Button text="submit" onClick={onSbumit} />
     </div>
   );

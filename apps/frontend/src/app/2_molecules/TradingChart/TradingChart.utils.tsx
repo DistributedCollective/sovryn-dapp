d5079dae5e27c2898ab50012264f119e0781a8fd
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2024-10-28 20:04:19 +0200

    SOv-4462: charts using indexer (#1036)
    * chore: add chart to Convert Page
    
    * chore: update TradingChart component with using version of lib
    
    * Create slow-drinks-invite.md
    
    * fix: wrong import paths to charting library
    
    * chore: update copyLibs
    
    * Use indexer as datafeed
    
    * fix: datafeed
    
    * chore: remove console log
    
    * Delete obsolete code
    
    * fix: add default tokens and bob chain to chart
    
    * fix: chart
    
    ---------
    
    Co-authored-by: pietro-maximoff <pietro.maxim.official@protonmail.com>
    Co-authored-by: Pietro <74987028+pietro-maximoff@users.noreply.github.com>
    Co-authored-by: tiltom <tilnak.tomas@gmail.com>

---
+++ apps/frontend/src/app/2_molecules/TradingChart/TradingChart.utils.tsx
@@ -0,0 +1,66 @@
+import EventEmitter from 'events';
+
+import { getContract } from '@sovryn/contracts';
+
+import { getCurrentChain } from '../../../hooks/useChainStore';
+import { isMainnet } from '../../../utils/helpers';
+import {
+  SOVRYN_INDEXER_MAINNET,
+  SOVRYN_INDEXER_TESTNET,
+} from './TradingChart.constants';
+import { Bar } from './TradingChart.types';
+import { CandleDetails } from './dictionary';
+
+const pushes: Record<string, number> = {};
+const hub = new EventEmitter({ captureRejections: true });
+
+export const pushPrice = (symbol: string, price: number) => {
+  pushes[symbol] = price;
+  hub.emit(symbol, price);
+};
+
+const indexerBaseUrl = isMainnet()
+  ? SOVRYN_INDEXER_MAINNET
+  : SOVRYN_INDEXER_TESTNET;
+
+export const queryCandles = async (
+  chainId: number,
+  candleDetails: CandleDetails,
+  baseToken: string,
+  quoteToken: string,
+  startTime: number,
+  endTime: number,
+) => {
+  try {
+    const fullIndexerUrl = `${indexerBaseUrl}?chainId=${chainId}&base=${baseToken}&quote=${quoteToken}&start=${startTime}&end=${endTime}&timeframe=${candleDetails.candleSymbol}`;
+
+    const result = await (await fetch(fullIndexerUrl)).json();
+
+    const bars: Bar[] = result.data.reverse().map(item => ({
+      time: Number(item.date) * 1000,
+      low: item.low,
+      high: item.high,
+      open: item.open,
+      close: item.close,
+    }));
+
+    return bars;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Request error: ${error}`);
+  }
+};
+
+const getTokenAddress = async (asset: string) => {
+  const { address } = await getContract(asset, 'assets', getCurrentChain());
+  return address;
+};
+
+export const getTokensFromSymbol = (symbol: string) => {
+  let [base, quote, chainId] = symbol.split('/');
+  return {
+    baseToken: getTokenAddress(base),
+    quoteToken: getTokenAddress(quote),
+    chainId: Number(chainId),
+  };
+};

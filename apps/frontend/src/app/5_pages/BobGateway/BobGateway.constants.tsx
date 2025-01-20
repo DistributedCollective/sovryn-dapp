19abc61667458a0dde71a8c8d8ad87b02e386859
Tomas Tilnak <tilnak.tomas@gmail.com>
2025-01-20 15:13:53 +0100

    Force rebuild

--- apps/frontend/src/app/5_pages/BobGateway/BobGateway.constants.tsx
+++ apps/frontend/src/app/5_pages/BobGateway/BobGateway.constants.tsx
@@ -24,6 +24,7 @@ export const strategies = [
     about:
       'Maximize your rewards with liquid Bitcoin staking. This pool facilitates seamless swaps between SolvBTC.BBN and UniBTC, two Liquid Staking Tokens (LSTs) representing BTC. With a 20x Spice multiplier, it offers high incentives for liquidity providers looking to optimize yield while staying fully exposed to BTC derivatives.',
   },
+
   {
     strategyAddress: '0x2a4F027cb568F2F3f7b2F7008b92C3B0eFc8DCfD',
     toToken: 'wBTC',
@@ -41,6 +42,7 @@ export const strategies = [
     about:
       'Combine wrapped BTC liquidity with staking rewards. This pool pairs wBTC with SolvBTC.BBN, allowing users to move between a widely-used BTC wrapper and an interest-bearing LST. Liquidity providers earn a 7x Spice multiplier on wBTC and a 20x multiplier on SolvBTC.BBN, making it an attractive option for BTC holders looking to gain exposure to liquid staking rewards.',
   },
+
   {
     strategyAddress: '0x9509e42a304b408A17403a0a439692DBe8b68F6c',
     toToken: 'wBTC',

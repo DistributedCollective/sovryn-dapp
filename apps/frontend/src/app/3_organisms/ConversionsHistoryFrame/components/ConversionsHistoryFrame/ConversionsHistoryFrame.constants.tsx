aa5eaecfb6543886f9265a0da2584b4a8ac7d5b5
Pietro <74987028+pietro-maximoff@users.noreply.github.com>
2025-07-29 14:07:14 +0200

    SOV-4565: Update conversion history (#1087)
    * chore: update conversion history
    
    * Create kind-goats-repair.md
    
    * chore: update fees

--- apps/frontend/src/app/3_organisms/ConversionsHistoryFrame/components/ConversionsHistoryFrame/ConversionsHistoryFrame.constants.tsx
+++ apps/frontend/src/app/3_organisms/ConversionsHistoryFrame/components/ConversionsHistoryFrame/ConversionsHistoryFrame.constants.tsx
@@ -2,20 +2,49 @@ import React from 'react';
 
 import { t } from 'i18next';
 
+import { ChainId } from '@sovryn/ethers-provider';
+
 import { AmountRenderer } from '../../../../2_molecules/AmountRenderer/AmountRenderer';
 import { TransactionIdRenderer } from '../../../../2_molecules/TransactionIdRenderer/TransactionIdRenderer';
-import { BTC_RENDER_PRECISION } from '../../../../../constants/currencies';
+import {
+  BTC_RENDER_PRECISION,
+  TOKEN_RENDER_PRECISION,
+} from '../../../../../constants/currencies';
 import { getTokenDisplayName } from '../../../../../constants/tokens';
 import { translations } from '../../../../../locales/i18n';
-import { Swap } from '../../../../../utils/graphql/rsk/generated';
+import { COMMON_SYMBOLS } from '../../../../../utils/asset';
 import { dateFormat } from '../../../../../utils/helpers';
 import { decimalic } from '../../../../../utils/math';
 
-export const COLUMNS_CONFIG = [
+export type Token = {
+  address: string;
+  decimals: number;
+  symbol: string;
+  logoUrl: string;
+};
+
+export type Swap = {
+  id: number;
+  transactionHash: string;
+  user: string;
+  base: Token;
+  baseAmount: string;
+  quote: Token;
+  quoteAmount: string;
+  fees: string;
+  price: string;
+  confirmedAt: string;
+  block: number;
+  callIndex: number;
+};
+
+export const COLUMNS_CONFIG = (chain: ChainId) => [
   {
     id: 'timestamp',
     title: t(translations.common.tables.columnTitles.timestamp),
-    cellRenderer: (item: Swap) => <>{dateFormat(item.transaction.timestamp)}</>,
+    cellRenderer: (item: Swap) => (
+      <>{dateFormat(new Date(item.confirmedAt).getTime() / 1000)}</>
+    ),
     sortable: true,
   },
   {
@@ -28,8 +57,8 @@ export const COLUMNS_CONFIG = [
     title: t(translations.conversionsHistory.table.sent),
     cellRenderer: (item: Swap) => (
       <AmountRenderer
-        value={item.fromAmount}
-        suffix={getTokenDisplayName(item.fromToken.symbol!)}
+        value={item.baseAmount}
+        suffix={getTokenDisplayName(item.base.symbol!)}
       />
     ),
   },
@@ -38,8 +67,8 @@ export const COLUMNS_CONFIG = [
     title: t(translations.conversionsHistory.table.received),
     cellRenderer: (item: Swap) => (
       <AmountRenderer
-        value={item.toAmount}
-        suffix={getTokenDisplayName(item.toToken.symbol!)}
+        value={item.quoteAmount}
+        suffix={getTokenDisplayName(item.quote.symbol!)}
       />
     ),
   },
@@ -48,9 +77,13 @@ export const COLUMNS_CONFIG = [
     title: t(translations.conversionsHistory.table.conversionFee),
     cellRenderer: (swap: Swap) => (
       <AmountRenderer
-        value={decimalic(swap.conversionFee).add(swap.protocolFee || '0')}
-        suffix={getTokenDisplayName(swap.toToken.symbol!)}
-        precision={BTC_RENDER_PRECISION}
+        value={decimalic(swap.fees)}
+        suffix={getTokenDisplayName(swap.quote.symbol!)}
+        precision={
+          [COMMON_SYMBOLS.BTC, COMMON_SYMBOLS.WBTC].includes(swap.base.symbol)
+            ? BTC_RENDER_PRECISION
+            : TOKEN_RENDER_PRECISION
+        }
         dataAttribute="amm-history-fee"
       />
     ),
@@ -60,7 +93,8 @@ export const COLUMNS_CONFIG = [
     title: t(translations.common.tables.columnTitles.transactionID),
     cellRenderer: (item: Swap) => (
       <TransactionIdRenderer
-        hash={item.transaction.id}
+        hash={item.transactionHash}
+        chainId={chain}
         dataAttribute="amm-conversion-history-tx-hash"
       />
     ),

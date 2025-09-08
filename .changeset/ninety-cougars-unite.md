8715049694f4d840a1ab8c62ac0ceb014536e189
tiltom <tiltom.defi@protonmail.com>
2025-09-08 09:13:25 +0200

    SOV-5102: Set destination token if there is only one option (#1102)
    * Set destination token if there is only one option
    
    * Create ninety-cougars-unite.md
    
    * fix: build crashes
    
    ---------
    
    Co-authored-by: Victor Creed <69458664+creed-victor@users.noreply.github.com>
    Co-authored-by: Victor Creed <victor@sovryn.app>

---
+++ .changeset/ninety-cougars-unite.md
@@ -0,0 +1,5 @@
+---
+"frontend": patch
+---
+
+SOV-5102: Set destination token if there is only one option
--- apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.tsx
+++ apps/frontend/src/app/5_pages/ConvertPage/ConvertPage.tsx
@@ -697,6 +697,20 @@ const ConvertPage: FC = () => {
     }
   }, [account, setAmount]);
 
+  // Set destination token if not already set and there is only one option
+  useEffect(() => {
+    if (
+      sourceToken &&
+      (!destinationToken ||
+        !destinationTokenOptions.find(
+          option => option.value === destinationToken,
+        )) &&
+      destinationTokenOptions.length === 1
+    ) {
+      setDestinationToken(destinationTokenOptions[0].value);
+    }
+  }, [sourceToken, destinationTokenOptions, destinationToken]);
+
   return (
     <>
       <Helmet>

1d72f0f29a33101d61f1818f181e3760d095d470
Victor Creed <victor@sovryn.app>
2026-01-20 11:06:09 +0200

    fix: estimated amm weekly rewards

--- apps/frontend/src/app/5_pages/MarketMakingPage/components/AdjustAndDepositModal/hooks/useGetPoolBalanceAndRewards.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/components/AdjustAndDepositModal/hooks/useGetPoolBalanceAndRewards.ts
@@ -1,57 +1,96 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import { Decimal } from '@sovryn/utils';
 
 import { RSK_CHAIN_ID } from '../../../../../../config/chains';
 
 import { useGetTokenContract } from '../../../../../../hooks/useGetContract';
-import { COMMON_SYMBOLS } from '../../../../../../utils/asset';
+import {
+  COMMON_SYMBOLS,
+  maybeWrappedAsset,
+} from '../../../../../../utils/asset';
 import { decimalic } from '../../../../../../utils/math';
 import { useGetTokenPrice } from '../../../../BorrowPage/hooks/useGetTokenPrice';
+import { useGetReturnRate } from '../../../hooks/useGetReturnRate';
 import { AmmLiquidityPool } from '../../../utils/AmmLiquidityPool';
-import { WEEKLY_REWARDS_AMOUNT } from '../AdjustAndDepositModal.constants';
 
 export const useGetPoolBalanceAndRewards = (
   pool: AmmLiquidityPool,
   newPoolBalanceA: Decimal,
+  newPoolBalanceB: Decimal,
+  isTokenA: boolean,
+  isV2Pool: boolean,
 ) => {
-  const [weeklyRewardsEstimation, setWeeklyRewardsEstimation] =
-    useState<Decimal>(Decimal.ZERO);
-
   const poolContract = useGetTokenContract(COMMON_SYMBOLS.WBTC, RSK_CHAIN_ID);
 
-  const tokenContract = useGetTokenContract(pool.assetA, RSK_CHAIN_ID);
-  const { data: tokenPrice } = useGetTokenPrice(tokenContract?.address || '');
+  const tokenContractA = useGetTokenContract(
+    maybeWrappedAsset(pool.assetA),
+    RSK_CHAIN_ID,
+  );
+  const tokenContractB = useGetTokenContract(
+    maybeWrappedAsset(pool.assetB),
+    RSK_CHAIN_ID,
+  );
+  const { data: tokenPriceA } = useGetTokenPrice(tokenContractA?.address || '');
+  const { data: tokenPriceB } = useGetTokenPrice(tokenContractB?.address || '');
+  const returnRates = useGetReturnRate(pool);
 
-  const fetchPoolBalance = useCallback(async () => {
-    if (!poolContract || !newPoolBalanceA || !tokenPrice || !tokenContract) {
-      return;
-    }
-    try {
+  const { data: weeklyRewardsEstimation } = useQuery({
+    queryKey: [
+      'poolBalanceAndRewards',
+      pool.converter,
+      newPoolBalanceA.toString(),
+      newPoolBalanceB.toString(),
+      tokenPriceA,
+      tokenPriceB,
+      isTokenA,
+      isV2Pool,
+      returnRates,
+    ],
+    queryFn: async () => {
+      if (
+        !poolContract ||
+        !newPoolBalanceA ||
+        !tokenPriceA ||
+        !tokenContractA
+      ) {
+        return Decimal.ZERO;
+      }
       const poolBalance = await poolContract
         .balanceOf(pool.converter)
         .then(Decimal.fromBigNumberString);
       if (poolBalance) {
-        const value = newPoolBalanceA
-          .div(poolBalance)
-          .mul(WEEKLY_REWARDS_AMOUNT)
-          .mul(decimalic(tokenPrice?.token?.lastPriceBtc || '0'));
-        setWeeklyRewardsEstimation(value);
+        if (isV2Pool) {
+          const balance = isTokenA ? newPoolBalanceA : newPoolBalanceB;
+          return balance
+            .mul(
+              decimalic(
+                isTokenA
+                  ? tokenPriceA.token?.lastPriceBtc || '0'
+                  : tokenPriceB?.token?.lastPriceBtc || '0',
+              ),
+            )
+            .mul(decimalic(returnRates.beforeRewards).div(100))
+            .div(52);
+        } else {
+          const value1 = newPoolBalanceA
+            .mul(decimalic(tokenPriceA?.token?.lastPriceBtc || '0'))
+            .mul(decimalic(returnRates.beforeRewards).div(100))
+            .div(52);
+          const value2 = newPoolBalanceB
+            .mul(decimalic(tokenPriceB?.token?.lastPriceBtc || '0'))
+            .mul(decimalic(returnRates.beforeRewards).div(100))
+            .div(52);
+          return value1.add(value2);
+        }
       }
-    } catch (error) {
-      console.error('Error fetching pool balance:', error);
-    }
-  }, [
-    pool.converter,
-    newPoolBalanceA,
-    tokenPrice,
-    poolContract,
-    tokenContract,
-  ]);
 
-  useEffect(() => {
-    fetchPoolBalance();
-  }, [fetchPoolBalance]);
+      return Decimal.ZERO;
+    },
+    enabled:
+      !!poolContract && !!newPoolBalanceA && !!tokenPriceA && !!tokenContractA,
+    initialData: Decimal.ZERO,
+  });
 
   return { weeklyRewardsEstimation };
 };
--- apps/frontend/src/app/5_pages/MarketMakingPage/hooks/useGetReturnRate.ts
+++ apps/frontend/src/app/5_pages/MarketMakingPage/hooks/useGetReturnRate.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import { useBlockNumber } from '../../../../hooks/useBlockNumber';
 import { getAmmServiceUrl } from '../../../../utils/helpers';
@@ -32,53 +32,44 @@ export const useGetReturnRate = ({
   const ammServiceUrl = getAmmServiceUrl();
   const { value: block } = useBlockNumber();
 
-  const [returnRates, setReturnRates] = useState<ReturnRates>({
-    beforeRewards: '0',
-    afterRewards: '0',
+  const { data } = useQuery({
+    queryKey: ['ammPoolReturnRates', converter, block],
+    queryFn: async () => {
+      const response = await fetch(`${ammServiceUrl}/amm`);
+      const data: AmmResponse = await response.json();
+
+      if (data[converter]) {
+        const poolData =
+          converterVersion === 1
+            ? data[converter].data[Object.keys(data[converter].data)[0]]
+            : data[converter].data[poolTokenA];
+
+        if (poolData && poolData.length > 0) {
+          const sumFeesApy = poolData.reduce(
+            (acc, entry) => acc + parseFloat(entry.APY_fees_pc),
+            0,
+          );
+          const sumTotalApy = poolData.reduce(
+            (acc, entry) => acc + parseFloat(entry.APY_pc),
+            0,
+          );
+
+          const avgFeesApy = (sumFeesApy / poolData.length).toFixed(2);
+          const avgTotalApy = (sumTotalApy / poolData.length).toFixed(2);
+
+          return {
+            beforeRewards: avgFeesApy,
+            afterRewards: avgTotalApy,
+          };
+        } else {
+          return { beforeRewards: '0', afterRewards: '0' };
+        }
+      } else {
+        return { beforeRewards: '0', afterRewards: '0' };
+      }
+    },
+    initialData: { beforeRewards: '0', afterRewards: '0' },
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${ammServiceUrl}/amm`);
-        const data: AmmResponse = await response.json();
-
-        if (data[converter]) {
-          const poolData =
-            converterVersion === 1
-              ? data[converter].data[Object.keys(data[converter].data)[0]]
-              : data[converter].data[poolTokenA];
-
-          if (poolData && poolData.length > 0) {
-            const sumFeesApy = poolData.reduce(
-              (acc, entry) => acc + parseFloat(entry.APY_fees_pc),
-              0,
-            );
-            const sumTotalApy = poolData.reduce(
-              (acc, entry) => acc + parseFloat(entry.APY_pc),
-              0,
-            );
-
-            const avgFeesApy = (sumFeesApy / poolData.length).toFixed(2);
-            const avgTotalApy = (sumTotalApy / poolData.length).toFixed(2);
-
-            setReturnRates({
-              beforeRewards: avgFeesApy,
-              afterRewards: avgTotalApy,
-            });
-          } else {
-            setReturnRates({ beforeRewards: '0', afterRewards: '0' });
-          }
-        } else {
-          setReturnRates({ beforeRewards: '0', afterRewards: '0' });
-        }
-      } catch (error) {
-        console.error('Error fetching amm pool data:', error);
-      }
-    };
-
-    fetchData();
-  }, [ammServiceUrl, block, converter, converterVersion, poolTokenA]);
-
-  return { returnRates };
+  return data;
 };
